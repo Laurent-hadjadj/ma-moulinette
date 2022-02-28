99e24e3240bc0db3c8c40ecbb6b84a47181c9604
Laurent HADJADJ <laurent_h@me.com>
2022-02-28 09:19:02 +0100

    Ajout de la version du serveur

---
+++ src/Controller/OwaspController.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ *  Copyright (c) 2021-2022.
+ *  Laurent HADJADJ <laurent_h@me.com>.
+ *  Licensed Creative Common  CC-BY-NC-SA 4.0.
+ *  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+ *  http://creativecommons.org/licenses/by-nc-sa/4.0/
+ *
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OwaspController extends AbstractController
+{
+  #[Route('/owasp', name: 'owasp')]
+  public function index()
+    {
+      return $this->render('owasp/index.html.twig',
+      [
+        "serveur"=>$this->getParameter("sonar.url"),
+        "version" => $this->getParameter("version"), "dateCopyright" => \date("Y")
+      ]);
+
+    }
+
+}
