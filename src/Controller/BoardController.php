87d2b5cb74e68a2d29f97303c6ed866a6d95b2c8
Laurent HADJADJ <laurent_h@me.com>
2022-03-13 14:24:12 +0100

    Supprime les doublons

--- src/Controller/BoardController.php
+++ src/Controller/BoardController.php
@@ -33,22 +33,18 @@ class BoardController extends AbstractController
     $maven_key=$request->get('maven_key');
 
     // On récupere les failles owasp
-    $sql="select nom_projet as nom, version, suppress_warning, no_sonar, nombre_bug as bug, nombre_vulnerability as faille, nombre_code_smell as mauvaise_pratique, hotspot_total as nombre_hotspot, frontend as presentation, backend as metier, batch, note_reliability as fiabilite, note_security as securite, note_hotspot, note_sqale as maintenabilite from historique where maven_key='". $maven_key."' order by date_version ASC";
+    $sql="SELECT nom_projet as nom, version, suppress_warning, no_sonar, nombre_bug as bug, nombre_vulnerability as faille, nombre_code_smell as mauvaise_pratique, hotspot_total as nombre_hotspot, frontend as presentation, backend as metier, batch, note_reliability as fiabilite, note_security as securite, note_hotspot, note_sqale as maintenabilite FROM historique WHERE maven_key='". $maven_key."' GROUP BY date_version ORDER BY date_version ASC";
     $select=$em->getConnection()->prepare($sql)->executeQuery();
     $dash=$select->fetchAllAssociative();
 
-    $sql="select nombre_anomalie_bloquante as bloquant, nombre_anomalie_critique as critique, nombre_anomalie_majeur as majeur from historique where maven_key='". $maven_key."' order by date_version ASC";
+    // On récupére les anomalies par sévérité
+    $sql="SELECT nombre_anomalie_bloquante as bloquant, nombre_anomalie_critique as critique, nombre_anomalie_majeur as majeur FROM historique where maven_key='". $maven_key."' GROUP BY date_version ORDER BY date_version ASC";
     $select=$em->getConnection()->prepare($sql)->executeQuery();
     $severite=$select->fetchAllAssociative();
-
-    // si on ne trouve pas la liste
-    //if (empty($dash)) { return $response->setData(["code"=>"406"]); }
 
     return $this->render('dash/index.html.twig',
     [   'dash'=>$dash, 'severite'=>$severite, 'nom'=>$dash[0]["nom"],
         'version' => $this->getParameter('version'), 'dateCopyright' => \date('Y')
     ]);
-
   }
-
 }
