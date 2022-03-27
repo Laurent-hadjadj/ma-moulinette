ea61f80d26424325ba64d372706ca72fd5a53e20
Laurent HADJADJ <laurent_h@me.com>
2022-03-27 13:10:03 +0200

    Correction typographique

--- assets/js/app-projet-enregistrement.js
+++ assets/js/app-projet-enregistrement.js
@@ -127,7 +127,7 @@ export function enregistrement(maven_key) {
   { maven_key: maven_key, nom_projet:nom_projet,
     version_release:version_release, version_snapshot:version_snapshot, version:version,
     date_version:date_version, suppress_warning:suppress_warning, no_sonar:no_sonar,
-    nombre_de_ligne_de_code:nombre_ligne_de_code, nombre_ligne:nombre_ligne, couverture:couverture, duplication:duplication,
+    nombre_ligne_de_code:nombre_ligne_de_code, nombre_ligne:nombre_ligne, couverture:couverture, duplication:duplication,
     tests_unitaires:tests_unitaires, nombre_defaut:nombre_defaut,
     dette:dette,
     nombre_bug:nombre_bug, nombre_vulnerability:nombre_vulnerability, nombre_code_smell:nombre_code_smell,
