40dc90f0100ad7e5e8a0afda5a5c94e4fa5d7611
Laurent HADJADJ <laurent_h@me.com>
2022-03-29 10:53:41 +0200

    Nettoyage

--- src/Repository/HistoriqueRepository.php
+++ src/Repository/HistoriqueRepository.php
@@ -19,32 +19,4 @@ class HistoriqueRepository extends ServiceEntityRepository
         parent::__construct($registry, Historique::class);
     }
 
-    // /**
-    //  * @return Historique[] Returns an array of Historique objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('h')
-            ->andWhere('h.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('h.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Historique
-    {
-        return $this->createQueryBuilder('h')
-            ->andWhere('h.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
