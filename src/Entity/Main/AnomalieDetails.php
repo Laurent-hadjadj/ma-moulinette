cfd7c0a6fa127ded7b2698f972473d165c74c3cf
Laurent HADJADJ <laurent_h@me.com>
2022-08-01 14:07:59 +0200

    Mise à jour des Entity (regeneration)

--- src/Entity/Main/AnomalieDetails.php
+++ src/Entity/Main/AnomalieDetails.php
@@ -14,6 +14,7 @@
 namespace App\Entity\Main;
 
 use App\Repository\Main\AnomalieDetailsRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AnomalieDetailsRepository::class)]
--- src/Entity/Main/NoSonar.php
+++ src/Entity/Main/NoSonar.php
@@ -14,6 +14,7 @@
 namespace App\Entity\Main;
 
 use App\Repository\Main\NoSonarRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: NoSonarRepository::class)]
