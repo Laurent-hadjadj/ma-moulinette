96f37493597912e421a95f546e173368969716f5
Laurent HADJADJ <laurent_h@me.com>
2022-09-07 10:59:31 +0200

    Mise à jour php 8.1.10

---
+++ bin/dcomposer.bat
@@ -0,0 +1,1 @@
+docker run -it --rm -v /opt/web/DATA/WEBCONTENT/ma-moulinette:/app composer $1
--- bin/symfony_stop.bat
+++ bin/symfony_stop.bat
@@ -5,17 +5,20 @@
 @echo  Laurent HADJADJ - 2022-01-25 v1.0.0
 @echo  Laurent HADJADJ - 2022-03-28 v1.1.0
 @echo  Laurent HADJADJ - 2022-03-29 v1.2.0
+@echo  Laurent HADJADJ - 2022-09-07 v1.3.0
 @echo *****************************************
 
 @echo:
 @echo Env       : dev
-@echo Script    : v1.2.0
-@echo Symfony   : v5.4.2
-@echo Php       : v8.1.0
+@echo Script    : v1.3.0
+@echo Symfony   : v6.1.3
+@echo Php       : v8.1.10
 
 @set app=c:\sonar-dash.dev
+@set php=%app%\php-8.1.10
+@set nodejs=%app%\node-12.22.12
 
-@set PATH=%app%\symfony-cli\current;%app%\php-8.1.0;%PATH%
+@set PATH=%app%\symfony-cli\current;%php%;%PATH%
 @cd %app%\ma-moulinette
 
 @symfony server:stop
