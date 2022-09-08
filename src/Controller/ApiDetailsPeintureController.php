13b783446554f2d72a195e1b42eecfeec8d0b12f
Laurent HADJADJ <laurent_h@me.com>
2022-09-08 11:04:24 +0200

    Constructeur EntityManagerInterface

--- src/Controller/ApiDetailsPeintureController.php
+++ src/Controller/ApiDetailsPeintureController.php
@@ -26,6 +26,14 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class ApiDetailsPeintureController extends AbstractController
 {
+  private $em;
+
+  public function __construct(
+    EntityManagerInterface $em,
+    )
+  {
+    $this->em = $em;
+  }
 
   const HTTP_ERROR_406 = "                   Vous devez lancer une analyse pour ce projet !!!";
 
@@ -33,15 +41,14 @@ class ApiDetailsPeintureController extends AbstractController
    * is_valide
    * Vérification de l'existence du projet dans la table information_projet
    *
-   * @param  mixed $em
    * @param  mixed $mavenKey
    * @return array
    */
-  protected function isValide(EntityManagerInterface $em, $mavenKey): array
+  protected function isValide($mavenKey): array
   {
     // On regarde si une analyse a été réalisée.
     $sql = "SELECT * FROM information_projet WHERE maven_key='" . $mavenKey . "' LIMIT 1";
-    $r = $em->getConnection()->prepare($sql)->executeQuery();
+    $r = $this->em->getConnection()->prepare($sql)->executeQuery();
     $response = $r->fetchAllAssociative();
     if (empty($response)) {
       return ["code" => 406];
@@ -54,24 +61,23 @@ class ApiDetailsPeintureController extends AbstractController
    * peinture_projet_anomalie_details
    * Récupère les informations sur la dette technique et les anamalies
    *
-   * @param  mixed $em
    * @param  mixed $request
    * @return response
    */
   #[Route('/api/peinture/projet/anomalie/details', name: 'peinture_projet_anomalie_details', methods: ['GET'])]
-  public function peintureProjetAnomalieDetails(EntityManagerInterface $em, Request $request): response
+  public function peintureProjetAnomalieDetails(Request $request): response
   {
     $mavenKey = $request->get('mavenKey');
     $response = new JsonResponse();
 
-    $isValide = $this->isValide($em, $mavenKey);
+    $isValide = $this->isValide($mavenKey);
     if ($isValide["code"] == 406) {
       return $response->setData(["message" => static::HTTP_ERROR_406, Response::HTTP_NOT_ACCEPTABLE]);
     }
 
     // On récupère la dernière version et sa date de publication
     $sql = "SELECT * FROM anomalie WHERE maven_key='" . $mavenKey . "' ORDER BY date_enregistrement DESC LIMIT 1";
-    $r = $em->getConnection()->prepare($sql)->executeQuery();
+    $r = $this->em->getConnection()->prepare($sql)->executeQuery();
     $anomalies = $r->fetchAllAssociative();
 
     /* Calcul de la dette technique */
@@ -82,22 +88,22 @@ class ApiDetailsPeintureController extends AbstractController
 
     // Calcul du nombre total d'anomalie
     $bug = $anomalies[0]["bug_blocker"] +
-           $anomalies[0]["bug_critical"] +
-           $anomalies[0]["bug_info"] +
-           $anomalies[0]["bug_major"] +
-           $anomalies[0]["bug_minor"];
+            $anomalies[0]["bug_critical"] +
+            $anomalies[0]["bug_info"] +
+            $anomalies[0]["bug_major"] +
+            $anomalies[0]["bug_minor"];
 
     $vulnerability = $anomalies[0]["vulnerability_blocker"] +
-                     $anomalies[0]["vulnerability_critical"] +
-                     $anomalies[0]["vulnerability_info"] +
-                     $anomalies[0]["vulnerability_major"] +
-                     $anomalies[0]["vulnerability_minor"];
+                      $anomalies[0]["vulnerability_critical"] +
+                      $anomalies[0]["vulnerability_info"] +
+                      $anomalies[0]["vulnerability_major"] +
+                      $anomalies[0]["vulnerability_minor"];
 
     $codeSmell = $anomalies[0]["code_smell_blocker"] +
-                 $anomalies[0]["code_smell_critical"] +
-                 $anomalies[0]["code_smell_info"] +
-                 $anomalies[0]["code_smell_major"] +
-                 $anomalies[0]["code_smell_minor"];
+                  $anomalies[0]["code_smell_critical"] +
+                  $anomalies[0]["code_smell_info"] +
+                  $anomalies[0]["code_smell_major"] +
+                  $anomalies[0]["code_smell_minor"];
 
     /* Répartition des anomalies par sévérité et par nature*/
     $bugBlocker = $anomalies[0]["bug_blocker"];
