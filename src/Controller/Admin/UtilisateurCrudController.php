5ef81511fa6d0068503e3ed5d225e344a5a60b0c
Laurent HADJADJ <laurent_h@me.com>
2022-09-09 10:42:57 +0200

    Création du controller CRUD pour l'Entity Utilisateur

---
+++ src/Controller/Admin/UtilisateurCrudController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Main\Utilisateur;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AvatarField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+
+
+class UtilisateurCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Utilisateur::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return parent::configureActions($actions)
+            ->disable(Action::NEW);
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        yield idField::new('id')->onlyOnIndex();
+        yield AvatarField::new('avatar')
+            // If we have a User, then call getAvatarUrl() and return that string. But if we don't have a user, skip calling the method and just return null.
+            ->formatValue(static function ($value, ?Utilisateur $utilisateur) {
+                return $utilisateur?->getAvatarUrl();
+            })
+            ->setFormTypeOption(
+                'disabled',
+                $pageName !== Crud::PAGE_DETAIL
+            )
+            ->hideOnForm();
+
+        yield ImageField::new('avatar', 'Image')
+            ->setBasePath('public/avatar')
+            ->setUploadDir('public/avatar')
+            ->setUploadedFileNamePattern('[slug]-[timestamp].[extension]')
+            ->hideOnDetail()
+            ->hideOnIndex();
+        yield EmailField::new('courriel');
+        yield TextField::new('personne')
+            ->hideOnForm();
+            $roles = ['ROLE_ADMIN', 'ROLE_USER'];
+        yield ChoiceField::new('roles')
+            ->setChoices(array_combine($roles, $roles))
+            ->allowMultipleChoices()
+            ->renderExpanded()
+            ->renderAsBadges();
+        yield BooleanField::new('actif')->renderAsSwitch(false);
+        yield DateTimeField::new('dateEnregistrement')->hideOnForm();
+        yield DateTimeField::new('dateModification')
+            ->setTimezone('Europe/Paris')
+            ->hideOnForm();
+    }
+}
