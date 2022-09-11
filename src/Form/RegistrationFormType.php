27a2f16c3161b6e6be83e7beafa2993b64633568
Laurent HADJADJ <laurent_h@me.com>
2022-09-11 12:04:24 +0200

    Ajout du registrationForm

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ *  Ma-Moulinette
+ *  --------------
+ *  Copyright (c) 2021-2022.
+ *  Laurent HADJADJ <laurent_h@me.com>.
+ *  Licensed Creative Common  CC-BY-NC-SA 4.0.
+ *  ---
+ *  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+ *  http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+namespace App\Form;
+
+use App\Entity\Main\Utilisateur;
+use Symfony\Component\Form\AbstractType;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom',TextType::class, [
+                'required' => true,
+                'attr' => ['maxlength' => 64],
+                'attr' => ['placeholder' => 'placeholder.nom'],
+                'label' => 'label.nom',
+                'trim' => true
+            ])
+            ->add('prenom',TextType::class, [
+                'required' => true,
+                'attr' => ['maxlength' => 32],
+                'attr' => ['placeholder' => 'placeholder.prenom'],
+                'label' => 'label.prenom',
+                'trim' => true
+            ])
+            ->add('courriel',EmailType::class, [
+                'attr' => ['maxlength' => 72],
+                'attr' => ['placeholder' => 'placeholder.courriel'],
+                'label' => 'label.courriel',
+                'trim' => true
+                //There is already an account with this courriel
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'label' => 'label.motdepasse',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Merci de saisir votre mot de passe.',
+                    ]),
+                    new Length([
+                        'min' => 8,
+                        'minMessage' => 'Votre mot de passe doit avoir au moins {{ limit }} caratères.',
+                        'max' => 255,
+                    ]),
+                ],
+            ])
+            ->add('avatar', HiddenType::class, [
+                'data' => 'personne.png',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Utilisateur::class,
+        ]);
+    }
+}
