0a49070c7661e9c5494424c1d99fc6cc95070ec5
Laurent HADJADJ <laurent_h@me.com>
2022-09-11 18:59:32 +0200

    Ajout des contrôles sur courriel et password

--- assets/js/app-register.js
+++ assets/js/app-register.js
@@ -23,6 +23,16 @@ import './foundation.js';
 
 console.log('Register : Chargement de webpack !');
 
+let checkOkSvg, checkKoSvg;
+
+checkOkSvg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -46 417.813 417" class="info-check-ok-svg">';
+checkOkSvg +='<path d="M159.988 318.582c-3.988 4.012-9.43 6.25-15.082 6.25s-11.094-2.238-15.082-6.25L9.375 198.113c-12.5-12.5-12.5-32.77 0-45.246l15.082-15.086c12.504-12.5 32.75-12.5 45.25 0l75.2 75.203L348.104 9.781c12.504-12.5 32.77-12.5 45.25 0l15.082 15.086c12.5 12.5 12.5 32.766 0 45.246zm0 0"/>';
+checkOkSvg +='</svg>';
+
+checkKoSvg = '<svg xmlns="http://www.w3.org/2000/svg" overflow="visible" x="620" y="239.5" viewBox="0 0 64 64" class="info-check-ko-svg">';
+checkKoSvg +='<g class="layer" pointer-events="all"><path d="M45.413 32l12.914-13.593c3.565-3.753 3.565-9.84 0-13.593-3.566-3.753-9.348-3.753-12.914 0L32.5 18.407 19.587 4.814c-3.566-3.753-9.348-3.753-12.914 0s-3.565 9.84 0 13.593L19.587 32 6.673 45.593c-3.565 3.753-3.565 9.84 0 13.593 3.566 3.753 9.348 3.753 12.914 0L32.5 45.593l12.913 13.593c3.566 3.753 9.348 3.753 12.914 0 3.565-3.753 3.565-9.84 0-13.593L45.413 32z"/></g>';
+checkKoSvg +='</svg>';
+
 // Nettoyage de formulaire
 $('#registration_form_courriel').val('');
 $('#registration_form_plainPassword').val('');
@@ -56,6 +66,77 @@ $('#registration_form_plainPassword').on('keyup', function () {
     $('label[for="registration_form_plainPassword"]').removeClass('show');
   }
 });
+
+$('#registration_form_courriel').on('keyup', function()
+  {
+    /**
+     * La longueur d'un mail est de 64+1+255
+     * On verifie la plus part des erreurs de saisie avant d'envoyer au contrôleur
+     * qui vérifira la conformité de l'adresse de courriel.
+     */
+
+    let domaine;
+    const courriel=$('#registration_form_courriel').val();
+    const arobase=courriel.indexOf('@');
+    const point=courriel.indexOf('.');
+
+    // Si le champ est vide
+    if ( courriel.length === 0 ) {
+      $('#register-info-check-courriel').html('');
+      return;
+    }
+
+    // Si le permier caractère est @
+    if (courriel.length === 1 && arobase ===0 ) {
+      $('#register-info-check-courriel').html(checkKoSvg);
+      return;
+    }
+
+    // Si on à un @  à la 65eme position
+    if (courriel.length > 64 && arobase >0 ){
+      $('#register-info-check-courriel').html(checkKoSvg);
+      return;
+    }
+
+    if (courriel.length < 64 && arobase <0 ){
+      $('#register-info-check-courriel').html(checkKoSvg);
+      return;
+    }
+
+    if (courriel.length < 64 && arobase >0 ) {
+      domaine=courriel.split('@');
+      $('#register-info-check-courriel').html(checkOkSvg);
+    }
+
+    // si le domaine n'est pas correcte
+    if (domaine[1].length==1 && point>1) {
+      $('#register-info-check-courriel').html(checkKoSvg);
+    }
+
+    // On verifie que le domaine est <256
+    if (domaine[1].length >255) {
+      $('#register-info-check-courriel').html(checkKoSvg);
+    }
+});
+
+// Vérification du mot de passe
+$('#registration_form_plainPassword').on('keyup', function()
+  {
+    const password=$('#registration_form_plainPassword').val();
+
+    if ( password.length === 0 ) { $('#register-info-check').html(''); }
+    if ( password.length>0 && password.length<8 ) {
+        $('#register-info-check-password').html('');
+        $('#register-info-check').html('<span class="register-info-erreur">Taille Min 8.</span>'); }
+    if ( password.length>53 ) {
+        $('#register-info-check').html('<span class="profil-info-erreur">Taille Max 52.</span>'); }
+    if ( password.length>7 && password.length<53 ) {
+        $('#register-info-check').html('');
+        $('#register-info-check-password').html(checkOkSvg);
+      }
+      else {
+        $('#register-info-check-password').html(checkKoSvg);} }
+  );
 
 // Validation du choix de l'avatar
 $('.thumbnail').on('click', function()
