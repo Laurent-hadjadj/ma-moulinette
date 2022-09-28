821cae8e629b46b221958d435826395360f29eb0
Laurent HADJADJ <laurent_h@me.com>
2022-09-28 11:56:04 +0200

    Ajout de la table properties

---
+++ src/Repository/Main/PropertiesRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository\Main;
+
+use App\Entity\Main\Properties;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Properties>
+ *
+ * @method Properties|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Properties|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Properties[]    findAll()
+ * @method Properties[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PropertiesRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Properties::class);
+    }
+
+    public function add(Properties $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Properties $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return Properties[] Returns an array of Properties objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('p.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Properties
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
