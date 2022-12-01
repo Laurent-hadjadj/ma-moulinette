fa76e313721efe5ade71bdd50b8f01204e1f0fb6
Laurent HADJADJ <laurent_h@me.com>
2022-12-01 17:05:04 +0100

    fix: Remplacement Equipes par Portefeuille

--- src/Repository/Main/PortefeuilleRepository.php
+++ src/Repository/Main/PortefeuilleRepository.php
@@ -2,26 +2,26 @@
 
 namespace App\Repository\Main;
 
-use App\Entity\Main\Equipes;
+use App\Entity\Main\Portefeuille;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<Equipes>
+ * @extends ServiceEntityRepository<Portefeuille>
  *
- * @method Equipes|null find($id, $lockMode = null, $lockVersion = null)
- * @method Equipes|null findOneBy(array $criteria, array $orderBy = null)
- * @method Equipes[]    findAll()
- * @method Equipes[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method Portefeuille|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Portefeuille|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Portefeuille[]    findAll()
+ * @method Portefeuille[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class PortefeuilleRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Equipes::class);
+        parent::__construct($registry, Portefeuille::class);
     }
 
-    public function save(Equipes $entity, bool $flush = false): void
+    public function save(Portefeuille $entity, bool $flush = false): void
     {
         $this->getEntityManager()->persist($entity);
 
@@ -30,7 +30,7 @@ class PortefeuilleRepository extends ServiceEntityRepository
         }
     }
 
-    public function remove(Equipes $entity, bool $flush = false): void
+    public function remove(Portefeuille $entity, bool $flush = false): void
     {
         $this->getEntityManager()->remove($entity);
 
@@ -39,28 +39,4 @@ class PortefeuilleRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Equipes[] Returns an array of Equipes objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('e')
-//            ->andWhere('e.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('e.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Equipes
-//    {
-//        return $this->createQueryBuilder('e')
-//            ->andWhere('e.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
