9093b7de34ab99b308638f62aedc1867f15153e2
Laurent HADJADJ <laurent_h@me.com>
2022-12-15 21:22:16 +0100

    refactor: Commentaires

--- src/Controller/ApiDetailsPeintureController.php
+++ src/Controller/ApiDetailsPeintureController.php
@@ -26,11 +26,17 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class ApiDetailsPeintureController extends AbstractController
 {
-  private $em;
-
-  public function __construct(
-    EntityManagerInterface $em,
-    )
+  /**
+   * [Description for __construct]
+   *
+   * @param  private
+   * @param mixed
+   *
+   * Created at: 15/12/2022, 21:09:53 (Europe/Paris)
+   * @author     Laurent HADJADJ <laurent_h@me.com>
+   * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+   */
+  public function __construct(private EntityManagerInterface $em)
   {
     $this->em = $em;
   }
@@ -46,7 +52,7 @@ class ApiDetailsPeintureController extends AbstractController
    */
   protected function isValide($mavenKey): array
   {
-    // On regarde si une analyse a été réalisée.
+    /** On regarde si une analyse a été réalisée. */
     $sql = "SELECT * FROM information_projet WHERE maven_key='" . $mavenKey . "' LIMIT 1";
     $r = $this->em->getConnection()->prepare($sql)->executeQuery();
     $response = $r->fetchAllAssociative();
@@ -58,11 +64,17 @@ class ApiDetailsPeintureController extends AbstractController
 
 
   /**
+   * [Description for peintureProjetAnomalieDetails]
    * peinture_projet_anomalie_details
    * Récupère les informations sur la dette technique et les anamalies
    *
-   * @param  mixed $request
+   * @param Request $request
+   *
    * @return response
+   *
+   * Created at: 15/12/2022, 21:11:15 (Europe/Paris)
+   * @author     Laurent HADJADJ <laurent_h@me.com>
+   * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
    */
   #[Route('/api/peinture/projet/anomalie/details', name: 'peinture_projet_anomalie_details', methods: ['GET'])]
   public function peintureProjetAnomalieDetails(Request $request): response
@@ -75,7 +87,7 @@ class ApiDetailsPeintureController extends AbstractController
       return $response->setData(["message" => static::HTTP_ERROR_406, Response::HTTP_NOT_ACCEPTABLE]);
     }
 
-    // On récupère la dernière version et sa date de publication
+    /** On récupère la dernière version et sa date de publication */
     $sql = "SELECT * FROM anomalie WHERE maven_key='" . $mavenKey . "' ORDER BY date_enregistrement DESC LIMIT 1";
     $r = $this->em->getConnection()->prepare($sql)->executeQuery();
     $anomalies = $r->fetchAllAssociative();
