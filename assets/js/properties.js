2ae8dde198e42a0b1c59aa19294ff97e7f8f8ce1
Laurent HADJADJ <laurent_h@me.com>
2023-01-04 08:41:42 +0100

    Fix : ajout du header de copyright

--- assets/js/properties.js
+++ assets/js/properties.js
@@ -1,4 +1,15 @@
 /**
+ *  Ma-Moulinette
+ *  --------------
+ *  Copyright (c) 2021-2022.
+ *  Laurent HADJADJ <laurent_h@me.com>.
+ *  Licensed Creative Common  CC-BY-NC-SA 4.0.
+ *  ---
+ *  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+ *  http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+/**
  * On renvoie l'adresse du serveur ou est installé l'application !
  * L'adresse doit être modifiée en fonction de la ou est installé l'application.
  * */
