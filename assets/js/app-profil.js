be93170d8c1a840c721d108b2decd5a9dedf28b2
Laurent HADJADJ <laurent_h@me.com>
2023-01-04 10:40:13 +0100

    Fix :  Sonarlint(javascript:S109)

--- assets/js/app-profil.js
+++ assets/js/app-profil.js
@@ -29,14 +29,9 @@ import zoomPlugin from 'chartjs-plugin-zoom';
 Chart.register(ChartDataLabels);
 Chart.register(zoomPlugin);
 
-const contentType = 'application/json; charset=utf-8';
-const matrice = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-  16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-const paletteCouleur = [
-  '#065535', '#133337', '#000000', '#ffc0cb', '#008080', '#ff0000', '#ffd700', '#666666',
-  '#ff7373', '#fa8072', '#800080', '#800000', '#003366', '#333333', '#20b2aa', '#ffc3a0',
-  '#f08080', '#66cdaa', '#f6546a', '#ff6666', '#468499', '#c39797', '#bada55', '#ff7f50',
-  '#660066', '#008000', '#088da5', '#808080', '#8b0000', '#0e2f44', '#3b5998', '#cc0000' ];
+/** On importe les constantes */
+import {contentType, paletteCouleur, matrice,
+        dateOptions, dateOptionsShort} from './constante.js';
 
 /**
  * [Description for shuffle]
@@ -78,9 +73,6 @@ const palette=function() {
   return nouvellePalette;
 };
 
-const dateOptions1 = {year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false };
-const dateOptions2 = {year: 'numeric', month: 'numeric', day: 'numeric' };
-
 /**
  * [Description for refreshQuality]
  * Met à jour la liste des référentiels
@@ -119,9 +111,9 @@ const refreshQuality=function() {
                     ${new Intl.NumberFormat('fr-FR', { style: 'decimal' }).format(profil.regle)}</td>
                 <td class="text-center">
                     <span class="show-for-small-only">
-                      ${new Intl.DateTimeFormat('default', dateOptions2).format(new Date(profil.date))}
+                      ${new Intl.DateTimeFormat('default', dateOptionsShort).format(new Date(profil.date))}
                     </span>
-                    <span class="show-for-medium">${new Intl.DateTimeFormat('default', dateOptions1).format(new Date(profil.date))}
+                    <span class="show-for-medium">${new Intl.DateTimeFormat('default', dateOptions).format(new Date(profil.date))}
                     </span>
                 </td>
                 <td class="text-center">
