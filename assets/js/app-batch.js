b8843930941b38e642535c0a294608952c13f9af
Laurent HADJADJ <laurent_h@me.com>
2023-01-12 14:29:55 +0100

    Ajout bouton automatique/manuel

--- assets/js/app-batch.js
+++ assets/js/app-batch.js
@@ -12,8 +12,38 @@
 /** Import des dépendances */
 import '../css/batch.css';
 
+/** Intégration de jquery */
+import $ from 'jquery';
+
 import 'what-input';
 import 'foundation-sites';
 import 'motion-ui';
 
 import './foundation.js';
+
+/** On importe les constatntes */
+import { mille } from './constante';
+
+/** On gére l'affichage des jobs */
+const jsAutomatique = '.js-automatique';
+const jsManuel = '.js-manuel';
+
+$(jsAutomatique).on('click', function() {
+  if ($(jsAutomatique).hasClass('active')) {
+    $('.js-automatique').removeClass('active').addClass('bouton-automatique');
+    $('.automatique').hide(mille);
+  } else {
+    $(jsAutomatique).removeClass('bouton-automatique').addClass('active');
+    $('.automatique').show(mille);
+  }
+});
+
+$(jsManuel).on('click', function() {
+  if ($(jsManuel).hasClass('active')) {
+    $(jsManuel).removeClass('active').addClass('bouton-manuel');
+    $('.manuel').hide(mille);
+  } else {
+    $(jsManuel).removeClass('bouton-manuel').addClass('active');
+    $('.manuel').show(mille);
+  }
+});
