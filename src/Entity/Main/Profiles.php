169934520bf286604515c29043c0a585da2f6793
Laurent HADJADJ <laurent_h@me.com>
2023-02-14 10:51:18 +0100

    Fix: ajout setId()

--- src/Entity/Main/Profiles.php
+++ src/Entity/Main/Profiles.php
@@ -286,4 +286,16 @@ class Profiles
         return $this;
     }
 
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
 }
