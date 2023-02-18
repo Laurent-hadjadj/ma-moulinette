e3104680831977e8a95f70742e24cc85cbd8c255
Laurent HADJADJ <laurent_h@me.com>
2023-02-18 20:10:18 +0100

    Ajout des TU,  API ProjetRepartitionController

---
+++ tests/Api/ProjetRepartitionControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Tests\Api;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use App\Repository\Main\UtilisateurRepository;
+use App\Controller\ApiProfilController;
+
+class ProjetRepartitionControllerTest extends ApiTestCase
+{
+  private static $userTest='admin@ma-moulinette.fr';
+  private static $mavenKey='mavenKey=fr.franceagrimer:ma-moulinette';
+
+  /**
+   * [Description for testProjetRepartition200]
+   * On test une vue, oui monsieur !!!
+   * @return void
+   *
+   * Created at: 15/02/2023, 22:05:52 (Europe/Paris)
+   * @author     Laurent HADJADJ <laurent_h@me.com>
+   * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+   */
+  public function testProjetRepartition200(): void
+  {
+    $client = static::createClient();
+    $userRepository = static::getContainer()->get(UtilisateurRepository::class);
+    $testUser = $userRepository->findOneByCourriel(static::$userTest);
+    $client->loginUser($testUser);
+    $client->request('GET', '/projet/repartition?'.static::$mavenKey.'&mode=TEST');
+    $response = $client->getResponse()->getContent();
+    $decode=json_decode($response, true, 512, JSON_THROW_ON_ERROR);
+    $this->assertResponseIsSuccessful();
+    $this->assertResponseHeaderSame('content-type', 'application/json');
+    $this->assertIsArray($decode);
+    $this->assertEquals(5,count($decode));
+    $this->assertEquals('200', $decode[0]);
+    $this->assertArrayHasKey('monApplication', $decode);
+    $this->assertArrayHasKey('mavenKey', $decode);
+    $this->assertArrayHasKey('setup', $decode);
+    $this->assertArrayHasKey('statut', $decode);
+  }
+
+}
