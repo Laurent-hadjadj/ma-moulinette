e5bca72c4b2423ad1a27a6edba4395f42c5d2245
Laurent HADJADJ <laurent_h@me.com>
2023-02-20 15:39:47 +0100

    Fix: Ciphers

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -92,12 +92,11 @@ class HomeController extends AbstractController
 
         $response = $this->client->request('GET', $url,
         [
-            'ciphers' => `AES128-SHA  DH-RSA-AES128-SHA
-            DH-RSA-AES256-SHA DHE-DSS-AES128-SHA DHE-DSS-AES256-SHA DHE-RSA-AES128-SHA
-            DHE-RSA-AES256-SHA ADH-AES128-SHA ADH-AES256-SHA`,
-            'auth_basic' => [$user, $password], 'timeout' => 45,
-            'headers' => ['Accept' => static::$strContentType,
-            'Content-Type' => static::$strContentType]
+          'ciphers' => `DH-RSA-AES256-SHA DHE-DSS-AES128-SHA DHE-DSS-AES256-SHA
+          DHE-RSA-AES128-SHADHE-RSA-AES256-SHA ADH-AES128-SHA ADH-AES256-SHA`,
+          'auth_basic' => [$user, $password], 'timeout' => 45,
+          'headers' => ['Accept' => static::$strContentType,
+          'Content-Type' => static::$strContentType]
         ]
         );
 
