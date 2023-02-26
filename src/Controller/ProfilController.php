0201d3e61e6b60b963f8af4e9ffbd2bd64b9af5d
Laurent HADJADJ <laurent_h@me.com>
2023-02-26 20:20:25 +0100

    Fix: Prise en compte des TU

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -16,9 +16,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 // Accès aux tables SLQLite
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Response;
 
 class ProfilController extends AbstractController
 {
@@ -42,12 +45,22 @@ class ProfilController extends AbstractController
    * @return Response
    *
    * Created at: 15/12/2022, 22:14:55 (Europe/Paris)
-   * @author     Laurent HADJADJ <laurent_h@me.com>
+   * @author    Laurent HADJADJ <laurent_h@me.com>
    * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
    */
-  #[Route('/profil', name: 'profil')]
-  public function index(): Response
+  #[Route('/profil', name: 'profil', methods: 'GET')]
+  public function index(Request $request): Response
   {
+    /** oN créé un objet réponse */
+    $response = new JsonResponse();
+
+    /** On on vérifie si on a activé le mode test */
+    if (is_null($request->get('mode'))) {
+      $mode="null";
+    } else {
+      $mode = $request->get('mode');
+    }
+
     /** On récupère la liste des profiles; */
     $sql = "SELECT name as profil, language_name as langage,
       active_rule_count as regle, rules_update_at as date, is_default as actif
@@ -56,11 +69,17 @@ class ProfilController extends AbstractController
     $select = $this->em->getConnection()->prepare(trim(preg_replace("/\s+/u", " ", $sql)))->executeQuery();
     $liste = $select->fetchAllAssociative();
 
-    return $this->render('profil/index.html.twig',
-      [
-        "liste" => $liste,
-        "version" => $this->getParameter("version"), "dateCopyright" => \date("Y")
-      ]
-    );
+    $render =
+      [ "mode"=>$mode, "liste" => $liste,
+        "version" => $this->getParameter("version"),
+        "dateCopyright" => \date("Y"),
+        Response::HTTP_OK
+      ];
+
+    if ($mode=="TEST") {
+      return $response->setData($render);
+    } else {
+      return $this->render('profil/index.html.twig', $render);
+    }
   }
 }
