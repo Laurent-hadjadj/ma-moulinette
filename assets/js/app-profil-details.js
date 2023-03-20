42c6d0c43d3016dd96a25164062a5e3fb57fb751
Laurent HADJADJ <laurent_h@me.com>
2023-03-20 13:11:20 +0100

    Ajout de la page profil-details

---
+++ assets/js/app-profil-details.js
@@ -0,0 +1,18 @@
+/**
+ *  Ma-Moulinette
+ *  --------------
+ *  Copyright (c) 2021-2022.
+ *  Laurent HADJADJ <laurent_h@me.com>.
+ *  Licensed Creative Common  CC-BY-NC-SA 4.0.
+ *  ---
+ *  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+ *  http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+import '../css/profil-details.css';
+
+import 'what-input';
+import 'foundation-sites';
+import 'motion-ui';
+
+import './foundation.js';
