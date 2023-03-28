7b1ddf99bddc553a3c01ca226d5be4184b51d0ee
Laurent HADJADJ <laurent_h@me.com>
2023-03-28 09:19:14 +0200

    Mise à jour des utilisateurs de tests

--- migrations/fixtures-postinstall.sql
+++ migrations/fixtures-postinstall.sql
@@ -9,8 +9,8 @@
  * Vous pouvez obtenir une copie de la licence à l'adresse suivante :
  * http://creativecommons.org/licenses/by-nc-sa/4.0/
  * ----------------------------------------------------------------------
- * Last Modified: Fri Dec 02 2022
- * Modified By: Laurent HADJADJ
+ * Last Modified: Tue Feb 28 2023
+ * Modified By: HADJADJ Laurent
  * ----------------------------------------------------------------------
  * HISTORY:
  * Date      	By	Comments
@@ -33,7 +33,9 @@ INSERT INTO ma_moulinette (version, date_version, date_enregistrement) VALUES ('
 INSERT INTO ma_moulinette (version, date_version, date_enregistrement) VALUES ('1.5.0-RC1', '2022-10-06', date('now'));
 INSERT INTO ma_moulinette (version, date_version, date_enregistrement) VALUES ('1.5.0', '2022-10-12', date('now'));
 -- ## Ajout de la version 1.6.0 dans la table ma_moulinette
+INSERT INTO ma_moulinette (version, date_version, date_enregistrement) VALUES ('2.0.0-RC1', '2022-12-13', date('now'));
 INSERT INTO ma_moulinette (version, date_version, date_enregistrement) VALUES ('2.0.0', '2023-01-01', date('now'));
+
 
 -- ## Ajout du compte admin
 INSERT INTO utilisateur
@@ -48,5 +50,34 @@ VALUES
 1,
 'chiffre/01.png',
 '');
+
+-- ## Ajout des comptes de tests
+INSERT INTO utilisateur
+(courriel, roles, password, prenom, nom, date_enregistrement, actif, avatar, equipe)
+VALUES
+('aurelie.petit-coeur@ma-moulinette.fr','["ROLE_GESTIONNAIRE"]',
+'$2y$13$O81lc.HQTtb5oJSEt.8PLuvlMgOGTgGKiLYdmn/xMP3j4Kq2yIA52',
+'Aurélie', 'PETIT-COEUR','1980-01-01 00:00:00', 0,'fille-1/05.png',
+'');
+
+INSERT INTO utilisateur
+(courriel, roles, password, prenom, nom, date_enregistrement, actif, avatar, equipe)
+VALUES
+('josh.liberman@ma-moulinette.fr','["ROLE_UTILISATEUR"]',
+'$2y$13$Xkrgtpq3bejvCdfd7qLvhujDT9XpgbxRLGEA0FoOfZGPmgW0ADBpS',
+'Josh', 'LIBERMAN','1980-01-01 00:00:00', 0,'garcon-2/08.png',
+'');
+
+INSERT INTO utilisateur
+(courriel, roles, password, prenom, nom, date_enregistrement, actif, avatar, equipe)
+VALUES
+('emma.van-de-berg@ma-moulinette.fr','["ROLE_BATCH"]',
+'$2y$13$JIieJW5yYYe7NHeYcjzS0OpGNkZ5Lc1klrQZL3K0ZN4AUieasKpiG',
+'Emma', 'VAN DE BERG','1980-01-01 00:00:00', 0,'fille-2/03.png',
+'');
+
+-- ## Ajout de l'équipe par défaut
+INSERT INTO utilisateur (titre, description, date_enregistrement, )
+VALUES ('AUCUNE', 'Personne ne m''aime !', '1980-01-01 00:00:00');
 
 COMMIT;
