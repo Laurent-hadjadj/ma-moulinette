d037d9fdddbc93a0d4e4af4ae770d4c58c1c3c49
Laurent HADJADJ <laurent_h@me.com>
2023-03-28 10:55:48 +0200

    AJout du scrupt JS pour mes preferences

---
+++ assets/js/app-preference.js
@@ -0,0 +1,22 @@
+/**
+ *  Ma-Moulinette
+ *  --------------
+ *  Copyright (c) 2021-2022.
+ *  Laurent HADJADJ <laurent_h@me.com>.
+ *  Licensed Creative Common  CC-BY-NC-SA 4.0.
+ *  ---
+ *  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+ *  http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+/** Import des dépendances */
+import '../css/preference.css';
+
+/** Intégration de jquery */
+//import $ from 'jquery';
+
+import 'what-input';
+import 'foundation-sites';
+import 'motion-ui';
+
+import './foundation.js';
