8d82d5fda57872d101bd3a0b080e8fd7eed660a0
Laurent HADJADJ <laurent_h@me.com>
2023-04-10 17:09:04 +0200

    Ajout de la class d'entités Todo

---
+++ src/Entity/Main/Todo.php
@@ -0,0 +1,239 @@
+<?php
+
+/*
+ *  Ma-Moulinette
+ *  --------------
+ *  Copyright (c) 2021-2022.
+ *  Laurent HADJADJ <laurent_h@me.com>.
+ *  Licensed Creative Common  CC-BY-NC-SA 4.0.
+ *  ---
+ *  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+ *  http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+namespace App\Entity\Main;
+
+use App\Repository\Main\TodoRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TodoRepository::class)]
+class Todo
+
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 128)]
+    private $mavenKey;
+
+    #[ORM\Column(type: 'string', length: 128)]
+    private $rule;
+
+    #[ORM\Column(type: 'text')]
+    private $component;
+
+    #[ORM\Column(type: 'integer')]
+    private $line;
+
+    #[ORM\Column(type: 'datetime')]
+    private $dateEnregistrement;
+
+    /**
+     * [Description for getId]
+     *
+     * @return int|null
+     *
+     * Created at: 10/04/2023, 14:47:31 (Europe/Paris)
+     * @author    Laurent HADJADJ <laurent_h@me.com>
+     * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * [Description for getMavenKey]
+     *
+     * @return string|null
+     *
+     * Created at: 10/04/2023, 14:47:31 (Europe/Paris)
+     * @author    Laurent HADJADJ <laurent_h@me.com>
+     * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function getMavenKey(): ?string
+    {
+        return $this->mavenKey;
+    }
+
+    /**
+     * [Description for setMavenKey]
+     *
+     * @param string $mavenKey
+     *
+     * @return self
+     *
+     * Created at: 10/04/2023, 14:47:31 (Europe/Paris)
+     * @author    Laurent HADJADJ <laurent_h@me.com>
+     * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function setMavenKey(string $mavenKey): self
+    {
+        $this->mavenKey = $mavenKey;
+
+        return $this;
+    }
+
+
+    /**
+     * [Description for getRule]
+     *
+     * @return string|null
+     *
+     * Created at: 10/04/2023, 14:48:19 (Europe/Paris)
+     * @author    Laurent HADJADJ <laurent_h@me.com>
+     * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function getRule(): ?string
+    {
+        return $this->rule;
+    }
+
+    /**
+     * [Description for setRule]
+     *
+     * @param string $rule
+     *
+     * @return self
+     *
+     * Created at: 10/04/2023, 14:47:31 (Europe/Paris)
+     * @author    Laurent HADJADJ <laurent_h@me.com>
+     * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function setRule(string $rule): self
+    {
+        $this->rule = $rule;
+
+        return $this;
+    }
+
+
+    /**
+     * [Description for getComponent]
+     *
+     * @return string|null
+     *
+     * Created at: 10/04/2023, 14:48:41 (Europe/Paris)
+     * @author    Laurent HADJADJ <laurent_h@me.com>
+     * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function getComponent(): ?string
+    {
+        return $this->component;
+    }
+
+    /**
+     * [Description for setComponent]
+     *
+     * @param string $component
+     *
+     * @return self
+     *
+     * Created at: 10/04/2023, 14:48:41 (Europe/Paris)
+     * @author    Laurent HADJADJ <laurent_h@me.com>
+     * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function setComponent(string $component): self
+    {
+        $this->component = $component;
+
+        return $this;
+    }
+
+
+    /**
+     * [Description for getLine]
+     *
+     * @return int|null
+     *
+     * Created at: 10/04/2023, 14:49:00 (Europe/Paris)
+     * @author    Laurent HADJADJ <laurent_h@me.com>
+     * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function getLine(): ?int
+    {
+        return $this->line;
+    }
+
+    /**
+     * [Description for setLine]
+     *
+     * @param int $line
+     *
+     * @return self
+     *
+     * Created at: 10/04/2023, 14:49:00 (Europe/Paris)
+     * @author    Laurent HADJADJ <laurent_h@me.com>
+     * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function setLine(int $line): self
+    {
+        $this->line = $line;
+
+        return $this;
+    }
+
+    /**
+     * [Description for getDateEnregistrement]
+     *
+     * @return \DateTimeInterface|null
+     *
+     * Created at: 02/01/2023, 18:04:34 (Europe/Paris)
+     * @author    Laurent HADJADJ <laurent_h@me.com>
+     * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function getDateEnregistrement(): ?\DateTimeInterface
+    {
+        return $this->dateEnregistrement;
+    }
+
+
+    /**
+     * [Description for setDateEnregistrement]
+     *
+     * @param \DateTimeInterface $dateEnregistrement
+     *
+     * @return self
+     *
+     * Created at: 10/04/2023, 14:49:45 (Europe/Paris)
+     * @author    Laurent HADJADJ <laurent_h@me.com>
+     * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function setDateEnregistrement(\DateTimeInterface $dateEnregistrement): self
+    {
+        $this->dateEnregistrement = $dateEnregistrement;
+
+        return $this;
+    }
+
+    /**
+     * [Description for setId]
+     *
+     * @param mixed $id
+     *
+     * @return [type]
+     *
+     * Created at: 10/04/2023, 14:50:05 (Europe/Paris)
+     * @author    Laurent HADJADJ <laurent_h@me.com>
+     * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+}
