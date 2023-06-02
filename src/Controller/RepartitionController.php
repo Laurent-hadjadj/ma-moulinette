e2a97e0708f0237bb3991a6bbb6c083d6cfdd252
Laurent HADJADJ <laurent_h@me.com>
2023-06-02 10:51:49 +0200

    Fix: Indentation

--- src/Controller/RepartitionController.php
+++ src/Controller/RepartitionController.php
@@ -50,7 +50,7 @@ class RepartitionController extends AbstractController
      * @return Response
      *
      * Created at: 15/12/2022, 22:32:12 (Europe/Paris)
-     * @author     Laurent HADJADJ <laurent_h@me.com>
+     * @author    Laurent HADJADJ <laurent_h@me.com>
      * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
      */
     #[Route('/projet/repartition', name: 'projet_repartition')]
