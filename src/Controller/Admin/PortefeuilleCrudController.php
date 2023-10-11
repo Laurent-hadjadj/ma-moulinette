7ee524e5b6640a8dbfb1682ccbe31d6fedd9f25c
Laurent HADJADJ <laurent_h@me.com>
2023-10-11 14:33:18 +0200

    Fix: Suppression de @param private du constructeur.

--- src/Controller/Admin/PortefeuilleCrudController.php
+++ src/Controller/Admin/PortefeuilleCrudController.php
@@ -32,8 +32,6 @@ class PortefeuilleCrudController extends AbstractCrudController
     /**
      * [Description for __construct]
      *
-     * @param  private
-     *
      * Created at: 02/01/2023, 18:35:59 (Europe/Paris)
      * @author     Laurent HADJADJ <laurent_h@me.com>
      * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
