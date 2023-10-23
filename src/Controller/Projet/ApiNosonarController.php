cc122b8532662fc3ed76da57d912877545d21af9
Laurent HADJADJ <laurent_h@me.com>
2023-10-23 14:40:12 +0200

    Refactoring de la casse ApiProjetController

---
+++ src/Controller/Projet/ApiNosonarController.php
@@ -0,0 +1,242 @@
+<?php
+
+/*
+ *  Ma-Moulinette
+ *  --------------
+ *  Copyright (c) 2021-2022.
+ *  Laurent HADJADJ <laurent_h@me.com>.
+ *  Licensed Creative Common  CC-BY-NC-SA 4.0.
+ *  ---
+ *  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+ *  http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+namespace App\Controller\Projet;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/** Gestion de accès aux API */
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/** Gestion du temps */
+use DateTime;
+use DateTimeZone;
+
+// Accès aux tables SLQLite
+use App\Entity\Main\NoSonar;
+use App\Entity\Main\Todo;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+/** Logger */
+use Psr\Log\LoggerInterface;
+
+/** Client HTTP */
+use App\Service\Client;
+
+class ApiNosonarController extends AbstractController
+{
+
+  /** Définition des constantes */
+  public static $sonarUrl = "sonar.url";
+  public static $europeParis = "Europe/Paris";
+  public static $regex = "/\s+/u";
+  public static $erreurMavenKey="La clé maven est vide!";
+  public static $reference="<strong>[PROJET-002]</strong>";
+  public static $message="Vous devez avoir le rôle COLLECTE pour réaliser cette action.";
+
+  /**
+   * [Description for __construct]
+   *
+   * Created at: 15/12/2022, 21:25:23 (Europe/Paris)
+   * @author    Laurent HADJADJ <laurent_h@me.com>
+   * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+   */
+  public function __construct(
+    private LoggerInterface $logger,
+    private EntityManagerInterface $em)
+  {
+    $this->logger = $logger;
+    $this->em = $em;
+  }
+
+  /**
+   * [Description for projetNosonarAjout]
+   * On récupère la liste des fichiers ayant fait l'objet d'un
+   * @@supresswarning ou d'un noSONAR
+   * http://{url}api/issues/search?componentKeys={key}&rules={rules}&ps=500&p=1
+   * {key} = la clé du projet
+   * {rules} = java:S1309 et java:NoSonar
+   *
+   * @param Request $request
+   *
+   * @return response
+   *
+   * Created at: 15/12/2022, 21:42:59 (Europe/Paris)
+   * @author    Laurent HADJADJ <laurent_h@me.com>
+   * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+   */
+  #[Route('/api/projet/nosonar/details', name: 'projet_nosonar', methods: ['GET'])]
+  public function projetNosonarAjout(Client $client, Request $request): response
+  {
+    /** On créé un objet response */
+    $response = new JsonResponse();
+
+    /** On bind les variables. */
+    $mode = $request->get('mode');
+    $mavenKey = $request->get('mavenKey');
+    $tempoUrl = $this->getParameter(static::$sonarUrl);
+
+    /** On teste si la clé est valide */
+    if ($mavenKey==="null" && $mode==="TEST") {
+      return $response->setData([
+        "mode"=>$mode, "mavenKey"=>$mavenKey,
+        "message" => static::$erreurMavenKey, Response::HTTP_BAD_REQUEST]);
+    }
+
+    /** On vérifie si l'utilisateur à un rôle Collecte ? */
+    if (!$this->isGranted('ROLE_COLLECTE')){
+      return $response->setData([
+        "mode"=>$mode ,
+        "type"=>'alert',
+        "reference" => static::$reference,
+        "message"=> static::$message,
+        Response::HTTP_OK]);
+    }
+
+    /** On construit l'URL et on appel le WS. */
+    $url = "$tempoUrl/api/issues/search?componentKeys=$mavenKey
+            &rules=java:S1309,java:NoSonar&ps=500&p=1";
+
+    $result = $client->http(trim(preg_replace(static::$regex, " ", $url)));
+    $date = new DateTime();
+    $date->setTimezone(new DateTimeZone(static::$europeParis));
+
+    /** On supprime les données du projet de la table NoSonar. */
+    $sql = "DELETE FROM no_sonar WHERE maven_key='$mavenKey'";
+    if($mode!=="TEST"){
+      $this->em->getConnection()->prepare($sql)->executeQuery();
+    }
+
+    /**
+     * Si on a trouvé des @notations de type nosSonar ou SuprressWarning.
+     * dans le code alors on les dénombre
+     */
+    if ($result["paging"]["total"] !== 0) {
+      foreach ($result["issues"] as $issue) {
+        $nosonar = new NoSonar();
+        $nosonar->setMavenKey($request->get('mavenKey'));
+        $nosonar->setRule($issue["rule"]);
+        $component = str_replace("$mavenKey :", "", $issue["component"]);
+        $nosonar->setComponent($component);
+        if (empty($issue["line"])) {
+          $line = 0;
+        } else {
+          $line = $issue["line"];
+        }
+        $nosonar->setLine($line);
+        $nosonar->setDateEnregistrement($date);
+
+        $this->em->persist($nosonar);
+        if ($mode!="TEST") {
+          $this->em->flush();
+        }
+      }
+    } else {
+      /** Il n'y a pas de noSOnar ou de suppressWarning */
+    }
+
+    return $response->setData(["mode"=>$mode,"nosonar" => $result["paging"]["total"], Response::HTTP_OK]);
+  }
+
+  /**
+   * [Description for projetNosonarAjout]
+   * On récupère la liste des fichiers ayant fait l'objet d'un "To do"
+   * http://{url}api/issues/search?componentKeys={key}&rules={rules}&ps=500&p=1
+   * {key} = la clé du projet
+   * {rules} = javascript:S1135, xml:S1135, typescript:S1135, Web:S1135, java:S1135
+   *
+   * @param Request $request
+   *
+   * @return response
+   *
+   * Created at: 10/04/2023, 15:18:45 (Europe/Paris)
+   * @author    Laurent HADJADJ <laurent_h@me.com>
+   * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+   */
+  #[Route('/api/projet/todo/details', name: 'projet_todo', methods: ['GET'])]
+  public function projetTodoAjout(Client $client, Request $request): response
+  {
+    /** On créé un objet response */
+    $response = new JsonResponse();
+
+    /** On bind les variables. */
+    $mode = $request->get('mode');
+    $mavenKey = $request->get('mavenKey');
+    $tempoUrl = $this->getParameter(static::$sonarUrl);
+
+    /** On teste si la clé est valide */
+    if ($mavenKey==="null" && $mode==="TEST") {
+      return $response->setData([
+        "mode"=>$mode, "mavenKey"=>$mavenKey,
+        "message" => static::$erreurMavenKey, Response::HTTP_BAD_REQUEST]);
+    }
+
+    /** On vérifie si l'utilisateur à un rôle Collecte ? */
+    if (!$this->isGranted('ROLE_COLLECTE')){
+      return $response->setData([
+        "mode"=>$mode ,
+        "type"=>'alert',
+        "reference" => static::$reference,
+        "message"=> static::$message,
+        Response::HTTP_OK]);
+    }
+
+    /** On construit l'URL et on appel le WS. */
+    $url = "$tempoUrl/api/issues/search?componentKeys=$mavenKey
+            &rules=javascript:S1135,xml:S1135,typescript:S1135,Web:S1135,java:S1135&ps=500&p=1";
+
+    $result = $client->http(trim(preg_replace(static::$regex, " ", $url)));
+    $date = new DateTime();
+    $date->setTimezone(new DateTimeZone(static::$europeParis));
+
+    /** On supprime les données du projet de la table to do. */
+    $sql = "DELETE FROM todo WHERE maven_key='$mavenKey'";
+    if($mode!=="TEST"){
+      $this->em->getConnection()->prepare($sql)->executeQuery();
+    }
+
+    /**
+     * Si on a trouvé des to do dans le code alors on les dénombre
+     */
+    if ($result["paging"]["total"] !== 0) {
+      foreach ($result["issues"] as $issue) {
+        $nosonar = new Todo();
+        $nosonar->setMavenKey($request->get('mavenKey'));
+        $nosonar->setRule($issue["rule"]);
+        $component = str_replace("$mavenKey :", "", $issue["component"]);
+        $nosonar->setComponent($component);
+        if (empty($issue["line"])) {
+          $line = 0;
+        } else {
+          $line = $issue["line"];
+        }
+        $nosonar->setLine($line);
+        $nosonar->setDateEnregistrement($date);
+
+        $this->em->persist($nosonar);
+        if ($mode!="TEST") {
+          $this->em->flush();
+        }
+      }
+    } else {
+      /** Il n'y a pas de to do */
+    }
+
+    return $response->setData(["mode"=>$mode,"todo" => $result["paging"]["total"], Response::HTTP_OK]);
+  }
+
+}
---
+++ src/Controller/Projet/ApiOwaspController.php
@@ -0,0 +1,454 @@
+<?php
+
+/*
+ *  Ma-Moulinette
+ *  --------------
+ *  Copyright (c) 2021-2022.
+ *  Laurent HADJADJ <laurent_h@me.com>.
+ *  Licensed Creative Common  CC-BY-NC-SA 4.0.
+ *  ---
+ *  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+ *  http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+namespace App\Controller\Projet;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/** Gestion de accès aux API */
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/** Gestion du temps */
+use DateTime;
+use DateTimeZone;
+
+// Accès aux tables SLQLite
+use App\Entity\Main\Owasp;
+
+use Doctrine\ORM\EntityManagerInterface;
+
+/** Logger */
+use Psr\Log\LoggerInterface;
+
+/** Client HTTP */
+use App\Service\Client;
+
+/**
+ * [Description ApiOwaspController]
+ */
+class ApiOwaspController extends AbstractController
+{
+
+  /** Définition des constantes */
+  public static $sonarUrl = "sonar.url";
+  public static $europeParis = "Europe/Paris";
+  public static $apiIssuesSearch = "/api/issues/search?componentKeys=";
+  public static $regex = "/\s+/u";
+  public static $erreurMavenKey="La clé maven est vide!";
+  public static $reference="<strong>[PROJET-002]</strong>";
+  public static $message="Vous devez avoir le rôle COLLECTE pour réaliser cette action.";
+
+  /**
+   * [Description for __construct]
+   *
+   * Created at: 15/12/2022, 21:25:23 (Europe/Paris)
+   * @author    Laurent HADJADJ <laurent_h@me.com>
+   * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+   */
+  public function __construct(
+    private LoggerInterface $logger,
+    private EntityManagerInterface $em)
+  {
+    $this->logger = $logger;
+    $this->em = $em;
+  }
+
+  /**
+   * [Description for issuesOwasp]
+   * Récupère le top 10 OWASP
+   * http://{url}/api/issues/search?componentKeys={key}&facets=owaspTop10&owaspTop10=a1,a2,a3,a4,a5,a6,a7,a8,a9,a10
+   * Attention une faille peut être comptée deux fois ou plus, cela dépend du tag.
+   * Donc il  est possible d'avoir pour la clé une faille de type OWASP-A3 et OWASP-A10
+   *
+   * @param Request $request
+   *
+   * @return response
+   *
+   * Created at: 15/12/2022, 21:37:54 (Europe/Paris)
+   * @author    Laurent HADJADJ <laurent_h@me.com>
+   * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+   */
+  #[Route('/api/projet/issues/owasp', name: 'projet_issues_owasp', methods: ['GET'])]
+  public function projetIssuesOwasp(Request $request, Client $client): response
+  {
+
+    /** On créé un objet response */
+    $response = new JsonResponse();
+
+    /** On bind les variables. */
+    $mavenKey = $request->get('mavenKey');
+    $mode = $request->get('mode');
+    $tempoUrlLong = $this->getParameter(static::$sonarUrl).static::$apiIssuesSearch;
+
+    /** On teste si la clé est valide */
+    if ($mavenKey==="null" && $mode==="TEST") {
+      return $response->setData([
+        "mode"=>$mode, "mavenKey"=>$mavenKey,
+        "message" => static::$erreurMavenKey, Response::HTTP_BAD_REQUEST]);
+    }
+
+    /** On vérifie si l'utilisateur à un rôle Collecte ? */
+    if (!$this->isGranted('ROLE_COLLECTE')){
+      return $response->setData([
+        "mode"=>$mode ,
+        "type"=>'alert',
+        "reference" => static::$reference,
+        "message"=> static::$message,
+        Response::HTTP_OK]);
+    }
+
+    /** URL de l'appel. */
+    $url = "$tempoUrlLong$mavenKey&facets=owaspTop10
+            &owaspTop10=a1,a2,a3,a4,a5,a6,a7,a8,a9,a10";
+
+    /** On appel l'API. */
+    $result = $client->http(trim(preg_replace(static::$regex, " ", $url)));
+
+    $date = new DateTime();
+    $date->setTimezone(new DateTimeZone(static::$europeParis));
+    $owasp = [$result["total"]];
+    $effortTotal = $result["effortTotal"];
+
+    for ($a = 0; $a < 10; $a++) {
+      switch ($result["facets"][0]["values"][$a]["val"]) {
+        case 'a1':
+          $owasp[1] = $result["facets"][0]["values"][$a]["count"];
+          break;
+        case 'a2':
+          $owasp[2] = $result["facets"][0]["values"][$a]["count"];
+          break;
+        case 'a3':
+          $owasp[3] = $result["facets"][0]["values"][$a]["count"];
+          break;
+        case 'a4':
+          $owasp[4] = $result["facets"][0]["values"][$a]["count"];
+          break;
+        case 'a5':
+          $owasp[5] = $result["facets"][0]["values"][$a]["count"];
+          break;
+        case 'a6':
+          $owasp[6] = $result["facets"][0]["values"][$a]["count"];
+          break;
+        case 'a7':
+          $owasp[7] = $result["facets"][0]["values"][$a]["count"];
+          break;
+        case 'a8':
+          $owasp[8] = $result["facets"][0]["values"][$a]["count"];
+          break;
+        case 'a9':
+          $owasp[9] = $result["facets"][0]["values"][$a]["count"];
+          break;
+        case 'a10':
+          $owasp[10] = $result["facets"][0]["values"][$a]["count"];
+          break;
+        default:
+          $this->logger->NOTICE("HoneyPot : Référentiel OWASP !");
+      }
+    }
+
+    $a1Blocker=$a1Critical=$a1Major=$a1Info=$a1Minor=0;
+    $a2Blocker=$a2Critical=$a2Major=$a2Info=$a2Minor=0;
+    $a3Blocker=$a3Critical=$a3Major=$a3Info=$a3Minor=0;
+    $a4Blocker=$a4Critical=$a4Major=$a4Info=$a4Minor=0;
+    $a5Blocker=$a5Critical=$a5Major=$a5Info=$a5Minor=0;
+    $a6Blocker=$a6Critical=$a6Major=$a6Info=$a6Minor=0;
+    $a7Blocker=$a7Critical=$a7Major=$a7Info=$a7Minor=0;
+    $a8Blocker=$a8Critical=$a8Major=$a8Info=$a8Minor=0;
+    $a9Blocker=$a9Critical=$a9Major=$a9Info=$a9Minor=0;
+    $a10Blocker=$a10Critical=$a10Major=$a10Info=$a10Minor=0;
+
+    if ($result["total"] != 0) {
+      foreach ($result["issues"] as $issue) {
+        $severity = $issue["severity"];
+        if (
+          $issue["status"] == 'OPEN' ||
+          $issue["status"] == 'CONFIRMED' ||
+          $issue["status"] == 'REOPENED'
+        ) {
+          $tagMatch = preg_match("/owasp-a/is", var_export($issue["tags"], true));
+          if ($tagMatch != 0) {
+            foreach ($issue["tags"] as $tag) {
+              switch ($tag) {
+                case "owasp-a1":
+                  if ($severity == 'BLOCKER') {
+                    $a1Blocker++;
+                  }
+                  if ($severity == 'CRITICAL') {
+                    $a1Critical++;
+                  }
+                  if ($severity == 'MAJOR') {
+                    $a1Major++;
+                  }
+                  if ($severity == 'INFO') {
+                    $a1Info++;
+                  }
+                  if ($severity == 'MINOR') {
+                    $a1Minor++;
+                  }
+                  break;
+                case "owasp-a2":
+                  if ($severity == 'BLOCKER') {
+                    $a2Blocker++;
+                  }
+                  if ($severity == 'CRITICAL') {
+                    $a2Critical++;
+                  }
+                  if ($severity == 'MAJOR') {
+                    $a2Major++;
+                  }
+                  if ($severity == 'INFO') {
+                    $a2Info++;
+                  }
+                  if ($severity == 'MINOR') {
+                    $a2Minor++;
+                  }
+                  break;
+                case "owasp-a3":
+                  if ($severity == 'BLOCKER') {
+                    $a3Blocker++;
+                  }
+                  if ($severity == 'CRITICAL') {
+                    $a3Critical++;
+                  }
+                  if ($severity == 'MAJOR') {
+                    $a3Major++;
+                  }
+                  if ($severity == 'INFO') {
+                    $a3Info++;
+                  }
+                  if ($severity == 'MINOR') {
+                    $a3Minor++;
+                  }
+                  break;
+                case "owasp-a4":
+                  if ($severity == 'BLOCKER') {
+                    $a4Blocker++;
+                  }
+                  if ($severity == 'CRITICAL') {
+                    $a4Critical++;
+                  }
+                  if ($severity == 'MAJOR') {
+                    $a4Major++;
+                  }
+                  if ($severity == 'INFO') {
+                    $a4Info++;
+                  }
+                  if ($severity == 'MINOR') {
+                    $a4Minor++;
+                  }
+                  break;
+                case "owasp-a5":
+                  if ($severity == 'BLOCKER') {
+                    $a5Blocker++;
+                  }
+                  if ($severity == 'CRITICAL') {
+                    $a5Critical++;
+                  }
+                  if ($severity == 'MAJOR') {
+                    $a5Major++;
+                  }
+                  if ($severity == 'INFO') {
+                    $a5Info++;
+                  }
+                  if ($severity == 'MINOR') {
+                    $a5Minor++;
+                  }
+                  break;
+                case "owasp-a6":
+                  if ($severity == 'BLOCKER') {
+                    $a6Blocker++;
+                  }
+                  if ($severity == 'CRITICAL') {
+                    $a6Critical++;
+                  }
+                  if ($severity == 'MAJOR') {
+                    $a6Major++;
+                  }
+                  if ($severity == 'INFO') {
+                    $a6Info++;
+                  }
+                  if ($severity == 'MINOR') {
+                    $a6Minor++;
+                  }
+                  break;
+                case "owasp-a7":
+                  if ($severity == 'BLOCKER') {
+                    $a7Blocker++;
+                  }
+                  if ($severity == 'CRITICAL') {
+                    $a7Critical++;
+                  }
+                  if ($severity == 'MAJOR') {
+                    $a7Major++;
+                  }
+                  if ($severity == 'INFO') {
+                    $a7Info++;
+                  }
+                  if ($severity == 'MINOR') {
+                    $a7Minor++;
+                  }
+                  break;
+                case "owasp-a8":
+                  if ($severity == 'BLOCKER') {
+                    $a8Blocker++;
+                  }
+                  if ($severity == 'CRITICAL') {
+                    $a8Critical++;
+                  }
+                  if ($severity == 'MAJOR') {
+                    $a8Major++;
+                  }
+                  if ($severity == 'INFO') {
+                    $a8Info++;
+                  }
+                  if ($severity == 'MINOR') {
+                    $a8Minor++;
+                  }
+                  break;
+                case "owasp-a9":
+                  if ($severity == 'BLOCKER') {
+                    $a9Blocker++;
+                  }
+                  if ($severity == 'CRITICAL') {
+                    $a9Critical++;
+                  }
+                  if ($severity == 'MAJOR') {
+                    $a9Major++;
+                  }
+                  if ($severity == 'INFO') {
+                    $a9Info++;
+                  }
+                  if ($severity == 'MINOR') {
+                    $a9Minor++;
+                  }
+                  break;
+                case "owasp-a10":
+                  if ($severity == 'BLOCKER') {
+                    $a10Blocker++;
+                  }
+                  if ($severity == 'CRITICAL') {
+                    $a10Critical++;
+                  }
+                  if ($severity == 'MAJOR') {
+                    $a10Major++;
+                  }
+                  if ($severity == 'INFO') {
+                    $a10Info++;
+                  }
+                  if ($severity == 'MINOR') {
+                    $a10Minor++;
+                  }
+                  break;
+                default:
+                  $this->logger->NOTICE("HoneyPot : Référentiel OWASP !");
+              }
+            }
+          }
+        }
+      }
+    }
+
+    /** On supprime les informations sur le projet. */
+    $sql = "DELETE FROM owasp WHERE maven_key='$mavenKey'";
+    if ($mode!=="TEST") {
+      $this->em->getConnection()->prepare($sql)->executeQuery();
+    }
+    /** Enregistre en base. */
+    $owaspTop10 = new Owasp();
+    $owaspTop10->setMavenKey($request->get("mavenKey"));
+    $owaspTop10->setEffortTotal($effortTotal);
+    $owaspTop10->setA1($owasp[1]);
+    $owaspTop10->setA2($owasp[2]);
+    $owaspTop10->setA3($owasp[3]);
+    $owaspTop10->setA4($owasp[4]);
+    $owaspTop10->setA5($owasp[5]);
+    $owaspTop10->setA6($owasp[6]);
+    $owaspTop10->setA7($owasp[7]);
+    $owaspTop10->setA8($owasp[8]);
+    $owaspTop10->setA9($owasp[9]);
+    $owaspTop10->setA10($owasp[10]);
+
+    $owaspTop10->setA1Blocker($a1Blocker);
+    $owaspTop10->setA1Critical($a1Critical);
+    $owaspTop10->setA1Major($a1Major);
+    $owaspTop10->setA1Info($a1Info);
+    $owaspTop10->setA1Minor($a1Minor);
+
+    $owaspTop10->setA2Blocker($a2Blocker);
+    $owaspTop10->setA2Critical($a2Critical);
+    $owaspTop10->setA2Major($a2Major);
+    $owaspTop10->setA2Info($a2Info);
+    $owaspTop10->setA2Minor($a2Minor);
+
+    $owaspTop10->setA3Blocker($a3Blocker);
+    $owaspTop10->setA3Critical($a3Critical);
+    $owaspTop10->setA3Major($a3Major);
+    $owaspTop10->setA3Info($a3Info);
+    $owaspTop10->setA3Minor($a3Minor);
+
+    $owaspTop10->setA4Blocker($a4Blocker);
+    $owaspTop10->setA4Critical($a4Critical);
+    $owaspTop10->setA4Major($a4Major);
+    $owaspTop10->setA4Info($a4Info);
+    $owaspTop10->setA4Minor($a4Minor);
+
+    $owaspTop10->setA5Blocker($a5Blocker);
+    $owaspTop10->setA5Critical($a5Critical);
+    $owaspTop10->setA5Major($a5Major);
+    $owaspTop10->setA5Info($a5Info);
+    $owaspTop10->setA5Minor($a5Minor);
+
+    $owaspTop10->setA6Blocker($a6Blocker);
+    $owaspTop10->setA6Critical($a6Critical);
+    $owaspTop10->setA6Major($a6Major);
+    $owaspTop10->setA6Info($a6Info);
+    $owaspTop10->setA6Minor($a6Minor);
+
+    $owaspTop10->setA7Blocker($a7Blocker);
+    $owaspTop10->setA7Critical($a7Critical);
+    $owaspTop10->setA7Major($a7Major);
+    $owaspTop10->setA7Info($a7Info);
+    $owaspTop10->setA7Minor($a7Minor);
+
+    $owaspTop10->setA8Blocker($a8Blocker);
+    $owaspTop10->setA8Critical($a8Critical);
+    $owaspTop10->setA8Major($a8Major);
+    $owaspTop10->setA8Info($a8Info);
+    $owaspTop10->setA8Minor($a8Minor);
+
+    $owaspTop10->setA9Blocker($a9Blocker);
+    $owaspTop10->setA9Critical($a9Critical);
+    $owaspTop10->setA9Major($a9Major);
+    $owaspTop10->setA9Info($a9Info);
+    $owaspTop10->setA9Minor($a9Minor);
+
+    $owaspTop10->setA10Blocker($a10Blocker);
+    $owaspTop10->setA10Critical($a10Critical);
+    $owaspTop10->setA10Major($a10Major);
+    $owaspTop10->setA10Info($a10Info);
+    $owaspTop10->setA10Minor($a10Minor);
+
+    $owaspTop10->setDateEnregistrement($date);
+    $this->em->persist($owaspTop10);
+    if ($mode!=="TEST")
+    {
+      $this->em->flush();
+    }
+
+    return $response->setData(["mode"=>$mode,"owasp" => $result["total"], Response::HTTP_OK]);
+  }
+
+}
