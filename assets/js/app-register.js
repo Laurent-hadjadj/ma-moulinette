7741e70df58991af7a5bdb9f83232ce7fe52e21a
Laurent HADJADJ <laurent_h@me.com>
2024-01-10 18:05:45 +0100

    Controle du courriel, ajout de remotdepasse, ajout du masquage/affichage.

--- assets/js/app-register.js
+++ assets/js/app-register.js
@@ -22,8 +22,7 @@ import 'motion-ui';
 import './foundation.js';
 
 /** On importe les constantes */
-import { un, deux, sept, huit, cinquanteTrois,soixanteQuatre } from './constante.js';
-
+import { zero, deux, huit, trenteDeux, cinquanteDeux  } from './constante.js';
 
 /**
  * checkOkSvg
@@ -47,123 +46,103 @@ const checkKoSvg = `<svg xmlns="http://www.w3.org/2000/svg" overflow="visible" x
 3.753 12.914 0L32.5 45.593l12.913 13.593c3.566 3.753 9.348 3.753 12.914 0 3.565-3.753 3.565-9.84 0-13.593L45.413 32z"/></g></svg>`;
 
 /** Nettoyage de formulaire */
-$('#registration_form_courriel').val('');
-$('#registration_form_plainPassword').val('');
+$('#registration_form_plainPassword_first').val('');
+$('#registration_form_plainPassword_second').val('');
+$('#message-erreur-valider').val('');
 
-/** Affichage des libellés */
+/** Ajout du label pour le nom */
 $('#registration_form_nom').on('keyup', function(){
+  const nomLength=$('#registration_form_nom').val().length;
+
   if (this.value !== '') {
     $('label[for="registration_form_nom"]').addClass('show');
   } else {
     $('label[for="registration_form_nom"]').removeClass('show');
   }
+
+if (nomLength>=deux && nomLength<=trenteDeux) {
+  $('#register-info-check-nom').html(checkOkSvg);
+} else if (nomLength===zero) {
+    $('#register-info-check-nom').html('');
+  } else {
+    $('#register-info-check-nom').html(checkKoSvg);
+  }
+
+
 });
+
+/** Ajout du label pour le prénom */
 $('#registration_form_prenom').on('keyup', function(){
+  const prenomLength=$('#registration_form_prenom').val().length;
+
   if (this.value !== '') {
     $('label[for="registration_form_prenom"]').addClass('prenom');
   } else {
     $('label[for="registration_form_prenom"]').removeClass('prenom');
   }
+  if (prenomLength>=deux && prenomLength<=trenteDeux) {
+    $('#register-info-check-prenom').html(checkOkSvg);
+  } else if (prenomLength===zero) {
+      $('#register-info-check-prenom').html('');
+    } else {
+      $('#register-info-check-prenom').html(checkKoSvg);
+    }
 });
+
+/** Ajout du label pour le courriel */
 $('#registration_form_courriel').on('keyup', function(){
   if (this.value !== '') {
     $('label[for="registration_form_courriel"]').addClass('show');
   } else {
     $('label[for="registration_form_courriel"]').removeClass('show');
   }
-});
-$('#registration_form_plainPassword').on('keyup', function(){
-  if (this.value !== '') {
-    $('label[for="registration_form_plainPassword"]').addClass('show');
+
+  const courrielValue = document.getElementById("registration_form_courriel");
+  if (courrielValue.checkValidity()) {
+    $('#register-info-check-courriel').html(checkOkSvg);
   } else {
-    $('label[for="registration_form_plainPassword"]').removeClass('show');
+  $('#register-info-check-courriel').html(checkKoSvg);
   }
 });
 
-$('#registration_form_courriel').on('keyup', function(){
-    /**
-     * La longueur d'un mail est de 64+1+255
-     * On verifie la plus part des erreurs de saisie avant d'envoyer au contrôleur
-     * qui vérifira la conformité de l'adresse de courriel.
-     */
+/** Ajout du label pour le mot de passe */
+$('#registration_form_plainPassword_first').on('keyup', function(){
+  if (this.value !== '') {
+    $('label[for="registration_form_plainPassword_first"]').addClass('show');
+  } else {
+    $('label[for="registration_form_plainPassword_first"]').removeClass('show');
+  }
+});
 
-    let domaine;
-    const courriel=$('#registration_form_courriel').val();
-    const arobase=courriel.indexOf('@');
-    const point=courriel.indexOf('.');
-
-    /** Si le champ est vide */
-    if ( courriel.length === 0 ) {
-      $('#register-info-check-courriel').html('');
-      return;
-    }
-
-    /** Si le permier caractère est @ */
-    if (courriel.length === 1 && arobase === 0 ) {
-      $('#register-info-check-courriel').html(checkKoSvg);
-      return;
-    }
-
-    /** Si on à un @ à la 65eme position */
-    if (courriel.length > soixanteQuatre && arobase > 0 ){
-      $('#register-info-check-courriel').html(checkKoSvg);
-      return;
-    }
-
-    /** Si on a pas de @ avant la 64eme position */
-    if (courriel.length < soixanteQuatre && arobase < 0 ){
-      $('#register-info-check-courriel').html(checkKoSvg);
-      return;
-    }
-
-    /** Si on a un @ avant 65eme posistion */
-    if (courriel.length < soixanteQuatre && arobase > 0 ){
-      domaine=courriel.split('@');
-      /* Si le tableau contient plus de 2 éléments on sort */
-      if (domaine.length>deux) {
-        $('#register-info-check-courriel').html(checkKoSvg);
-        return;
-      }
-      $('#register-info-check-courriel').html(checkOkSvg);
-    }
-
-    /** Si le domaine n'est pas correcte. */
-    if (domaine[1].length===un && point>un){
-      $('#register-info-check-courriel').html(checkKoSvg);
-    }
-
-    /** On vérifie que le nom de domaine est correcte */
-    if (domaine[1].length===un && point>un){
-      $('#register-info-check-courriel').html(checkKoSvg);
-    }
-
-    /** On verifie que le domaine est <256 */
-    const points=(domaine[1].match(/\./g)||[]).length;
-    if (points>un) {
-      $('#register-info-check-courriel').html(checkKoSvg);
-    }
-
+/** Ajout du label pour le re-motdepasse */
+$('#registration_form_plainPassword_second').on('keyup', function(){
+  if (this.value !== '') {
+    $('label[for="registration_form_plainPassword_second"]').addClass('show');
+  } else {
+    $('label[for="registration_form_plainPassword_second"]').removeClass('show');
+  }
 });
 
 /** Vérification du mot de passe */
-$('#registration_form_plainPassword').on('keyup', function(){
-    const password=$('#registration_form_plainPassword').val();
+$('#registration_form_plainPassword_first, #registration_form_plainPassword_second').on(
+  'keyup', ()=>{
+    const password=$('#registration_form_plainPassword_first').val();
+    const repassword=$('#registration_form_plainPassword_second').val();
 
-    if ( password.length === 0 ) {
-      $('#register-info-check').html('');
-      }
-    if ( password.length>0 && password.length<huit ) {
-        $('#register-info-check-password').html('');
-        $('#register-info-check').html('<span class="register-info-erreur">Taille Min 8.</span>');
-      }
-    if ( password.length>cinquanteTrois ) {
-        $('#register-info-check').html('<span class="profil-info-erreur">Taille Max 52.</span>');
-      }
-    if ( password.length>sept && password.length<cinquanteTrois ) {
-        $('#register-info-check').html('');
+    if (password.length>=huit && password.length<=cinquanteDeux) {
         $('#register-info-check-password').html(checkOkSvg);
-      } else {
-        $('#register-info-check-password').html(checkKoSvg);
+      } else if (password.length===zero) {
+          $('#register-info-check-password').html('');
+        } else {
+          $('#register-info-check-password').html(checkKoSvg);
+        }
+
+      if ( repassword.length>=huit && repassword.length<=cinquanteDeux ) {
+        $('#register-info-check-repassword').html(checkOkSvg);
+        } else if (repassword.length===zero) {
+        $('#register-info-check-repassword').html('');
+        } else {
+        $('#register-info-check-repassword').html(checkKoSvg);
       }
   });
 
@@ -181,3 +160,59 @@ $('.thumbnail').on('click', function(){
     $('#registration_form_avatar').val(path+'.png');
     $('#mes-avatars').foundation('close');
   });
+
+/** Activation du bouton d'enregistrement */
+$('#registration_form_plainPassword_second').on('focus', function(){
+  $('#valider-formulaire-enregistrement').removeClass('disabled');
+});
+
+  /** Vérification des informations du formulaire */
+$('#valider-formulaire-enregistrement').on('click', async ()=>{
+  const nomLength=$('#registration_form_nom').val().length;
+  const prenomLength=$('#registration_form_prenom').val().length;
+  const courrielLength=$('#registration_form_courriel').val().length;
+  const courrielValue = document.getElementById("registration_form_courriel");
+  const passwordLength=$('#registration_form_plainPassword_first').val().length;
+  const repasswordLength=$('#registration_form_plainPassword_second').val().length;
+  const passwordValue=$('#registration_form_plainPassword_first').val();
+  const repassordValue=$('#registration_form_plainPassword_second').val();
+
+if (nomLength>=2 && prenomLength>=2 && courrielLength>=5 &&
+    passwordLength>=8 && repasswordLength>=8 &&
+    passwordValue===repassordValue &&
+    courrielValue.checkValidity()) {
+      $('#message-erreur-valider').html('');
+      $('#message-erreur-courriel').html('');
+      $('#valider-formulaire-enregistrement').attr('type', 'submit');
+      const link = document.getElementById('valider-formulaire-enregistrement');
+      link.click()
+  } else {
+    const message='<ul><li>[001] - Le formulaire contient des erreurs !!!</li></ul>';
+    $('#message-erreur-valider').html(message);
+  }
+});
+
+
+/*
+  Switch actions
+*/
+$('.unmask1, .unmask2').on('click', function(){
+
+  if($(this).prev('input').attr('type') == 'password')
+    changeType($(this).prev('input'), 'text');
+
+  else
+    changeType($(this).prev('input'), 'password');
+
+  return false;
+});
+
+function changeType(x, type) {
+  if(x.prop('type') == type)
+  return x; //That was easy.
+  try {
+    return x.prop('type', type); //Stupid IE security will not allow this
+  } catch(e) {
+    console.log('Stupid IE security will not allow this')
+  }
+}
