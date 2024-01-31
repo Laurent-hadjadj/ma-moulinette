d5d61e14caab9f7d9c51bbc737d7ee163d874bd2
Laurent HADJADJ <laurent_h@me.com>
2024-01-31 08:43:12 +0100

    Fix: Prise en compte des évolutions Symfony 6.4

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -21,7 +21,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
@@ -30,8 +29,6 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
-
-//https://github.com/NouvelleTechno/e-commerce-Symfony-6/blob/main/src/Security/UsersAuthenticator.php
 
 class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 {
@@ -55,13 +52,12 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         $courriel = $request->request->get('courriel', '');
         $motDePasse = $request->request->get('password', '');
 
-        $request->getSession()->set(Security::LAST_USERNAME, $courriel);
-
+        //$request->getSession()->set(self::LAST_USERNAME, $courriel);
         // On cherche si l'utilisateur existe !
         return new Passport(
             /**
              * Si l'utilisateur n'existe pas on génére une exception.
-             * Si l'utilisateur existe mais que son ststut est 'FALSE',
+             * Si l'utilisateur existe mais que son statut est 'FALSE',
              *  i.e. l'attribut 'actif', on génére une excption.
              * Si l'utilisateur existe et son statut est à "TRUE" et
              *  que le mot de passe est correcte :
@@ -69,7 +65,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
              * 2 - On ajoute le support de "Remember-me" ;
              */
 
-            new UserBadge($courriel, function ($utilisateurIdentifier) {
+            new UserBadge($courriel, function (string $utilisateurIdentifier) {
                 //'actif => TRUE
                 $utilisateur = $this->utilisateurRepository
                     ->findOneBy(['courriel' => $utilisateurIdentifier, 'actif' => true]);
@@ -89,7 +85,6 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
     // si le courriel existe et le credential est bon on redirige vers home
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
         }
