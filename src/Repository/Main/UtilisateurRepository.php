823dbb08acedeb6b6b1e408dd87c1e66cbaa823f
Laurent HADJADJ <laurent_h@me.com>
2024-02-19 09:35:35 +0100

    refactor: mise à jour de la méthode aujour/suppression d'un favori.

--- src/Repository/Main/UtilisateurRepository.php
+++ src/Repository/Main/UtilisateurRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository\Main;
 
+use ApiPlatform\JsonApi\State\JsonApiProvider;
 use App\Entity\Main\Utilisateur;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -66,6 +67,100 @@ class UtilisateurRepository extends ServiceEntityRepository
     }
 
     /**
+     * [Description for insertUtilisateurPreferenceFavori]
+     *
+     * @param string $mode
+     * @param array $preference
+     * @param array $map
+     *
+     * @return array
+     *
+     * Created at: 17/02/2024 23:05:33 (Europe/Paris)
+     * @author     Laurent HADJADJ <laurent_h@me.com>
+     * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function insertUtilisateurPreferenceFavori($mode, $preference, $map):array
+    {
+        /** On récupére les préférences */
+        $statut = $preference['statut'];
+        $listeProjet = $preference['projet'];
+        $listeFavori = $preference['favori'];
+        $listeVersion = $preference['version'];
+        $bookmark = $preference['bookmark'];
+
+        /**
+         * On recupère la valeur de l'index pour le projet en favori
+         */
+        $i=$index=-1;
+        foreach($preference['version'] as $projet){
+            $i++;
+            if (array_key_exists($map['maven_key'],$projet)){
+                $index=$i;
+            };
+        }
+
+
+        /* si le projet n'est pas un favori on l'ajoute */
+        if (!in_array($map['maven_key'], $listeFavori)){
+            array_push($preference['favori'], $map['maven_key']);
+        }
+
+        /** On ajoute la version du projet en favori si il n'existe pas */
+        if (!str_contains(\serialize($preference['version']), $map['maven_key'])){
+            /** On ajoute à la liste la version */
+            array_push($preference['version'],[$map['maven_key'] => [$map['version']]]);
+        } else {
+            /** Il existe déjà un projet avec au moins une version en favori, on ajoute une nouvelle version. */
+            $listeVersion = [];
+            /**
+             * On parse le json, pour trouver la version ($index)
+             * et on ajoute une nouvelle version, si la version n'existe pas dans la liste
+             * liste = [ "fr.ma-moulinette:monapplication" => [ 0 => "4.1.0-RELEASE"] ]
+             */
+            foreach ($preference['version'] as $key => $liste) {
+                /** On récupère chaque liste de version et on cherche si la version existe ou non pour la clé*/
+                if ($key === $index) {
+                    if (!in_array($map['version'],$liste[$map['maven_key']])) {
+                        array_push($preference['version'][$index][$map['maven_key']],$map['version']);
+                    }
+                }
+                if ($key !== $index) {
+                    array_push($listeVersion, $preference['version'][$key]);
+                }
+            }
+        }
+
+        /** On met à jour l'objet et on vire les \. */
+        $jsonArray = stripslashes(
+            json_encode([
+                'statut' => $statut,
+                'projet' => $listeProjet,
+                'favori' => $preference['favori'],
+                'version' => $preference['version'],
+                'bookmark' => $bookmark
+                ])
+        );
+
+        $response=['mode'=>$mode, 'code'=>200, 'erreur'=>''];
+        $sql = "UPDATE utilisateur
+                SET preference=:preference
+                WHERE courriel=:courriel";
+        $conn=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnline, " ", $sql));
+        $conn->bindValue(':courriel', $map['courriel']);
+        $conn->bindValue(':preference', $jsonArray);
+        try {
+            if ($mode !== 'TEST') {
+                $conn->executeQuery();
+            } else {
+                $response=['mode'=>$mode, 'code'=> 202, 'erreur'=>'TEST'];
+            }
+        } catch (\Doctrine\DBAL\Exception $e) {
+            $response=['mode'=>$mode, 'code'=>500, 'erreur'=> $e->getCode()];
+        }
+        return $response;
+    }
+
+    /**
      * [Description for deletePreferenceFavoris]
      * Permet de supprimer un favoris ou une version favorite d'un projet
      *
@@ -81,7 +176,7 @@ class UtilisateurRepository extends ServiceEntityRepository
      */
     public function deleteUtilisateurPreferenceFavori($mode, $preference, $map):array {
         /**
-         * On reagrde d'abord si le projet à une version en favori
+         * On regarde d'abord si le projet à une version en favori
          * ensuite on regarde conblen de version pour ce projet sont en favori
          * si, il n'y a q'une version, on supprime la version et la clé de la liste de versions
          * et on supprimer aussi le projet de la liste des projets favoris
