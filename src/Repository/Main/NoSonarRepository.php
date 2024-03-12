5f79b11047b78552f77688cd32b96646371c8937
Laurent HADJADJ <laurent_h@me.com>
2024-03-12 21:13:45 +0100

    Refactor: Migration des requêtes SQL vers l'EntityRepository.

--- src/Repository/Main/NoSonarRepository.php
+++ src/Repository/Main/NoSonarRepository.php
@@ -19,9 +19,43 @@ use Doctrine\Persistence\ManagerRegistry;
 
 class NoSonarRepository extends ServiceEntityRepository
 {
+    public static $removeReturnline = "/\s+/u";
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, NoSonar::class);
+    }
+
+    /**
+     * [Description for deleteNoSonarMavenKey]
+     * Supprime les données de la version courrante (i.e. correspondant à la maven_key)
+     *
+     * @param mixed $mode
+     * @param mixed $map
+     *
+     * @return array
+     *
+     * Created at: 11/03/2024 17:36:11 (Europe/Paris)
+     * @author     Laurent HADJADJ <laurent_h@me.com>
+     * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function deleteNoSonarMavenKey($mode,$map):array
+    {
+        $sql = "DELETE
+                FROM no_sonar
+                WHERE maven_key=:maven_key";
+        $conn=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnline, " ", $sql));
+        $conn->bindValue(':maven_key', $map['maven_key']);
+        try {
+                if ($mode !== 'TEST') {
+                    $conn->executeQuery();
+                } else {
+                    return ['mode'=>$mode, 'code'=> 202, 'erreur'=>'TEST'];
+                }
+        } catch (\Doctrine\DBAL\Exception $e) {
+            return ['mode'=>$mode, 'code'=>500, 'erreur'=> $e->getCode()];
+        }
+        return ['mode'=>$mode, 'code'=>200, 'erreur'=>''];
     }
 
 }
