458997ccf4add4a9559592c76205662095016d8b
Laurent HADJADJ <laurent_h@me.com>
2024-03-12 21:10:55 +0100

    Refactor: Migration GET en POST, code clean, migration des requêtes SQL vers l'EntityRepository.

--- src/Controller/Projet/ApiNoteController.php
+++ src/Controller/Projet/ApiNoteController.php
@@ -21,11 +21,13 @@ use Symfony\Component\Routing\Annotation\Route;
 /** Gestion de accès aux API */
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-use Doctrine\ORM\EntityManagerInterface;
-
 /** Gestion du temps */
 use DateTime;
 use DateTimeZone;
+
+/** gestion des entity */
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Main\Notes;
 
 /** Client HTTP */
 use App\Service\Client;
@@ -39,10 +41,12 @@ class ApiNoteController extends AbstractController
     public static $sonarUrl = "sonar.url";
     public static $dateFormat = "Y-m-d H:i:s";
     public static $europeParis = "Europe/Paris";
-    public static $regex = "/\s+/u";
-    public static $erreurMavenKey = "La clé maven est vide!";
-    public static $reference = "<strong>[PROJET-002]</strong>";
-    public static $message = "Vous devez avoir le rôle COLLECTE pour réaliser cette action.";
+    public static $removeReturnline = "/\s+/u";
+    public static $reference = "<strong>[PROJET-003]</strong>";
+    public static $erreur400 = "La requête est incorrecte (Erreur 400).";
+    public static $erreur401 = "Erreur d\'Authentification. La clé n\'est pas correcte (Erreur 401).";
+    public static $erreur403 = "Vous devez avoir le rôle COLLECTE pour réaliser cette action (Erreur 403).";
+    public static $erreur404 = "L'appel à l'API n'a pas abouti (Erreur 404).";
 
     /**
      * [Description for __construct]
@@ -66,6 +70,7 @@ class ApiNoteController extends AbstractController
      * Les valeurs possibles pour {type} sont : reliability_rating,security_rating,sqale_rating
      *
      * @param Request $request
+     * @param Client $client
      *
      * @return response
      *
@@ -73,66 +78,108 @@ class ApiNoteController extends AbstractController
      * @author    Laurent HADJADJ <laurent_h@me.com>
      * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
      */
-    #[Route('/api/projet/historique/note', name: 'projet_historique_note', methods: ['GET'])]
-    public function historiqueNoteAjout(Client $client, Request $request): response
+    #[Route('/api/projet/note', name: 'projet_note_collecte', methods: ['POST'])]
+    public function projetNoteCollecte(Client $client, Request $request): response
     {
+        /** On décode le body */
+        $data = json_decode($request->getContent());
 
-        /** On créé un objet response */
+        /** On crée un objet de reponse JSON */
         $response = new JsonResponse();
 
-        /** On bind les variables. */
-        $tempoUrl = $this->getParameter(static::$sonarUrl);
-        $mavenKey = $request->get('mavenKey');
-        $type = $request->get('type');
-        $mode = $request->get('mode');
-
         /** On teste si la clé est valide */
-        if ($mavenKey === "null" && $mode === "TEST") {
-            return $response->setData([
-              "mode" => $mode, "mavenKey" => $mavenKey,
-              "message" => static::$erreurMavenKey, Response::HTTP_BAD_REQUEST]);
-        }
+        if ($data === null) {
+            return $response->setData(['data' => null, 'type'=>'alert', 'code'=>400, "message" => static::$erreur400, Response::HTTP_BAD_REQUEST]); }
+        if (!property_exists($data, 'mode')) {
+            return $response->setData(['mode' => null, 'type'=>'alert', 'code'=>400, "message" => static::$erreur400, Response::HTTP_BAD_REQUEST]); }
+        if (!property_exists($data, 'maven_key')) {
+            return $response->setData(['maven_key' => null, 'type'=>'alert', 'code'=>400, "message" => static::$erreur400, Response::HTTP_BAD_REQUEST]); }
+        if (!property_exists($data, 'type')) {
+            return $response->setData(['type' => null, '_type'=>'alert', 'code'=>400, "message" => static::$erreur400, Response::HTTP_BAD_REQUEST]); }
 
         /** On vérifie si l'utilisateur à un rôle Collecte ? */
         if (!$this->isGranted('ROLE_COLLECTE')) {
             return $response->setData([
-              "mode" => $mode ,
-              "type" => 'alert',
-              "reference" => static::$reference,
-              "message" => static::$message,
-              Response::HTTP_OK]);
+                'type'=>'warning',
+                'mode' => $data->mode ,
+                'code' => 403,
+                'reference' => static::$reference,
+                'message' => static::$erreur403,
+                Response::HTTP_OK]);
         }
 
-        $url = "$tempoUrl/api/measures/search_history?component=$mavenKey&metrics=${type}_rating&ps=1000";
+        /** On récupère l'URl du serveur. */
+        $tempoUrl = $this->getParameter(static::$sonarUrl);
+
+        /** On construit l'URL */
+        $url = "$tempoUrl/api/measures/search_history?component=$data->mavenKey&metrics=$data->type"."_rating&ps=1000";
 
         /** On appel le client http. */
-        $result = $client->http(trim(preg_replace(static::$regex, " ", $url)));
+        $result = $client->http(trim(preg_replace(static::$removeReturnline, " ", $url)));
+        /** On catch les erreurs HTTP 400, 401 et 404, si possible :) */
+        if (array_key_exists('code', $result)){
+        if ($result['code']===401) {
+        return $response->setData([
+            'type'=>'warning',
+            'mode' => $data->mode ,
+            'code' => 401,
+            'reference' => static::$reference,
+            'message' => static::$erreur401,
+            Response::HTTP_OK]);
+        }
+        if ($result['code']===404){
+            return $response->setData([
+                'type'=>'alert',
+                "mode" => $data->mode ,
+                "code" => 404,
+                "reference" => static::$reference,
+                "message" => static::$erreur404,
+                Response::HTTP_OK]);
+            }
+    }
 
+        /** On construit un objet date */
         $date = new DateTime();
         $date->setTimezone(new DateTimeZone(static::$europeParis));
         $tempoDate = $date->format(static::$dateFormat);
+
+        /** on récupère le nombre et les mesures */
         $nombre = $result["paging"]["total"];
         $mesures = $result["measures"][0]["history"];
+
+        /** On bind l'entityRepository */
+        $notes = $this->em->getRepository(Notes::class);
+
+        /** On supprime les notes pour la maven_key. */
+        $map=['maven_key'=>$data->maven_key];
+        $request=$notes->deleteNotesMavenKey($data->mode, $map);
+        if ($request['code']!=200) {
+            return $response->setData([
+                'type' => 'alert',
+                'mode' => $data->mode,
+                'reference' => static::$reference,
+                'code' => $request['code'],
+                'message'=>$request['erreur'],
+                Response::HTTP_OK]);
+        }
 
         /** Enregistrement des nouvelles valeurs. */
         foreach ($mesures as $mesure) {
             $tempoMesureDate = $mesure["date"];
             $tempoMesureValue = $mesure["value"];
-            $sql = "INSERT OR IGNORE INTO notes (maven_key, type, date, value, date_enregistrement)
-              VALUES ('$mavenKey', '$type', '$tempoMesureDate', '$tempoMesureValue', '$tempoDate')";
-            $this->em->getConnection()->prepare($sql)->executeQuery();
+            $map=['maven_key'=>$data->mavenKey, 'type'=>$data->type, 'date'=>$tempoMesureDate, 'value'=>$tempoMesureValue, 'date_enregistrement'=>$tempoDate];
         }
 
-        if ($request->get('type') == "reliability") {
+        if ($data->type == "reliability") {
             $type = "Fiabilité";
         }
-        if ($request->get('type') == "security") {
+        if ($data->type == "security") {
             $type = "Sécurité";
         }
-        if ($request->get('type') == "sqale") {
+        if ($data->type == "sqale") {
             $type = "Mauvaises Pratiques";
         }
 
-        return $response->setData(["mode" => $mode, "nombre" => $nombre, "type" => $type, Response::HTTP_OK]);
+        return $response->setData(['mode' => $data->mode,'code' => 200, 'nombre' => $nombre, 'type' => $type, Response::HTTP_OK]);
     }
 }
