799ea4a65eeba6b8a9ff49a2ef0c55cd63bff50c
Laurent HADJADJ <laurent_h@me.com>
2024-03-14 10:06:04 +0100

    Refactor: Ajout de la méthode delete en remplacement de la requête SQL.

--- src/Repository/Main/HotspotsRepository.php
+++ src/Repository/Main/HotspotsRepository.php
@@ -22,9 +22,76 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class HotspotsRepository extends ServiceEntityRepository
 {
+    public static $removeReturnline = "/\s+/u";
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Hotspots::class);
+    }
+
+    /**
+     * [Description for deleteHotspotsMavenKey]
+     * Supprime les hotspots pour la version courrante (i.e. correspondant à la maven_key)
+     *
+     * @param mixed $mode
+     * @param mixed $map
+     *
+     * @return array
+     *
+     * Created at: 12/03/2024 21:49:02 (Europe/Paris)
+     * @author     Laurent HADJADJ <laurent_h@me.com>
+     * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function deleteHotspotsMavenKey($mode,$map):array
+    {
+        $sql = "DELETE
+                FROM hotspots
+                WHERE maven_key=:maven_key";
+        $conn=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnline, " ", $sql));
+        $conn->bindValue(':maven_key', $map['maven_key']);
+        try {
+                if ($mode !== 'TEST') {
+                    $conn->executeQuery();
+                } else {
+                    return ['mode'=>$mode, 'code'=> 202, 'erreur'=>'TEST'];
+                }
+        } catch (\Doctrine\DBAL\Exception $e) {
+            return ['mode'=>$mode, 'code'=>500, 'erreur'=> $e->getCode()];
+        }
+        return ['mode'=>$mode, 'code'=>200, 'erreur'=>''];
+    }
+
+    /**
+     * [Description for selectHotspotsToReview]
+     * Retourne la liste des hotspots pour le status TO_REVIEW
+     *
+     * @param string $mode
+     * @param array $map
+     *
+     * @return array
+     *
+     * Created at: 14/03/2024 08:50:52 (Europe/Paris)
+     * @author     Laurent HADJADJ <laurent_h@me.com>
+     * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function selectHotspotsToReview($mode,$map):array
+    {
+        $sql = "SELECT *
+                FROM hotspots
+                WHERE maven_key=:maven_key AND status='TO_REVIEW'
+                ORDER BY niveau";
+    $conn=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnline, " ", $sql));
+        $conn->bindValue(':maven_key', $map['maven_key']);
+        try {
+                if ($mode !== 'TEST') {
+                    $liste=$conn->executeQuery()->fetchAllAssociative();
+                } else {
+                    return ['mode'=>$mode, 'code'=> 202, 'erreur'=>'TEST'];
+                }
+        } catch (\Doctrine\DBAL\Exception $e) {
+            return ['mode'=>$mode, 'code'=>500, 'erreur'=> $e->getCode()];
+        }
+        return ['mode'=>$mode, 'code'=>200, 'erreur'=>'', 'liste'=>$liste];
     }
 
 }
