25b0358e2cac3f1cd98c6db5a297cae8ad06df1d
Laurent HADJADJ <laurent_h@me.com>
2024-04-16 16:35:25 +0200

    wip : batch

--- src/Repository/Main/BatchTraitementRepository.php
+++ src/Repository/Main/BatchTraitementRepository.php
@@ -19,35 +19,117 @@ use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<BatchTraitement>
- *
- * @method BatchTraitement|null find($id, $lockMode = null, $lockVersion = null)
- * @method BatchTraitement|null findOneBy(array $criteria, array $orderBy = null)
- * @method BatchTraitement[]    findAll()
- * @method BatchTraitement[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class BatchTraitementRepository extends ServiceEntityRepository
 {
+    public static $removeReturnline = "/\s+/u";
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, BatchTraitement::class);
     }
 
-    public function save(BatchTraitement $entity, bool $flush = false): void
+    /**
+     * [Description for selectBatchTraitementDateEnregistrementAutomatiqueLast]
+     * On récupère la dernière date de programmation du batch automatique
+     *
+     * @param string $mode
+     *
+     * @return array
+     *
+     * Created at: 10/04/2024 09:10:49 (Europe/Paris)
+     * @author     Laurent HADJADJ <laurent_h@me.com>
+     * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function selectBatchTraitementDateEnregistrementAutomatiqueLast($mode):array
     {
-        $this->getEntityManager()->persist($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
+        $sql = "SELECT date_enregistrement as date
+                FROM batch_traitement
+                WHERE demarrage='Automatique'
+                ORDER BY date_enregistrement DESC limit 1";
+        $conn=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnline, " ", $sql));
+        try {
+                if ($mode !== 'TEST') {
+                    $exec=$conn->executeQuery();
+                    $liste=$exec->fetchAllAssociative();
+                } else {
+                    return ['mode'=>$mode, 'code'=> 202, 'erreur'=>'TEST'];
+                }
+        } catch (\Doctrine\DBAL\Exception $e) {
+            return ['mode'=>$mode, 'code'=>500, 'erreur'=> $e->getCode()];
         }
+        return ['mode'=>$mode, 'code'=>200, 'liste'=>$liste, 'erreur'=>''];
     }
 
-    public function remove(BatchTraitement $entity, bool $flush = false): void
+    /**
+     * [Description for selectBatchTraitementDateEnregistrementLast]
+     * On récupère la dernière date du batch executé
+     *
+     * @param string $mode
+     *
+     * @return array
+     *
+     * Created at: 10/04/2024 09:35:40 (Europe/Paris)
+     * @author     Laurent HADJADJ <laurent_h@me.com>
+     * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function selectBatchTraitementDateEnregistrementLast($mode):array
     {
-        $this->getEntityManager()->remove($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
+        $sql = "SELECT date_enregistrement as date
+                FROM batch_traitement
+                ORDER BY date_enregistrement DESC limit 1";
+        $conn=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnline, " ", $sql));
+        try {
+                if ($mode !== 'TEST') {
+                    $exec=$conn->executeQuery();
+                    $liste=$exec->fetchAllAssociative();
+                } else {
+                    return ['mode'=>$mode, 'code'=> 202, 'erreur'=>'TEST'];
+                }
+        } catch (\Doctrine\DBAL\Exception $e) {
+            return ['mode'=>$mode, 'code'=>500, 'erreur'=> $e->getCode()];
         }
+        return ['mode'=>$mode, 'code'=>200, 'liste'=>$liste, 'erreur'=>''];
     }
 
+    /**
+     * [Description for selectBatchTraitementLast]
+     * On récupere la liste des derniers traitements,
+     * groupé par titre et ordonné par responsable
+     *
+     * @param string $mode
+     * @param string $date
+     *
+     * @return array
+     *
+     * Created at: 10/04/2024 09:38:41 (Europe/Paris)
+     * @author     Laurent HADJADJ <laurent_h@me.com>
+     * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function selectBatchTraitementLast($mode, $dateLike):array
+    {
+        $sql = "SELECT  demarrage, resultat, titre, portefeuille,
+                        nombre_projet as projet, responsable,
+                        debut_traitement as debut, fin_traitement as fin
+                        FROM batch_traitement
+                        WHERE date_enregistrement like :dateLike
+                        GROUP BY titre
+                        ORDER BY responsable ASC, demarrage ASC";
+        $conn=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnline, " ", $sql));
+        $conn->bindValue(':dateLike', $dateLike);
+        try {
+                if ($mode !== 'TEST') {
+                    $exec=$conn->executeQuery();
+                    $liste=$exec->fetchAllAssociative();
+                } else {
+                    return ['mode'=>$mode, 'code'=> 202, 'erreur'=>'TEST'];
+                }
+        } catch (\Doctrine\DBAL\Exception $e) {
+            return ['mode'=>$mode, 'code'=>500, 'erreur'=> $e->getCode()];
+        }
+        return ['mode'=>$mode, 'code'=>200, 'liste'=>$liste, 'erreur'=>''];
+    }
+
+
+    
 }
