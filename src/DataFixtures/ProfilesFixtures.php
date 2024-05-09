1d33ceb94185e3cafab6485ebb705d58d477242a
Laurent HADJADJ <laurent_h@me.com>
2024-05-09 23:14:02 +0200

    Fix: modification de l'attribut isDefault en referentiel_default

--- src/DataFixtures/ProfilesFixtures.php
+++ src/DataFixtures/ProfilesFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\Main\Profiles;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use phpDocumentor\Reflection\PseudoTypes\True_;
 
 /**
  * [Description ProfilesFixtures]
@@ -14,10 +15,10 @@ class ProfilesFixtures extends Fixture
 
   private static $key = 'AXyXMubJRtAGLwAs7Zcv';
   private static $name = 'Ma-Petite-Entreprise v1.0.0 (2024)';
-  private static $languageName = 'CSS';
+  private static $languageName = 'css';
   private static $activeRuleCount = 31;
   private static $rulesUpdateAt = '2024-04-13 12:10:51';
-  private static $default = true;
+  private static $referentielDefault = true;
   private static $dateEnregistrement = '2024-04-12 16:23:11';
 
   public function load(ObjectManager $manager): void
@@ -29,11 +30,11 @@ class ProfilesFixtures extends Fixture
           ->setLanguageName(static::$languageName)
           ->setActiveRuleCount(static::$activeRuleCount)
           ->setRulesUpdateAt(new \DateTime(static::$rulesUpdateAt))
-          ->setDefault(static::$default)
+          ->setReferentielDefault(static::$referentielDefault)
           ->setDateEnregistrement(new \DateTime(static::$dateEnregistrement));
       $manager->persist($profiles);
 
       /** Enregistrement des données dans la base de tests */
-        $manager->flush();
+      $manager->flush();
     }
   }
