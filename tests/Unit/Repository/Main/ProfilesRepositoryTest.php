da50892259f8dc2d8e051fda29bf1e15785bf239
Laurent HADJADJ <laurent_h@me.com>
2024-05-10 10:15:33 +0200

    Fix: Suppression des statics non utilisées.

--- tests/Unit/Repository/Main/ProfilesRepositoryTest.php
+++ tests/Unit/Repository/Main/ProfilesRepositoryTest.php
@@ -25,8 +25,6 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
  */
 class ProfilesRepositoryTest extends KernelTestCase
 {
-    private static $mavenKey = 'fr.ma-petite-entreprise:ma-moulinette';
-    private static $type = 'RELEASE';
 
     private static $erreurCode200 = 'Erreur le code retour doit être 200';
 
@@ -47,7 +45,6 @@ class ProfilesRepositoryTest extends KernelTestCase
         /* On se connecte à la base de tests */
         $container = static::getContainer();
         $entityManager = $container->get('doctrine')->getManager();
-
         // Appel de la méthode
         $profilesRepository = $entityManager->getRepository(Profiles::class);
         $r = $profilesRepository->countProfiles();
