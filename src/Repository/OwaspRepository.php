970ea08a6cd81be9db7dd81ff351242f5dc4554c
Laurent HADJADJ <laurent_h@me.com>
2024-05-26 15:43:15 +0200

    Refactor: Code Clean, Simplification et ajout de la requête insertOwaspMavenKey.

--- src/Repository/OwaspRepository.php
+++ src/Repository/OwaspRepository.php
@@ -61,7 +61,7 @@ class OwaspRepository extends ServiceEntityRepository
 
     /**
      * [Description for deleteOwaspMavenKey]
-     * Supprime les données de la version courrante (i.e. correspondant à la maven_key)
+     * Supprime les données de la version courante (i.e. correspondant à la maven_key)
      * @param mixed $map
      *
      * @return array
@@ -72,18 +72,77 @@ class OwaspRepository extends ServiceEntityRepository
      */
     public function deleteOwaspMavenKey($map):array
     {
-        $sql = "DELETE
-                FROM owasp
-                WHERE maven_key=:maven_key";
-        $conn=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnline, " ", $sql));
-        $conn->bindValue(':maven_key', $map['maven_key']);
         try {
-                #if ($mode !== 'TEST') {
-                    $conn->executeQuery();
-                #} else {
-                    return ['code'=> 202, 'erreur'=>'TEST'];
-                #}
+            $this->getEntityManager()->getConnection()->beginTransaction();
+                $sql = "DELETE
+                        FROM owasp
+                        WHERE maven_key=:maven_key";
+                $stmt=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnline, " ", $sql));
+                $stmt->bindValue(':maven_key', $map['maven_key']);
+                $stmt->executeStatement();
+            $this->getEntityManager()->getConnection()->commit();
         } catch (\Doctrine\DBAL\Exception $e) {
+            $this->getEntityManager()->getConnection()->rollBack();
+            return ['code'=>500, 'erreur'=> $e->getMessage()];
+        }
+        return ['code'=>200, 'erreur'=>''];
+    }
+
+    /**
+     * [Description for InsertOwasp]
+     * Ajoute les signalements et les issues OWASP pour le projet
+     *
+     * @param array $map
+     *
+     * @return array
+     *
+     * Created at: 26/05/2024 13:03:43 (Europe/Paris)
+     * @author     Laurent HADJADJ <laurent_h@me.com>
+     * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function insertOwasp($map):array
+    {
+        try {
+            $this->getEntityManager()->getConnection()->beginTransaction();
+            $sql = "INSERT INTO owasp
+                        (maven_key, version, date_version, effort_total, date_enregistrement,
+                        a1, a2, a3, a4, a5, a6, a7, a8, a9, a10,
+                        a1_blocker, a1_critical, a1_major, a1_info, a1_minor,
+                        a2_blocker, a2_critical, a2_major, a2_info, a2_minor,
+                        a3_blocker, a3_critical, a3_major, a3_info, a3_minor,
+                        a4_blocker, a4_critical, a4_major, a4_info, a4_minor,
+                        a5_blocker, a5_critical, a5_major, a5_info, a5_minor,
+                        a6_blocker, a6_critical, a6_major, a6_info, a6_minor,
+                        a7_blocker, a7_critical, a7_major, a7_info, a7_minor,
+                        a8_blocker, a8_critical, a8_major, a8_info, a8_minor,
+                        a9_blocker, a9_critical, a9_major, a9_info, a9_minor,
+                        a10_blocker, a10_critical, a10_major, a10_info, a10_minor)
+                    VALUES
+                        (:maven_key, :version, :date_version, :effort_total, :date_enregistrement,
+                        :a1, :a2, :a3, :a4, :a5, :a6, :a7, :a8, :a9, :a10,
+                        :a1_blocker, :a1_critical, :a1_major, :a1_info, :a1_minor,
+                        :a2_blocker, :a2_critical, :a2_major, :a2_info, :a2_minor,
+                        :a3_blocker, :a3_critical, :a3_major, :a3_info, :a3_minor,
+                        :a4_blocker, :a4_critical, :a4_major, :a4_info, :a4_minor,
+                        :a5_blocker, :a5_critical, :a5_major, :a5_info, :a5_minor,
+                        :a6_blocker, :a6_critical, :a6_major, :a6_info, :a6_minor,
+                        :a7_blocker, :a7_critical, :a7_major, :a7_info, :a7_minor,
+                        :a8_blocker, :a8_critical, :a8_major, :a8_info, :a8_minor,
+                        :a9_blocker, :a9_critical, :a9_major, :a9_info, :a9_minor,
+                        :a10_blocker, :a10_critical, :a10_major, :a10_info, :a10_minor)";
+                    $stmt=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnline, " ", $sql));
+
+                    foreach ($map as $key => $value) {
+                        if ($value instanceof \DateTime) {
+                            $stmt->bindValue(":$key", $value->format('Y-m-d H:i:s'));
+                        } else {
+                            $stmt->bindValue(":$key", $value);
+                        }
+                    }
+                    $stmt->executeStatement();
+            $this->getEntityManager()->getConnection()->commit();
+        } catch (\Doctrine\DBAL\Exception $e) {
+            $this->getEntityManager()->getConnection()->rollBack();
             return ['code'=>500, 'erreur'=> $e->getMessage()];
         }
         return ['code'=>200, 'erreur'=>''];
