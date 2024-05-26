5be8bad2675150e7061215ccf5233d8a588afe26
Laurent HADJADJ <laurent_h@me.com>
2024-05-26 08:06:54 +0200

    Fix: Suppression des dépendances inutiles

--- src/Controller/Batch/BatchCollecteInformationProjetController.php
+++ src/Controller/Batch/BatchCollecteInformationProjetController.php
@@ -15,10 +15,6 @@ namespace App\Controller\Batch;
 
 /** Core */
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 /** Accès aux tables */
 use Doctrine\ORM\EntityManagerInterface;
