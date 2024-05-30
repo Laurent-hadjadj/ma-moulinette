734cfc68e435bfdbb5f9a2a698c3ff225237aa16
Laurent HADJADJ <laurent_h@me.com>
2024-05-30 11:52:30 +0200

    Refactor: Ajout de la requête insertAnomalie

--- src/Repository/AnomalieRepository.php
+++ src/Repository/AnomalieRepository.php
@@ -113,4 +113,57 @@ class AnomalieRepository extends ServiceEntityRepository
         return ['code'=>200, 'liste'=>$liste, 'erreur'=>''];
     }
 
+    /**
+     * [Description for insertAnomalie]
+     *
+     * @param array $map
+     *
+     * @return array
+     *
+     * Created at: 29/05/2024 18:15:21 (Europe/Paris)
+     * @author     Laurent HADJADJ <laurent_h@me.com>
+     * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function insertAnomalie($map):array
+    {
+        try {
+                $this->getEntityManager()->getConnection()->beginTransaction();
+                    $sql = "INSERT INTO anomalie
+                                (maven_key, project_name, anomalie_total, dette_minute, dette_reliability_minute, dette_vulnerability_minute, dette_code_smell_minute, dette, dette_reliability, dette_vulnerability, dette_code_smell, frontend, backend, autre, blocker, critical, major, info, minor, bug, vulnerability, code_smell, date_enregistrement)
+                            VALUES
+                                (:maven_key, :project_name, :anomalie_total, :dette_minute, :dette_reliability_minute, :dette_vulnerability_minute, :dette_code_smell_minute, :dette, :dette_reliability, :dette_vulnerability, :dette_code_smell, :frontend, :backend, :autre, :blocker, :critical, :major, :info, :minor, :bug, :vulnerability, :code_smell, :date_enregistrement)";
+                    $stmt=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnline, " ", $sql));
+                        $stmt->bindValue(':maven_key', $map['maven_key']);
+                        $stmt->bindValue(':project_name', $map['project_name']);
+                        $stmt->bindValue(':anomalie_total', $map['anomalie_total']);
+                        $stmt->bindValue(':dette', $map['dette']);
+                        $stmt->bindValue(':dette_reliability', $map['dette_reliability']);
+                        $stmt->bindValue(':dette_vulnerability', $map['dette_vulnerability']);
+                        $stmt->bindValue(':dette_code_smell', $map['dette_code_smell']);
+                        $stmt->bindValue(':dette_minute', $map['dette_minute']);
+                        $stmt->bindValue(':dette_reliability_minute', $map['dette_reliability_minute']);
+                        $stmt->bindValue(':dette_vulnerability_minute', $map['dette_vulnerability_minute']);
+                        $stmt->bindValue(':dette_code_smell_minute', $map['dette_code_smell_minute']);
+                        $stmt->bindValue(':frontend', $map['frontend']);
+                        $stmt->bindValue(':backend', $map['backend']);
+                        $stmt->bindValue(':autre', $map['autre']);
+                        $stmt->bindValue(':blocker', $map['blocker']);
+                        $stmt->bindValue(':critical', $map['critical']);
+                        $stmt->bindValue(':major', $map['major']);
+                        $stmt->bindValue(':info', $map['info']);
+                        $stmt->bindValue(':minor', $map['minor']);
+                        $stmt->bindValue(':bug', $map['bug']);
+                        $stmt->bindValue(':vulnerability', $map['vulnerability']);
+                        $stmt->bindValue(':code_smell', $map['code_smell']);
+                        /** on formate la date avant de l'enregistrer */
+                        $stmt->bindValue(':date_enregistrement', $map['date_enregistrement']->format('Y-m-d H:i:sO'));
+                        $stmt->executeStatement();
+                $this->getEntityManager()->getConnection()->commit();
+        } catch (\Doctrine\DBAL\Exception $e) {
+            $this->getEntityManager()->getConnection()->rollBack();
+            return ['code'=>500, 'erreur'=> $e->getMessage()];
+        }
+        return ['code'=>200, 'erreur'=>''];
+    }
+
 }
