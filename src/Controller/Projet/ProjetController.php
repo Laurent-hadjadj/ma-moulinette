1188db4b6e06908321970460978f5f37546d5842
Laurent HADJADJ <laurent_h@me.com>
2024-05-31 20:34:23 +0200

    Fix: Orthographe

--- src/Controller/Projet/ProjetController.php
+++ src/Controller/Projet/ProjetController.php
@@ -17,13 +17,12 @@ namespace App\Controller\Projet;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-/** Securité */
+/** Sécurité */
 use Symfony\Bundle\SecurityBundle\Security;
 
 /** API */
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * [Description ProjetController]
