1872fb5424894198aa25ad43743f73c61a18f7c0
Laurent HADJADJ <laurent_h@me.com>
2024-06-07 13:15:14 +0200

    Refactor: Code clean

--- src/Entity/BatchTraitement.php
+++ src/Entity/BatchTraitement.php
@@ -23,107 +23,63 @@ class BatchTraitement
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column(
-        type: Types::INTEGER,
-        nullable: false,
-        options: ['comment' => 'Identifiant unique pour la table Batch Traitemeny']
-    )]
+    #[ORM\Column(type: Types::INTEGER, nullable: false,
+        options: ['comment' => 'Identifiant unique pour la table Batch Traitemeny'])]
     private $id;
 
-    #[ORM\Column(
-        type: Types::STRING,
-        length: 16,
-        nullable: false,
-        options: ['comment' => 'Mode de démarrage du traitement']
-    )]
+    #[ORM\Column(type: Types::STRING, length: 16, nullable: false,
+        options: ['comment' => 'Mode de démarrage du traitement'])]
     ##[Assert\Choice(choices: ["Manuel", "Automatique"], message: "Le démarrage doit être 'Manuel' ou 'Automatique'")]
     #[Assert\NotBlank]
     private $demarrage = "Manuel";
 
-    #[ORM\Column(
-        type: 'boolean',
-        nullable: false,
-        options: ['comment' => 'Indique si le traitement a réussi ou échoué']
-    )]
-    #[Assert\Type(
-        type: 'bool',
-        message: "Le résultat doit être un booléen."
-    )]
+    #[ORM\Column(type: 'boolean', nullable: false,
+        options: ['comment' => 'Indique si le traitement a réussi ou échoué'])]
+    #[Assert\Type(type: 'bool',
+        message: "Le résultat doit être un booléen.")]
     #[Assert\NotNull]
     private $resultat = false;
 
-    #[ORM\Column(
-        type: Types::STRING,
-        length: 32,
-        nullable: false,
-        options: ['comment' => 'Titre du traitement']
-    )]
+    #[ORM\Column(type: Types::STRING, length: 32, nullable: false,
+        options: ['comment' => 'Titre du traitement'])]
     #[Assert\NotBlank]
-    #[Assert\Length(
-        max: 32,
-        maxMessage: "Le titre ne doit pas dépasser 32 caractères."
-    )]
+    #[Assert\Length(max: 32,
+        maxMessage: "Le titre ne doit pas dépasser 32 caractères.")]
     private $titre;
 
-    #[ORM\Column(
-        type: Types::STRING,
-        length: 32,
-        nullable: false,
-        options: ['comment' => 'Nom du portefeuille de projets associé']
-    )]
+    #[ORM\Column(type: Types::STRING, length: 32, nullable: false,
+        options: ['comment' => 'Nom du portefeuille de projets associé'])]
     #[Assert\NotBlank]
-    #[Assert\Length(
-        max: 32,
-        maxMessage: "Le nom du portefeuille ne doit pas dépasser 32 caractères."
-    )]
+    #[Assert\Length(max: 32,
+        maxMessage: "Le nom du portefeuille ne doit pas dépasser 32 caractères.")]
     private $portefeuille = "Aucun";
 
-    #[ORM\Column(
-        type: Types::INTEGER,
-        nullable: false,
-        options: ['comment' => 'Nombre de projets traités']
-    )]
+    #[ORM\Column(type: Types::INTEGER, nullable: false,
+        options: ['comment' => 'Nombre de projets traités'])]
     #[Assert\NotNull]
-    #[Assert\Type(
-        type: 'integer',
-        message: "Le nombre de projets doit être un entier."
-    )]
+    #[Assert\Type(type: 'integer',
+        message: "Le nombre de projets doit être un entier.")]
     private $nombreProjet = 0;
 
-    #[ORM\Column(
-        type: Types::STRING,
-        length: 128,
-        nullable: false,
-        options: ['comment' => 'Responsable du traitement']
-    )]
+    #[ORM\Column(type: Types::STRING, length: 128, nullable: false,
+        options: ['comment' => 'Responsable du traitement'])]
     #[Assert\NotBlank]
-    #[Assert\Length(
-        max: 128,
-        maxMessage: "Le nom du responsable ne doit pas dépasser 128 caractères."
-    )]
+    #[Assert\Length(max: 128,
+        maxMessage: "Le nom du responsable ne doit pas dépasser 128 caractères.")]
     private $responsable;
 
-    #[ORM\Column(
-        type: Types::DATETIMETZ_IMMUTABLE,
-        nullable: true,
-        options: ['comment' => 'Date et heure de début du traitement']
-    )]
+    #[ORM\Column(type: Types::DATETIMETZ_IMMUTABLE, nullable: true,
+        options: ['comment' => 'Date et heure de début du traitement'])]
     #[Assert\NotNull]
     private $debutTraitement;
 
-    #[ORM\Column(
-        type: Types::DATETIMETZ_IMMUTABLE,
-        nullable: true,
-        options: ['comment' => 'Date et heure de fin du traitement']
-    )]
+    #[ORM\Column(type: Types::DATETIMETZ_IMMUTABLE, nullable: true,
+        options: ['comment' => 'Date et heure de fin du traitement'])]
     #[Assert\NotNull]
     private $finTraitement;
 
-    #[ORM\Column(
-        type: Types::DATETIMETZ_IMMUTABLE,
-        nullable: false,
-        options: ['comment' => 'Date d’enregistrement du traitement dans le système']
-    )]
+    #[ORM\Column(type: Types::DATETIMETZ_IMMUTABLE, nullable: false,
+        options: ['comment' => 'Date d’enregistrement du traitement dans le système'])]
     #[Assert\NotNull]
     private $dateEnregistrement;
 
