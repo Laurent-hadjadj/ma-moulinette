85752dc120b6bae3062b0d32f5e5a8a607957b47
Laurent HADJADJ <laurent_h@me.com>
2024-06-18 19:07:46 +0200

    Refactor: Suppression de API Plateform

--- config/bundles.php
+++ config/bundles.php
@@ -14,6 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
