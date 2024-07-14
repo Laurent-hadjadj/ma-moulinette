d35f22601d0ce8b40a7c87a3d0de1cf63585b663
Laurent HADJADJ <laurent_h@me.com>
2024-07-14 21:16:40 +0200

    Refactor: Ajout de la colonne language_distribution.

--- src/Repository/MesuresRepository.php
+++ src/Repository/MesuresRepository.php
@@ -74,14 +74,15 @@ class MesuresRepository extends ServiceEntityRepository
         try {
                 $this->getEntityManager()->getConnection()->beginTransaction();
                     $sql = "INSERT INTO mesures
-                                (maven_key, project_name, lines, ncloc, sqale_debt_ratio, coverage, duplication_density, tests, issues, mode_collecte, utilisateur_collecte, date_enregistrement)
+                                (maven_key, project_name, lines, ncloc, language_distribution, sqale_debt_ratio, coverage, duplication_density, tests, issues, mode_collecte, utilisateur_collecte, date_enregistrement)
                             VALUES
-                                (:maven_key, :project_name, :lines, :ncloc, :sqale_debt_ratio, :coverage, :duplication_density, :tests, :issues, :mode_collecte, :utilisateur_collecte, :date_enregistrement)";
+                                (:maven_key, :project_name, :lines, :ncloc, :language_distribution, :sqale_debt_ratio, :coverage, :duplication_density, :tests, :issues, :mode_collecte, :utilisateur_collecte, :date_enregistrement)";
                     $stmt=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnLine, " ", $sql));
                         $stmt->bindValue(':maven_key', $map['maven_key']);
                         $stmt->bindValue(':project_name', $map['project_name']);
                         $stmt->bindValue(':lines', $map['lines']);
                         $stmt->bindValue(':ncloc', $map['ncloc']);
+                        $stmt->bindValue(':language_distribution', $map['language_distribution']);
                         $stmt->bindValue(':sqale_debt_ratio', $map['sqale_debt_ratio']);
                         $stmt->bindValue(':coverage', $map['coverage']);
                         $stmt->bindValue(':duplication_density', $map['duplication_density']);
