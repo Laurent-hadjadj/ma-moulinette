23b03f2ed765a1f52aaec729545ca79631ca221f
Laurent HADJADJ <laurent_h@me.com>
2024-07-17 11:30:19 +0200

    Refactor: Ajout des traces SQL.

--- src/Exception/FatchDataexception.php
+++ src/Exception/FatchDataexception.php
@@ -11,15 +11,22 @@ use Exception;
  */
 class FetchDataException extends Exception
 {
+    private $debug;
     private $render;
 
-    public function __construct($message, $render)
+    public function __construct(string $message, string $debug, array $render, int $code = 0, Exception $previous = null)
     {
-        parent::__construct($message);
+        parent::__construct($message, $code, $previous);
+        $this->debug = $debug;
         $this->render = $render;
     }
 
-    public function getRender()
+    public function getDebug(): string
+    {
+        return $this->debug;
+    }
+
+    public function getRender(): array
     {
         return $this->render;
     }
