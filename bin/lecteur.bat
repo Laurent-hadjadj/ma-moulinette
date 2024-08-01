ed0d83639e4719604c50f9d8fba3e0105490f5d2
Laurent HADJADJ <laurent_h@me.com>
2024-08-01 16:52:43 +0200

    Refactor: Ajout du fichier lecteur.bat (à déplacer)

---
+++ bin/lecteur.bat
@@ -0,0 +1,12 @@
+@echo off
+@mode con: cols=160 lines=70
+@color 0f
+@CHCP 65001
+@cls
+@echo ###                                                                           ###
+@echo ### Atention le fichier doit être encodé en UTF-8 avec une séquence de fin de ###
+@echo ### ligne Windows (CRLF).                                                     ###
+@echo ###                                                                           ###
+echo:
+REM Lecteur par défaut c:
+@set lecteur=c:
