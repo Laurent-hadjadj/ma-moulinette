b2c227e664ae9932726fe05604ab61d5e87f680a
Laurent HADJADJ <laurent_h@me.com>
2024-08-01 22:18:54 +0200

    Refactor: UTILISATEUR -> COLLECTE

--- src/Controller/Batch/BatchCollecteInformationProjetController.php
+++ src/Controller/Batch/BatchCollecteInformationProjetController.php
@@ -212,7 +212,7 @@ class BatchCollecteInformationProjetController extends AbstractController
         $keyAnalyseSonarQube=$result['key'];
 
         /** 02 - Version  Locale, on prend la version historique par défaut */
-        if ($isValide['code']!=202 && $modeCollecte!='UTILISATEUR'){
+        if ($isValide['code']!=202 && $modeCollecte!='COLLECTE'){
             $local=$isValide['data-baseHistorique'] ?? $isValide['data-baseInformation'];
             $versionLocale=$local['version'] ?? $local['projectVersion'] ?? 'VIDE';
             $dateAnalyseLocale=$local['date_version'] ?? $local['date'] ?? 'VIDE';
