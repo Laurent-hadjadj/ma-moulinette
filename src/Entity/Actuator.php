a4787a26535cde65ff18031476494d801b5f6b75
Laurent HADJADJ <laurent_h@me.com>
2024-08-01 14:53:21 +0200

    Refactor: Code clean

--- src/Entity/Actuator.php
+++ src/Entity/Actuator.php
@@ -1,15 +1,15 @@
 <?php
 
 /*
- *  Ma-Moulinette
- *  --------------
- *  Copyright (c) 2021-2022.
- *  Laurent HADJADJ <laurent_h@me.com>.
- *  Licensed Creative Common  CC-BY-NC-SA 4.0.
- *  ---
- *  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
- *  http://creativecommons.org/licenses/by-nc-sa/4.0/
- */
+*  Ma-Moulinette
+*  --------------
+*  Copyright (c) 2021-2022.
+*  Laurent HADJADJ <laurent_h@me.com>.
+*  Licensed Creative Common  CC-BY-NC-SA 4.0.
+*  ---
+*  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+*  http://creativecommons.org/licenses/by-nc-sa/4.0/
+*/
 
 namespace App\Entity;
 
@@ -20,77 +20,62 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Validator\Constraints as Assert;
 
-
 #[ORM\Entity(repositoryClass: ActuatorRepository::class)]
 #[ORM\Table(name: "actuator", schema: "ma_moulinette")]
-/* @ORM\HasLifecycleCallbacks() */
+#[ORM\HasLifecycleCallbacks]
 class Actuator
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column(type: Types::INTEGER, nullable: false,
-        options: ['comment' => 'Identifiant unique de la table'])]
+    #[ORM\Column(type: Types::INTEGER, nullable: false, options: ['comment' => 'Identifiant unique de la table'])]
     private $id;
 
-    #[ORM\Column(type: Types::STRING, length: 255, nullable: false,
-    options: ['comment' => 'Clé Maven du projet'])]
+    #[ORM\Column(type: Types::STRING, length: 255, nullable: false, options: ['comment' => 'Clé Maven du projet'])]
     #[Assert\NotBlank]
-    #[Assert\Length( max: 255,
-        maxMessage: "La clé Maven ne doit pas dépasser 255 caractères.")]
+    #[Assert\Length(max: 255, maxMessage: "La clé Maven ne doit pas dépasser 255 caractères.")]
     private $mavenKey;
 
-    #[ORM\Column(type: Types::STRING, length: 128, nullable: false,
-        options: ['comment' => "Nom de l'application."])]
+    #[ORM\Column(type: Types::STRING, length: 128, nullable: false, options: ['comment' => "Nom de l'application."])]
     #[Assert\NotBlank]
-    #[Assert\Length(max:128, maxMessage: "Le nom de l'application ne doit pas dépasser 128 caractères.")]
+    #[Assert\Length(max: 128, maxMessage: "Le nom de l'application ne doit pas dépasser 128 caractères.")]
     private $nomApplication;
 
-    #[ORM\Column(type: Types::STRING, length: 255, nullable: false,
-        options: ['comment' => 'URL de base du serveur.'])]
+    #[ORM\Column(type: Types::STRING, length: 255, nullable: false, options: ['comment' => 'URL de base du serveur.'])]
     #[Assert\NotBlank]
     #[Assert\Length(max: 255, maxMessage: "L'URL ne doit pas dépasser 255 caractères.")]
     private $url;
 
-    #[ORM\Column(type: Types::STRING, length: 128, nullable: true,
-        options: ['comment' => "Nom de l'utilisateur Actuator"])]
-    #[Assert\NotBlank]
+    #[ORM\Column(type: Types::STRING, length: 128, nullable: true, options: ['comment' => "Nom de l'utilisateur Actuator"])]
     #[Assert\Length(max: 128, maxMessage: "Le nom de l'utilisateur Actuator ne doit pas dépasser 128 caractères.")]
-    private ?string $actuatorUser=null;
+    private ?string $actuatorUser = null;
 
-    #[ORM\Column(type: Types::STRING, length: 128, nullable:true,
-        options: ['comment' => "Mot de passe de l'utilisateur Actuator"])]
-    #[Assert\NotBlank]
+    #[ORM\Column(type: Types::STRING, length: 128, nullable: true, options: ['comment' => "Mot de passe de l'utilisateur Actuator"])]
     #[Assert\Length(max: 128, maxMessage: "Le mot de passe de l'utilisateur Actuator ne doit pas dépasser 128 caractères.")]
-    private ?string $actuatorPassword=null;
+    private ?string $actuatorPassword = null;
 
-    #[ORM\Column(type: Types::STRING, length: 128, nullable: false,
-        options: ['comment' => "Prénom et nom de l'utilisateur"])]
+    #[ORM\Column(type: Types::STRING, length: 128, nullable: false, options: ['comment' => "Prénom et nom de l'utilisateur"])]
     #[Assert\NotBlank]
     #[Assert\Length(max: 128, maxMessage: "Le prénom et nom de l'utilisateur ne doit pas dépasser 128 caractères.")]
     private ?string $personne;
 
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true, options: ['comment' => 'Date de la dernière modification.'])]
+    private ?\DateTimeInterface $dateModification = null;
 
-    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true,
-    options: ['comment' => 'Date de la dernière modification.'])]
-    private $dateModification;
-
-    #[ORM\Column(type: Types::DATETIMETZ_IMMUTABLE, nullable: false,
-        options: ['comment' => 'Date d’enregistrement.'])]
-    private $dateEnregistrement;
+    #[ORM\Column(type: Types::DATETIMETZ_IMMUTABLE, nullable: false, options: ['comment' => 'Date d’enregistrement.'])]
+    private \DateTimeImmutable $dateEnregistrement;
 
     /**
      * @var Collection<int, ActuatorInfo>
-     */
+    */
     #[ORM\OneToMany(mappedBy: "actuator", targetEntity: ActuatorInfo::class, orphanRemoval: true, cascade: ["persist"])]
     #[Assert\Count(min: 1)]
     #[Assert\Valid]
     private $actuatorInfo;
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\ActuatorInfo", mappedBy="actuator")
-     */
+
     public function __construct()
     {
         $this->actuatorInfo = new ArrayCollection();
+        $this->dateEnregistrement = new \DateTimeImmutable();
     }
 
     public function getId(): ?int
@@ -139,7 +124,7 @@ class Actuator
         return $this->actuatorUser;
     }
 
-    public function setActuatorUser(string $actuatorUser): static
+    public function setActuatorUser(?string $actuatorUser): static
     {
         $this->actuatorUser = $actuatorUser;
 
@@ -151,7 +136,7 @@ class Actuator
         return $this->actuatorPassword;
     }
 
-    public function setActuatorPassword(string $actuatorPassword): static
+    public function setActuatorPassword(?string $actuatorPassword): static
     {
         $this->actuatorPassword = $actuatorPassword;
 
@@ -182,9 +167,7 @@ class Actuator
         return $this;
     }
 
-    /**
-     * @ORM\PreUpdate()
-     */
+    #[ORM\PreUpdate]
     public function preUpdate(): void
     {
         $this->dateModification = new \DateTimeImmutable();
@@ -195,14 +178,14 @@ class Actuator
         return $this->dateEnregistrement;
     }
 
-    public function setDateEnregistrement(): void
+    public function setDateEnregistrement(\DateTimeImmutable $dateEnregistrement): void
     {
-        $this->dateEnregistrement = new \DateTimeImmutable();
+        $this->dateEnregistrement = $dateEnregistrement;
     }
 
     /**
      * @return Collection<int, ActuatorInfo>
-     */
+    */
     public function getActuatorInfo(): Collection
     {
         return $this->actuatorInfo;
@@ -229,5 +212,4 @@ class Actuator
 
         return $this;
     }
-
 }
