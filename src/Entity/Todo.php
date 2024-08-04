994f96883ff6661773384a3f87c03c60a0924bed
Laurent HADJADJ <laurent_h@me.com>
2024-08-04 13:30:02 +0200

    Fix: Corrections commentaires + longueur de la colonne utilisateurCollecte.

--- src/Entity/Todo.php
+++ src/Entity/Todo.php
@@ -51,15 +51,15 @@ class Todo
     private $line;
 
     #[ORM\Column(type: Types::STRING, length: 32, nullable: true,
-    options: ['comment' => 'Mode de collete : MANUEL | AUTOMATIQUE'])]
+    options: ['comment' => 'Mode de collecte : MANUEL | AUTOMATIQUE'])]
     #[Assert\Length(max: 32,
         maxMessage: "Le mode de collecte ne peut pas dépasser 32 caractères.")]
     private ?string $modeCollecte=null;
 
-    #[ORM\Column(type: Types::STRING, length: 128, nullable: true,
-    options: ['comment' => "Nom de l'utilisateur qui a réalisé la collecte."])]
-    #[Assert\Length(max: 128,
-        maxMessage: "Le nom de l'utilisatzeur ne peut pas dépasser 128 caractères.")]
+    #[ORM\Column(type: Types::STRING, length: 320, nullable: true,
+    options: ['comment' => "Le compte de l'utilisateur qui a réalisé la collecte."])]
+    #[Assert\Length(max: 320,
+        maxMessage: "Le compte de l'utilisateur ne peut pas dépasser 320 caractères.")]
     private ?string $utilisateurCollecte=null;
 
     #[ORM\Column(type: Types::DATETIMETZ_IMMUTABLE, nullable: false,
