3249ab9d16c81d8551f45496738e11edb684baae
Laurent HADJADJ <laurent_h@me.com>
2024-08-06 18:56:29 +0200

    Refactor: Mise à jour des TU noSonarRepository

--- tests/Unit/Repository/NoSonarRepositoryTest.php
+++ tests/Unit/Repository/NoSonarRepositoryTest.php
@@ -28,19 +28,23 @@ class NoSonarRepositoryTest extends KernelTestCase
 {
 
     private static $mavenKey = 'fr.ma-petite-entreprise:ma-moulinette';
-    private static $rule = 'java:S1309';
-    private static $component = 'fr.ma-petite-entreprise:mo-moulinette:
-    ma-moulinette-service/src/main/java/fr/ma-petite-entreprise/ma-moulinette/service/ClamAvService.java';
-    private static $line = 118;
-    private static $dateEnregistrement = '2024-03-26 14:46:38';
-
-    private static $erreurCode200 = 'Erreur le code retour doit être 200';
+    private static $erreurCode200 = 'Erreur le code retour doit être 200.';
 
     protected function setUp(): void
     {
         self::bootKernel();
         $container = static::getContainer();
         $entityManager = $container->get('doctrine')->getManager();
+
+        // Réinitialiser la séquence
+        $connection = $entityManager->getConnection();
+        $platform = $connection->getDatabasePlatform('SET search_path TO ma_moulinette_test');
+
+        if ($platform instanceof \Doctrine\DBAL\Platforms\PostgreSqlPlatform) {
+            $sequence = 'ma_moulinette.no_sonar_id_seq';
+            $connection->executeQuery("SELECT setval('$sequence', 1, false);");
+        }
+
         $purger = new ORMPurger($entityManager);
         $executor = new ORMExecutor($entityManager, $purger);
         $executor->execute([new NoSonarFixtures()]);
@@ -92,9 +96,11 @@ class NoSonarRepositoryTest extends KernelTestCase
         $container = static::getContainer();
         $entityManager = $container->get('doctrine')->getManager();
 
-        $map = ['maven_key' => static::$mavenKey, 'rule'=>static::$rule,
-                'component'=>static::$component, 'line' => static::$line,
-                'date_enregistrement'=> static::$dateEnregistrement ];
+        $map = [['maven_key' => static::$mavenKey, 'rule' => 'java:S1309',
+                'component'=> 'fr.ma-petite-entreprise:mo-moulinette:
+                ma-moulinette-service/src/main/java/fr/ma-petite-entreprise/ma-moulinette/service/ClamAvService.java', 'line' => 118,
+                'mode_collecte' => 'TRAITEMENT MANUEL','utilisateur_collecte' => 'laurent.hadjadj@ma-petite-entreprise.fr',
+                'date_enregistrement'=> new \DateTimeImmutable('2024-03-26 14:46:38')]];
 
         // Appel de la méthode
         $nosonarRepository = $entityManager->getRepository(NoSonar::class);
@@ -105,5 +111,15 @@ class NoSonarRepositoryTest extends KernelTestCase
         $this->assertEmpty($r['erreur'], $r['erreur']);
     }
 
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        // On se déconnecte pour éviter des problèmes de mémoires
+        $container = static::getContainer();
+        $entityManager = $container->get('doctrine')->getManager();
+        $entityManager->close();
+        $entityManager = null;
+    }
 
 }
