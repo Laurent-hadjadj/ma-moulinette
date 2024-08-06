f208d5620f31f33cfa125421028532158777ba20
Laurent HADJADJ <laurent_h@me.com>
2024-08-06 19:50:30 +0200

    Refactor: Mise à jour des TU portefeuilleRepository.

--- tests/Unit/Repository/PorteFeuilleRepositoryTest.php
+++ tests/Unit/Repository/PorteFeuilleRepositoryTest.php
@@ -26,14 +26,23 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 class PortefeuilleRepositoryTest extends KernelTestCase
 {
     private static $portefeuille = 'MES PROJETS';
-
-    private static $erreurCode200 = 'Erreur le code retour doit être 200';
+    private static $erreurCode200 = 'Erreur le code retour doit être 200.';
 
     protected function setUp(): void
     {
         self::bootKernel();
         $container = static::getContainer();
         $entityManager = $container->get('doctrine')->getManager();
+
+        // Réinitialiser la séquence
+        $connection = $entityManager->getConnection();
+        $platform = $connection->getDatabasePlatform('SET search_path TO ma_moulinette_test');
+
+        if ($platform instanceof \Doctrine\DBAL\Platforms\PostgreSqlPlatform) {
+            $sequence = 'ma_moulinette.portefeuille_id_seq';
+            $connection->executeQuery("SELECT setval('$sequence', 1, false);");
+        }
+
         $purger = new ORMPurger($entityManager);
         $executor = new ORMExecutor($entityManager, $purger);
         $executor->execute([new PortefeuilleFixtures()]);
@@ -56,6 +65,17 @@ class PortefeuilleRepositoryTest extends KernelTestCase
         // Assert
         $this->assertEquals(200, $r['code'], static::$erreurCode200);
         $this->assertEmpty($r['erreur'], $r['erreur']);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        // On se déconnecte pour éviter des problèmes de mémoires
+        $container = static::getContainer();
+        $entityManager = $container->get('doctrine')->getManager();
+        $entityManager->close();
+        $entityManager = null;
     }
 
 }
