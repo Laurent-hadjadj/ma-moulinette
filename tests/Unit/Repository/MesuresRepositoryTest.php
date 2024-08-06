d8da3ced7f420140d4b903d92accc900ce2287a9
Laurent HADJADJ <laurent_h@me.com>
2024-08-06 18:40:59 +0200

    Refactor: Code clean, ajout des TU pour insertMesures et deleteMesuresMavenKey

--- tests/Unit/Repository/MesuresRepositoryTest.php
+++ tests/Unit/Repository/MesuresRepositoryTest.php
@@ -26,7 +26,6 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 class MesuresRepositoryTest extends KernelTestCase
 {
     private static $mavenKey = 'fr.ma-petite-entreprise:ma-moulinette';
-
     private static $erreurCode200 = 'Erreur le code retour doit être 200';
 
     protected function setUp(): void
@@ -34,6 +33,16 @@ class MesuresRepositoryTest extends KernelTestCase
         self::bootKernel();
         $container = static::getContainer();
         $entityManager = $container->get('doctrine')->getManager();
+
+        // Réinitialiser la séquence
+        $connection = $entityManager->getConnection();
+        $platform = $connection->getDatabasePlatform('SET search_path TO ma_moulinette_test');
+
+        if ($platform instanceof \Doctrine\DBAL\Platforms\PostgreSqlPlatform) {
+            $sequence = 'ma_moulinette.mesures_id_seq';
+            $connection->executeQuery("SELECT setval('$sequence', 1, false);");
+        }
+
         $purger = new ORMPurger($entityManager);
         $executor = new ORMExecutor($entityManager, $purger);
         $executor->execute([new MesuresFixtures()]);
@@ -58,5 +67,57 @@ class MesuresRepositoryTest extends KernelTestCase
         $this->assertEmpty($r['erreur'], $r['erreur']);
     }
 
+    public function testInsertMesures(): void
+    {
+        // Connexion à la base de données
+        self::bootKernel();
+        /* On se connecte à la base de tests */
+        $container = static::getContainer();
+        $entityManager = $container->get('doctrine')->getManager();
+
+        $map = ['maven_key' => static::$mavenKey,
+                'project_name' => 'Ma-Moulinette', 'lines' => 22015,
+                'ncloc' => 10043, 'language_distribution' => ['java'=>4278, 'ts'=>18690], 'coverage' => 10.3, 'duplicated_lines_density' => 5.1, 'sqale_debt_ratio' => 26.0, 'issues' => 200, 'tests' => 123,
+                'mode_collecte' => 'TRAITEMENT MANUEL','utilisateur_collecte' => 'laurent.hadjadj@ma-petite-entreprise.fr',
+                'date_enregistrement' => new \DateTimeImmutable('2024-04-12 16:23:11')];
+
+        // Appel de la méthode
+        $mesuresRepository = $entityManager->getRepository(Mesures::class);
+        $r = $mesuresRepository->insertMesures($map);
+
+        // Assert
+        $this->assertEquals(200, $r['code'], static::$erreurCode200);
+        $this->assertEmpty($r['erreur'], $r['erreur']);
+    }
+
+    public function testDeleteMesuresMavenKey(): void
+    {
+        // Connexion à la base de données
+        self::bootKernel();
+        /* On se connecte à la base de tests */
+        $container = static::getContainer();
+        $entityManager = $container->get('doctrine')->getManager();
+
+        $map = ['maven_key' => static::$mavenKey];
+
+        // Appel de la méthode
+        $mesuresRepository = $entityManager->getRepository(Mesures::class);
+        $r = $mesuresRepository->deleteMesuresMavenKey($map);
+
+        // Assert
+        $this->assertEquals(200, $r['code'], static::$erreurCode200);
+        $this->assertEmpty($r['erreur'], $r['erreur']);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        // On se déconnecte pour éviter des problèmes de mémoires
+        $container = static::getContainer();
+        $entityManager = $container->get('doctrine')->getManager();
+        $entityManager->close();
+        $entityManager = null;
+    }
 
 }
