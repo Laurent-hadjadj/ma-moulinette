c9ab165916e526abf3729de6c5da100b87fb0933
Laurent HADJADJ <laurent_h@me.com>
2024-08-06 16:08:14 +0200

    Refactor: Ajout des TU pour LoggerRepository

---
+++ tests/Unit/Repository/LoggerRepositoryTest.php
@@ -0,0 +1,116 @@
+<?php
+
+/*
+ *  Ma-Moulinette
+ *  --------------
+ *  Copyright (c) 2021-2022.
+ *  Laurent HADJADJ <laurent_h@me.com>.
+ *  Licensed Creative Common  CC-BY-NC-SA 4.0.
+ *  ---
+ *  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+ *  http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+namespace App\Tests\Unit\Repository;
+
+use App\Entity\Logger;
+use App\DataFixtures\LoggerFixtures;
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * [Description LoggerRepositoryTest]
+ */
+class LoggerRepositoryTest extends KernelTestCase
+{
+
+    private static $erreurCode200 = 'Erreur le code retour doit être 200.';
+
+    protected function setUp(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        $entityManager = $container->get('doctrine')->getManager();
+
+        // Réinitialiser la séquence
+        $connection = $entityManager->getConnection();
+        $platform = $connection->getDatabasePlatform('SET search_path TO ma_moulinette_test');
+
+        if ($platform instanceof \Doctrine\DBAL\Platforms\PostgreSqlPlatform) {
+            $sequence = 'ma_moulinette.logger_id_seq';
+            $connection->executeQuery("SELECT setval('$sequence', 1, false);");
+        }
+
+        $purger = new ORMPurger($entityManager);
+        $executor = new ORMExecutor($entityManager, $purger);
+        $executor->execute([new LoggerFixtures()]);
+    }
+
+    public function testDeleteLoggerMavenKey(): void
+    {
+        // Connexion à la base de données
+        self::bootKernel();
+        /* On se connecte à la base de tests */
+        $container = static::getContainer();
+        $entityManager = $container->get('doctrine')->getManager();
+
+        // Appel de la méthode
+        $map=['maven_key' => 'fr.ma-petite-entreprise:ma-moulinette'];
+
+        $notesRepository = $entityManager->getRepository(Logger::class);
+        $r = $notesRepository->deleteLoggerMavenKey($map);
+
+        // Assert
+        $this->assertEquals(200, $r['code'], static::$erreurCode200);
+        $this->assertEmpty($r['erreur'], $r['erreur']);
+    }
+
+    public function testSelectLogger(): void
+    {
+        // Connexion à la base de données
+        self::bootKernel();
+        /* On se connecte à la base de tests */
+        $container = static::getContainer();
+        $entityManager = $container->get('doctrine')->getManager();
+
+        // Appel de la méthode
+        $map=['maven_key' => 'fr.ma-petite-entreprise:ma-moulinette'];
+
+        $loggerRepository = $entityManager->getRepository(Logger::class);
+        $r = $loggerRepository->selectLogger($map);
+
+        // Assert
+        $this->assertEquals(200, $r['code'], static::$erreurCode200);
+        $this->assertEmpty($r['erreur'], $r['erreur']);
+    }
+
+    public function testInsertLogger(): void
+    {
+        // Connexion à la base de données
+        self::bootKernel();
+        /* On se connecte à la base de tests */
+        $container = static::getContainer();
+        $entityManager = $container->get('doctrine')->getManager();
+
+        $map=[
+            'maven_key' => 'fr.ma-petite-entreprise:ma-moulinette',
+            'logger_info' => 14,
+            'logger_warn' => 0,
+            'logger_error' => 15,
+            'logger_debug' => 8,
+            'mode_collecte' => 'TRAITEMENT AUTOMATIQUE',
+            'utilisateur_collecte' => 'laurent.hadjadj@ma-petite-entreprise.fr',
+            'date_enregistrement' => new \DateTimeImmutable('2024-03-26 14:46:38+02')];
+
+        // Appel de la méthode
+        $loggerRepository = $entityManager->getRepository(Logger::class);
+        $r = $loggerRepository->insertLogger($map);
+
+        // Assert
+        $this->assertEquals(200, $r['code'], static::$erreurCode200);
+        $this->assertEmpty($r['erreur'], $r['erreur']);
+    }
+
+}
