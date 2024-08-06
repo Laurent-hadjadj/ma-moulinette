e956477a4ebef9d8a14f61ed1d85958b4e22def5
Laurent HADJADJ <laurent_h@me.com>
2024-08-06 15:17:58 +0200

    Refactor: Code clean, ajout de la requête InsertInformationProjet

--- tests/Unit/Repository/InformationProjetRepositoryTest.php
+++ tests/Unit/Repository/InformationProjetRepositoryTest.php
@@ -27,20 +27,29 @@ class InformationProjetRepositoryTest extends KernelTestCase
 {
     private static $mavenKey = 'fr.ma-petite-entreprise:ma-moulinette';
     private static $type = 'RELEASE';
-
-    private static $erreurCode200 = 'Erreur le code retour doit être 200';
+    private static $erreurCode200 = 'Erreur le code retour doit être 200.';
 
     protected function setUp(): void
     {
         self::bootKernel();
         $container = static::getContainer();
         $entityManager = $container->get('doctrine')->getManager();
+
+        // Réinitialiser la séquence
+        $connection = $entityManager->getConnection();
+        $platform = $connection->getDatabasePlatform('SET search_path TO ma_moulinette_test');
+
+        if ($platform instanceof \Doctrine\DBAL\Platforms\PostgreSqlPlatform) {
+            $sequence = 'ma_moulinette.information_projet_id_seq';
+            $connection->executeQuery("SELECT setval('$sequence', 1, false);");
+        }
+
         $purger = new ORMPurger($entityManager);
         $executor = new ORMExecutor($entityManager, $purger);
         $executor->execute([new InformationProjetFixtures()]);
     }
 
-    public function testSelectInformationProjetisValide(): void
+    public function testSelectInformationProjetIsValide(): void
     {
         // Connexion à la base de données
         self::bootKernel();
@@ -52,7 +61,7 @@ class InformationProjetRepositoryTest extends KernelTestCase
 
         // Appel de la méthode
         $informationProjetRepository = $entityManager->getRepository(InformationProjet::class);
-        $r = $informationProjetRepository->selectInformationProjetisValide($map);
+        $r = $informationProjetRepository->selectInformationProjetIsValide($map);
 
         // Assert
         $this->assertEquals(200, $r['code'], static::$erreurCode200);
@@ -173,7 +182,7 @@ class InformationProjetRepositoryTest extends KernelTestCase
         $this->assertEmpty($r['erreur'], $r['erreur']);
     }
 
-    public function testdeleteInformationProjetMavenKey(): void
+    public function testDeleteInformationProjetMavenKey(): void
     {
         // Connexion à la base de données
         self::bootKernel();
@@ -190,6 +199,43 @@ class InformationProjetRepositoryTest extends KernelTestCase
         // Assert
         $this->assertEquals(200, $r['code'], static::$erreurCode200);
         $this->assertEmpty($r['erreur'], $r['erreur']);
+    }
+
+    public function testInsertInformationProjet(): void
+    {
+        // Connexion à la base de données
+        self::bootKernel();
+        /* On se connecte à la base de tests */
+        $container = static::getContainer();
+        $entityManager = $container->get('doctrine')->getManager();
+
+        $map = ['maven_key' => static::$mavenKey,
+        'analyse_key' => 'BYVyxZcQo0TJpgSeq-ph',
+        'date' => '2024-08-12 16:23:11+02',
+        'project_version' => '3.0.0-RELEASE',
+        'type' => 'RELEASE', 'mode_collecte' => 'TRAITEMENT MANUEL',
+        'utilisateur_collecte' => 'laurent.hadjadj@ma-petite-entreprise.fr',
+        'date_enregistrement' => new \DateTimeImmutable('2024-04-12 16:23:11+01')];
+
+        // Appel de la méthode
+        $informationProjetRepository = $entityManager->getRepository(InformationProjet::class);
+        $r = $informationProjetRepository->insertInformationProjet($map);
+
+        // Assert
+        $this->assertEquals(200, $r['code'], static::$erreurCode200);
+        $this->assertEmpty($r['erreur'], $r['erreur']);
+    }
+
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        // On se déconnecte pour éviter des problèmes de mémoires
+        $container = static::getContainer();
+        $entityManager = $container->get('doctrine')->getManager();
+        $entityManager->close();
+        $entityManager = null;
     }
 
 }
