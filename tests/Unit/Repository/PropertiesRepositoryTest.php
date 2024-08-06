ca541b1d2ea58d6d49839e5556b939881df7d52f
Laurent HADJADJ <laurent_h@me.com>
2024-08-06 21:43:08 +0200

    Refactor: Ajout de la requête getProperties.

--- tests/Unit/Repository/PropertiesRepositoryTest.php
+++ tests/Unit/Repository/PropertiesRepositoryTest.php
@@ -25,6 +25,7 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
  */
 class PropertiesRepositoryTest extends KernelTestCase
 {
+    private static $type = 'properties';
     private static $projetBd = 100;
     private static $projetSonar = 12;
     private static $profilBd = 12;
@@ -33,16 +34,43 @@ class PropertiesRepositoryTest extends KernelTestCase
     private static $dateModificationProjet = '2024-03-27 10:26:31';
     private static $dateModificationProfil = '2024-04-12 16:23:11';
 
-    private static $erreurCode200 = 'Erreur le code retour doit être 200';
+    private static $erreurCode200 = 'Erreur le code retour doit être 200.';
 
     protected function setUp(): void
     {
         self::bootKernel();
         $container = static::getContainer();
         $entityManager = $container->get('doctrine')->getManager();
+
+        // Réinitialiser la séquence
+        $connection = $entityManager->getConnection();
+        $platform = $connection->getDatabasePlatform('SET search_path TO ma_moulinette_test');
+
+        if ($platform instanceof \Doctrine\DBAL\Platforms\PostgreSqlPlatform) {
+            $sequence = 'ma_moulinette.properties_id_seq';
+            $connection->executeQuery("SELECT setval('$sequence', 1, false);");
+        }
+
         $purger = new ORMPurger($entityManager);
         $executor = new ORMExecutor($entityManager, $purger);
         $executor->execute([new PropertiesFixtures()]);
+    }
+
+    public function testGetProperties(): void
+    {
+        // Connexion à la base de données
+        self::bootKernel();
+        /* On se connecte à la base de tests */
+        $container = static::getContainer();
+        $entityManager = $container->get('doctrine')->getManager();
+
+        // Appel de la méthode
+        $propertiesRepository = $entityManager->getRepository(Properties::class);
+        $r = $propertiesRepository->getProperties(static::$type);
+
+        // Assert
+        $this->assertEquals(200, $r['code'], static::$erreurCode200);
+        $this->assertEmpty($r['erreur'], $r['erreur']);
     }
 
     public function testInsertProperties(): void
@@ -53,8 +81,10 @@ class PropertiesRepositoryTest extends KernelTestCase
         $container = static::getContainer();
         $entityManager = $container->get('doctrine')->getManager();
 
-        $map = ['projet_bd' => static::$projetBd, 'projet_sonar'=> static::$projetSonar,
-                'profil_bd' => static::$profilBd, 'profil_sonar'=> static::$profilSonar,
+        $map = ['projet_bd' => static::$projetBd,
+                'projet_sonar'=> static::$projetSonar,
+                'profil_bd' => static::$profilBd,
+                'profil_sonar'=> static::$profilSonar,
                 'date_creation' => static::$dateCreation,
                 'date_modification_projet' => static::$dateModificationProjet,
                 'date_modification_profil' => static::$dateModificationProfil];
@@ -76,10 +106,10 @@ class PropertiesRepositoryTest extends KernelTestCase
         $container = static::getContainer();
         $entityManager = $container->get('doctrine')->getManager();
 
-        $map = ['projet_bd' => static::$projetBd, 'projet_sonar'=> static::$projetSonar,
-                'date_modification_projet' => static::$dateModificationProjet,
-                'type' => 'properties'];
-
+        $map = ['projet_bd' => static::$projetBd,
+                'projet_sonar'=> static::$projetSonar,
+                'date_modification_projet' => new \DateTimeImmutable (static::$dateModificationProjet),
+                'type' => static::$type];
 
         // Appel de la méthode
         $propertiesRepository = $entityManager->getRepository(Properties::class);
@@ -98,9 +128,10 @@ class PropertiesRepositoryTest extends KernelTestCase
         $container = static::getContainer();
         $entityManager = $container->get('doctrine')->getManager();
 
-        $map = ['profil_bd' => static::$profilBd, 'profil_sonar'=> static::$profilSonar,
-                'date_modification_profil' => static::$dateModificationProfil,
-                'type' => 'properties'];
+        $map = ['profil_bd' => static::$profilBd,
+                'profil_sonar'=> static::$profilSonar,
+                'date_modification_profil' => new \DateTimeImmutable(static::$dateModificationProfil),
+                'type' => static::$type];
 
         // Appel de la méthode
         $propertiesRepository = $entityManager->getRepository(Properties::class);
@@ -111,5 +142,15 @@ class PropertiesRepositoryTest extends KernelTestCase
         $this->assertEmpty($r['erreur'], $r['erreur']);
     }
 
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        // On se déconnecte pour éviter des problèmes de mémoires
+        $container = static::getContainer();
+        $entityManager = $container->get('doctrine')->getManager();
+        $entityManager->close();
+        $entityManager = null;
+    }
 
 }
