879365c9f263399d6b4f61fbfe4a3f52f335d91d
Laurent HADJADJ <laurent_h@me.com>
2024-11-12 15:54:54 +0100

    Migration SF7: Mise à jour du script JS pour le composant Cosui.

---
+++ assets/js/mon-application/cosui/index-cosui.js
@@ -0,0 +1,236 @@
+/**
+ *  Ma-Moulinette
+ *  --------------
+ *  Copyright (c) 2021-2024.
+ *  Laurent HADJADJ <laurent_h@me.com>.
+ *  Licensed Creative Common  CC-BY-NC-SA 4.0.
+ *  ---
+ *  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+ *  http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+/** Import des dépendances */
+import 'foundation-sites/dist/css/foundation.min.css';
+import 'motion-ui/dist/motion-ui.css';
+import '../../../styles/common/common.css';
+import '../../../styles/common/police.css';
+import '../../../styles/mon-application/cosui.css';
+
+/** Intégration de jquery */
+import $ from 'jquery';
+window.$ = $;
+
+import 'what-input';
+import 'foundation-sites';
+import 'motion-ui';
+import '../../common/foundation.js';
+import '../../auth/details.js';
+
+/** On importe les constantes */
+import { zero, dix, vingt, trente, quarante, cinquante, soixante, soixanteDix, cent, quatreVingt } from '../../common/constante.js';
+
+import {Chart, registerables} from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+/** On enregistre les classes et les plugins dans chart.js */
+Chart.register(...registerables);
+Chart.register(ChartDataLabels);
+
+const oui_non = '#js-oui-non';
+const upDownEqual = '.up, .down, .equal';
+
+/**
+ * The function `dessineMoiUnRadar(dataset, labels)` is responsible for drawing a radar chart using the Chart.js library.
+ *
+ * @function
+ * @name dessineMoiUnRadar
+ * @kind function
+ * @param {any} dataset
+ * @param {any} labels
+ * @returns {void}
+ */
+const dessineMoiUnRadar = function (dataset1, dataset2, label1, label2){
+  /** Préparation du radar */
+  const data = {
+    labels: ['Fiabilité','Vulnérabilité','Hotspot', 'Maintenabilité','Couverture','Dette'],  font: { size: 18 } ,
+
+    datasets: [
+    {
+      label:'v'+label1,
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255, 99, 132, 0.5)',
+      pointBackgroundColor: '#FF6384',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: '#FF6384',
+      data: dataset1,
+      fill: true,
+      tension: 0.2 },
+      {
+        label:'v'+label2,
+        backgroundColor: 'rgba(0, 68, 91, 0.2)',
+        borderColor: 'rgba(0, 68, 91, 0.5)',
+        pointBackgroundColor: '#00445b',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: '#00445b',
+        data: dataset2,
+        fill: true,
+        tension: 0.2 } ]};
+
+    const options = {
+      //aspectRatio:2,
+      //maintainAspectRatio: true,
+      responsive: true,
+      scales: {
+        r: {
+            // Taille de la police pour les labels autour du radar
+            pointLabels: {
+                font: {
+                    size: 16
+                }
+            },
+            // Taille de la police pour les valeurs de l'axe radial
+            ticks: {
+                font: {
+                    size: 14
+                }
+            }
+        }
+    },
+      plugins: {
+        title: {
+          display: true,
+          text: `Comparaison version v${label1} vs v${label2}.`,
+          font: { size: 18, weight: 'bold'}
+        },
+        tooltip: { enabled: true,
+          callbacks: {
+            label: context => {
+                switch (context.label) {
+                  case 'Fiabilité' :
+                  case 'Vulnérabilité' :
+                  case 'Maintenabilité' :
+                    if (context.parsed.r===cent){
+                      return ' Note : A';
+                    }
+                    if (context.parsed.r===quatreVingt){
+                      return ' Note : B';
+                    }
+                    if (context.parsed.r===soixante){
+                      return ' Note : C';
+                    }
+                    if (context.parsed.r===trente){
+                      return ' Note : D';
+                    }
+                    if (context.parsed.r===dix){
+                      return ' Note : E';
+                    }
+                    if (context.parsed.r===zero){
+                      return ' Note : Z';
+                    }
+                  break;
+                  case 'Hotspot' :
+                  if (context.parsed.r>=quatreVingt){
+                    return ' Note : A';
+                  }
+                  if (context.parsed.r>=soixanteDix && context.parsed.r<quatreVingt){
+                      return ' Note : B';
+                    }
+                  if (context.parsed.r>=cinquante && context.parsed.r<soixanteDix){
+                      return ' Note : C';
+                    }
+                  if (context.parsed.r>=trente && context.parsed.r<quarante){
+                      return ' Note : D';
+                    }
+                  if (context.parsed.r<trente){
+                    return ' Note : E';
+                  }
+                  break;
+                  case 'Dette' :
+                  case 'Couverture' :
+                    if (context.parsed.r>=quatreVingt){
+                        return ' Note : A';
+                      }
+                    if (context.parsed.r>=soixante && context.parsed.r<quatreVingt){
+                      return ' Note : B';
+                    }
+                    if (context.parsed.r>=quarante && context.parsed.r<soixante){
+                      return ' Note : C';
+                    }
+                    if (context.parsed.r>=vingt && context.parsed.r<quarante){
+                      return ' Note : D';
+                    }
+                    if (context.parsed.r<vingt){
+                      return ' Note : E';
+                    }
+                  break;
+                  default:
+                    sessionStorage.setItem('error', `Oups, le label ${context.label} n'est pas pris en charge.`);
+                }
+            }
+          },
+        },
+        legend: { position: 'bottom',
+                  labels: { font: { size: 16, weight: 'bold' } }
+                },
+        datalabels: { display: true,
+                      color: '#00445b' }
+        }};
+
+    const chartStatus = Chart.getChart('graphique-note');
+    if (chartStatus !== undefined) {
+      chartStatus.destroy();
+    }
+
+    const ctx = document.getElementById('graphique-note').getContext('2d');
+    const charts = new Chart(ctx, { type: 'radar', data, options });
+    if (charts === null) {
+      sessionStorage.setItem('error', "c'est pour rire, il ne peut pas y avoir d'erreur ici :)");
+    }
+
+  };
+
+/**
+ * description
+ * On affiche/désactive les indicateurs de variation
+ * Faux positif : sonarLint(javascript:S1192)
+ * @constant
+ * @name oui_non
+ * @type {"#js-oui-non"}
+ */
+$(oui_non).on('click', function () {
+  console.log('up-down');
+  if ($(oui_non).is(':checked')===true) {
+    $(upDownEqual).removeClass('hide');
+  }
+
+  /** On en fait deux pour être certain de capter l’événement */
+  if ( $(oui_non).is(':checked') === false && $(upDownEqual).hasClass('hide') === false ) {
+      $(upDownEqual).addClass('hide');
+    }
+});
+
+/**
+ * On affiche les indicateurs du projet de référence
+ *
+ * @method
+ * @name JQuery
+ */
+$('#js-affiche-projet-reference').on('click', function () {
+  $('#modal-projet-reference').foundation('open');
+});
+
+/** Affiche le radar des indicateurs clés */
+const elm=document.getElementById('js-series');
+let s1=[], s2=[];
+
+// ['Fiabilité','Vulnérabilité','Hotspot', 'Maintenabilité','Couverture','Dette']
+elm.dataset.initial.split(',').forEach(element => {
+  s1.push(parseInt(element,10));
+});
+elm.dataset.actuel.split(',').forEach(element => {
+  s2.push(parseInt(element,10));
+});
+
+dessineMoiUnRadar(s1, s2, elm.dataset.labela, elm.dataset.labelb);
