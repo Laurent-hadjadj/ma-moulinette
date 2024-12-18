5e90ca6d2424d3013b9cd08f954fc2fd57384d40
Laurent HADJADJ <laurent_h@me.com>
2024-12-18 15:14:49 +0100

    Migration SF7: Ajout du handlerDatabaseException

--- src/Repository/PortefeuilleRepository.php
+++ src/Repository/PortefeuilleRepository.php
@@ -3,7 +3,7 @@
 /*
  *  Ma-Moulinette
  *  --------------
- *  Copyright (c) 2021-2022.
+ *  Copyright (c) 2021-2024.
  *  Laurent HADJADJ <laurent_h@me.com>.
  *  Licensed Creative Common  CC-BY-NC-SA 4.0.
  *  ---
@@ -23,10 +23,32 @@ use Doctrine\Persistence\ManagerRegistry;
 class PortefeuilleRepository extends ServiceEntityRepository
 {
     public static $removeReturnLine = "/\s+/u";
+    public static $noDataBase = 'La connexion à la base de données a échoué.';
 
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Portefeuille::class);
+    }
+
+
+    /**
+     * [Description for handleDatabaseException]
+     *
+     * @param \Doctrine\DBAL\Exception $e
+     * 
+     * @return array
+     * 
+     * Created at: 18/12/2024 15:12:12 (Europe/Paris)
+     * @author     Laurent HADJADJ <laurent_h@me.com> 
+     * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0. 
+     */
+    protected function handleDatabaseException(\Doctrine\DBAL\Exception $e): array
+    {
+        if (strpos($e->getMessage(), 'SQLSTATE[08006]') !== false) {
+            return ['code'=>500, 'erreur' => static::$noDataBase];
+        } else {
+            return ['code' => 500, 'erreur' => $e->getMessage()];
+        }
     }
 
     /**
@@ -50,12 +72,11 @@ class PortefeuilleRepository extends ServiceEntityRepository
                 /** On escape les ' : normalement on en a pas besoin */
                 //"$reEncode = str_replace("'", "''", $map['portefeuille']);
 
-                $conn=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnLine, " ", $sql));
-                    $conn->bindValue(':portefeuille', $map['portefeuille']);
-                $exec=$conn->executeQuery();
-                $liste=$exec->fetchAllAssociative();
+                $stmt=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnLine, " ", $sql));
+                    $stmt->bindValue(':portefeuille', $map['portefeuille']);
+                $liste=$stmt->executeQuery()->fetchAllAssociative();
             } catch (\Doctrine\DBAL\Exception $e) {
-            return ['code'=>500, 'erreur'=> $e->getMessage()];
+                return $this->handleDatabaseException($e);
         }
         return ['code'=>200, 'liste'=>$liste, 'erreur'=>''];
     }
