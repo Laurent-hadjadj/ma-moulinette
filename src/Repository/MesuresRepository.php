7160cc52668421f28a648c29dabae5b645513b77
Laurent HADJADJ <laurent_h@me.com>
2024-12-18 15:38:50 +0100

    Migration SF7: Code-Clean, Ajout du handlerDatabaseException

--- src/Repository/MesuresRepository.php
+++ src/Repository/MesuresRepository.php
@@ -3,7 +3,7 @@
 /*
  *  Ma-Moulinette
  *  --------------
- *  Copyright (c) 2021-2022.
+ *  Copyright (c) 2021-2024.
  *  Laurent HADJADJ <laurent_h@me.com>.
  *  Licensed Creative Common  CC-BY-NC-SA 4.0.
  *  ---
@@ -24,10 +24,31 @@ class MesuresRepository extends ServiceEntityRepository
 {
     public static $removeReturnLine = "/\s+/u";
     public static $mavenKey = ':maven_key';
+    public static $noDataBase = 'La connexion à la base de données a échoué.';
 
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Mesures::class);
+    }
+
+    /**
+     * [Description for handleDatabaseException]
+     *
+     * @param \Doctrine\DBAL\Exception $e
+     *
+     * @return array
+     *
+     * Created at: 18/12/2024 15:29:21 (Europe/Paris)
+     * @author     Laurent HADJADJ <laurent_h@me.com>
+     * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    protected function handleDatabaseException(\Doctrine\DBAL\Exception $e): array
+    {
+        if (strpos($e->getMessage(), 'SQLSTATE[08006]') !== false) {
+            return ['code' => 500, 'erreur' => static::$noDataBase];
+        } else {
+            return ['code' => 500, 'erreur' => $e->getMessage()];
+        }
     }
 
     /**
@@ -44,18 +65,18 @@ class MesuresRepository extends ServiceEntityRepository
      */
     public function selectMesuresVersionLast($map): array
     {
+        $sql = "SELECT project_name as name, ncloc, language_distribution,
+                    lines, coverage, sqale_debt_ratio,
+                    duplicated_lines_density, tests, issues
+                FROM ma_moulinette.mesures
+                WHERE maven_key=:maven_key
+                ORDER BY date_enregistrement DESC LIMIT 1";
         try {
-                $sql = "SELECT project_name as name, ncloc, language_distribution,
-                            lines, coverage, sqale_debt_ratio,
-                            duplicated_lines_density, tests, issues
-                        FROM ma_moulinette.mesures
-                        WHERE maven_key=:maven_key
-                        ORDER BY date_enregistrement DESC LIMIT 1";
-                $conn=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnLine, " ", $sql));
-                    $conn->bindValue(static::$mavenKey, $map['maven_key']);
-                $mesures=$conn->executeQuery()->fetchAllAssociative();
+                $stmt=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnLine, " ", $sql));
+                    $stmt->bindValue(static::$mavenKey, $map['maven_key']);
+                $mesures=$stmt->executeQuery()->fetchAllAssociative();
         } catch (\Doctrine\DBAL\Exception $e) {
-            return ['code'=>500, 'erreur'=> $e->getMessage()];
+            return $this->handleDatabaseException($e);
         }
         return ['code'=>200, 'mesures'=>$mesures, 'erreur'=>''];
     }
@@ -73,34 +94,31 @@ class MesuresRepository extends ServiceEntityRepository
      */
     public function insertMesures($map): array
     {
+        $sql = "INSERT INTO ma_moulinette.mesures
+                    (maven_key, project_name, lines, ncloc, language_distribution, sqale_debt_ratio, coverage, duplicated_lines_density, tests, issues, mode_collecte, utilisateur_collecte, date_enregistrement)
+                VALUES
+                    (:maven_key, :project_name, :lines, :ncloc, :language_distribution::json, :sqale_debt_ratio, :coverage, :duplicated_lines_density, :tests, :issues, :mode_collecte, :utilisateur_collecte, :date_enregistrement)";
         try {
             $this->getEntityManager()->getConnection()->beginTransaction();
+                $stmt = $this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnLine, " ", $sql));
+                $stmt->bindValue(static::$mavenKey, $map['maven_key']);
+                $stmt->bindValue(':project_name', $map['project_name']);
+                $stmt->bindValue(':lines', $map['lines']);
+                $stmt->bindValue(':ncloc', $map['ncloc']);
+                // Encode le tableau en JSON et l'encapsule dans un tableau PostgreSQL
+                $language_distribution_json = json_encode([$map['language_distribution']]);
+                $stmt->bindValue(':language_distribution', $language_distribution_json);
+                $stmt->bindValue(':sqale_debt_ratio', $map['sqale_debt_ratio']);
+                $stmt->bindValue(':coverage', $map['coverage']);
+                $stmt->bindValue(':duplicated_lines_density', $map['duplicated_lines_density']);
+                $stmt->bindValue(':tests', $map['tests']);
+                $stmt->bindValue(':issues', $map['issues']);
+                $stmt->bindValue(':mode_collecte', $map['mode_collecte']);
+                $stmt->bindValue(':utilisateur_collecte', $map['utilisateur_collecte']);
 
-            $sql = "INSERT INTO ma_moulinette.mesures
-                        (maven_key, project_name, lines, ncloc, language_distribution, sqale_debt_ratio, coverage, duplicated_lines_density, tests, issues, mode_collecte, utilisateur_collecte, date_enregistrement)
-                    VALUES
-                        (:maven_key, :project_name, :lines, :ncloc, :language_distribution::json, :sqale_debt_ratio, :coverage, :duplicated_lines_density, :tests, :issues, :mode_collecte, :utilisateur_collecte, :date_enregistrement)";
-
-            $stmt = $this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnLine, " ", $sql));
-            $stmt->bindValue(static::$mavenKey, $map['maven_key']);
-            $stmt->bindValue(':project_name', $map['project_name']);
-            $stmt->bindValue(':lines', $map['lines']);
-            $stmt->bindValue(':ncloc', $map['ncloc']);
-            // Encode le tableau en JSON et l'encapsule dans un tableau PostgreSQL
-            $language_distribution_json = json_encode([$map['language_distribution']]);
-            $stmt->bindValue(':language_distribution', $language_distribution_json);
-            $stmt->bindValue(':sqale_debt_ratio', $map['sqale_debt_ratio']);
-            $stmt->bindValue(':coverage', $map['coverage']);
-            $stmt->bindValue(':duplicated_lines_density', $map['duplicated_lines_density']);
-            $stmt->bindValue(':tests', $map['tests']);
-            $stmt->bindValue(':issues', $map['issues']);
-            $stmt->bindValue(':mode_collecte', $map['mode_collecte']);
-            $stmt->bindValue(':utilisateur_collecte', $map['utilisateur_collecte']);
-
-            // Formate la date avant de l'enregistrer
-            $stmt->bindValue(':date_enregistrement', $map['date_enregistrement']->format('Y-m-d H:i:sO'));
-
-            $stmt->executeStatement();
+                // Formate la date avant de l'enregistrer
+                $stmt->bindValue(':date_enregistrement', $map['date_enregistrement']->format('Y-m-d H:i:sO'));
+                $stmt->executeStatement();
             $this->getEntityManager()->getConnection()->commit();
         } catch (\Doctrine\DBAL\Exception $e) {
             $this->getEntityManager()->getConnection()->rollBack();
@@ -125,19 +143,18 @@ class MesuresRepository extends ServiceEntityRepository
      */
     public function deleteMesuresMavenKey($map): array
     {
+        $sql = "DELETE
+                FROM ma_moulinette.mesures
+                WHERE maven_key=:maven_key";
         try {
                 $this->getEntityManager()->getConnection()->beginTransaction();
-                    $sql = "DELETE
-                            FROM ma_moulinette.mesures
-                            WHERE maven_key=:maven_key";
-
                     $stmt=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnLine, " ", $sql));
                         $stmt->bindValue(static::$mavenKey, $map['maven_key']);
                         $stmt->executeStatement();
                 $this->getEntityManager()->getConnection()->commit();
         } catch (\Doctrine\DBAL\Exception $e) {
             $this->getEntityManager()->rollback();
-            return ['code'=>500, 'erreur'=> $e->getMessage()];
+            return $this->handleDatabaseException($e);
         }
         return ['code'=>200, 'erreur'=>''];
     }
