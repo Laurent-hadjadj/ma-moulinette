24292ebda9e50611fde8426a2448269e20b6c500
Laurent HADJADJ <laurent_h@me.com>
2024-12-18 19:01:52 +0100

    Migration SF7: Ajout de la classe.

---
+++ src/Entity/OwaspTop10.php
@@ -0,0 +1,114 @@
+<?php
+
+/*
+ *  Ma-Moulinette
+ *  --------------
+ *  Copyright (c) 2021-2024.
+ *  Laurent HADJADJ <laurent_h@me.com>.
+ *  Licensed Creative Common  CC-BY-NC-SA 4.0.
+ *  ---
+ *  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+ *  http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+namespace App\Entity;
+
+use App\Repository\OwaspTop10Repository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * [Description OwaspTop10]
+ */
+#[ORM\Entity(repositoryClass: OwaspTop10Repository::class)]
+#[ORM\Table(name: 'owasp_top10', schema: 'ma_moulinette')]
+class OwaspTop10
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: Types::INTEGER, nullable: false,
+    options: ['comment' => 'clé unique pour la table OwaspTop10'])]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::INTEGER)]
+    private int $year;
+
+    #[ORM\Column(type: Types::STRING, length: 255)]
+    private string $category;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private string $description;
+
+    #[ORM\Column(type: Types::STRING, length: 128)]
+    private string $lien;
+
+    #[ORM\Column(type: Types::DATETIMETZ_MUTABLE, options: ['default' => 'CURRENT_TIMESTAMP'])]
+    private \DateTimeImmutable $dateEnregistrement;
+
+    // Getters and Setters
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getYear(): int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getCategory(): string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getLien(): string
+    {
+        return $this->lien;
+    }
+
+    public function setLien(string $lien): self
+    {
+        $this->lien = $lien;
+
+        return $this;
+    }
+
+    public function getDateEnregistrement(): ?\DateTimeImmutable
+    {
+        return $this->dateEnregistrement;
+    }
+
+    public function setDateEnregistrement(\DateTimeImmutable $dateEnregistrement): self
+    {
+        $this->dateEnregistrement = $dateEnregistrement;
+
+        return $this;
+    }
+}
