97fb0d653b261dd649ee20b6b4ebe14161ab2a76
Laurent HADJADJ <laurent_h@me.com>
2024-12-18 15:43:19 +0100

    Migration SF7: Code-Clean, Ajout du handlerDatabaseException

--- src/Repository/MaMoulinetteRepository.php
+++ src/Repository/MaMoulinetteRepository.php
@@ -3,7 +3,7 @@
 /*
  *  Ma-Moulinette
  *  --------------
- *  Copyright (c) 2021-2022.
+ *  Copyright (c) 2021-2024.
  *  Laurent HADJADJ <laurent_h@me.com>.
  *  Licensed Creative Common  CC-BY-NC-SA 4.0.
  *  ---
@@ -23,10 +23,31 @@ use Doctrine\Persistence\ManagerRegistry;
 class MaMoulinetteRepository extends ServiceEntityRepository
 {
     public static $removeReturnLine = "/\s+/u";
+    public static $noDataBase = 'La connexion à la base de données a échoué.';
 
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, MaMoulinette::class);
+    }
+
+    /**
+     * [Description for handleDatabaseException]
+     *
+     * @param \Doctrine\DBAL\Exception $e
+     *
+     * @return array
+     *
+     * Created at: 18/12/2024 15:40:39 (Europe/Paris)
+     * @author     Laurent HADJADJ <laurent_h@me.com>
+     * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    protected function handleDatabaseException(\Doctrine\DBAL\Exception $e): array
+    {
+        if (strpos($e->getMessage(), 'SQLSTATE[08006]') !== false) {
+            return ['code' => 500, 'erreur' => static::$noDataBase];
+        } else {
+            return ['code' => 500, 'erreur' => $e->getMessage()];
+        }
     }
 
     /**
@@ -40,14 +61,14 @@ class MaMoulinetteRepository extends ServiceEntityRepository
      */
     public function getMaMoulinetteVersion(): array
     {
+        $sql = "SELECT version
+                FROM ma_moulinette.ma_moulinette
+                ORDER BY date_version DESC LIMIT 1";
         try {
-                $sql = "SELECT version
-                        FROM ma_moulinette.ma_moulinette
-                        ORDER BY date_version DESC LIMIT 1";
-                $conn=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnLine, " ", $sql));
-                $request=$conn->executeQuery()->fetchAllAssociative();
+                $stmt=$this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnLine, " ", $sql));
+                $request=$stmt->executeQuery()->fetchAllAssociative();
         } catch (\Doctrine\DBAL\Exception $e) {
-            return ['code'=>500, 'erreur'=> $e->getMessage()];
+            return $this->handleDatabaseException($e);
         }
         return ['code'=>200, 'request'=>$request, 'erreur'=>''];
     }
