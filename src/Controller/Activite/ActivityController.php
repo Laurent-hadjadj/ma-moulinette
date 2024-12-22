a608532ddf14b1899f25727bdea0948a0fcd36fa
Laurent HADJADJ <laurent_h@me.com>
2024-12-22 10:09:39 +0100

    Migration SF7: Refactor du controller pour la page de suivi des analyses SonarQube.

--- src/Controller/Activite/ActivityController.php
+++ src/Controller/Activite/ActivityController.php
@@ -11,20 +11,20 @@
  *  http://creativecommons.org/licenses/by-nc-sa/4.0/
  */
 
-namespace App\Controller\Activite;
+namespace App\Controller\Activity;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
-use App\Entity\Activite;
-use App\Entity\ActiviteHistorique;
-use App\Service\ClientActivite;
+use App\Entity\Activity;
+use App\Entity\ActivityHistorique;
+use App\Service\Client;
 
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
 
-class ActiviteController extends AbstractController
+class ActivityController extends AbstractController
 {
 
     public static $sonarUrl = "sonar.url";
@@ -86,34 +86,34 @@ class ActiviteController extends AbstractController
      * @author    Laurent HADJADJ <laurent_h@me.com>
      * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
      */
-    #[Route('/activite', name: 'activite', methods: 'GET')]
-    public function index(ClientActivite $client): Response
+    #[Route('/activity', name: 'activity', methods: 'GET')]
+    public function index(Client $client): Response
     {
         /** On instancie l'EntityRepository */
-        $activiteEntity = $this->em->getRepository(Activite::class);
+        $activityEntity = $this->em->getRepository(Activity::class);
 
         // On récupère l'année actuelle
         $dateMoins1 = new \DateTime();
             $dateMoins1->setTimezone(new \DateTimeZone('Europe/Paris'));
-        $anneeActuelle = $dateMoins1->format('Y');
+        $actualYear = $dateMoins1->format('Y');
 
         $url = $this->getParameter(static::$sonarUrl)
         . "/api/ce/activity";
         $respond = $client->http($url);
 
-        if ($activiteEntity->selectActivite($anneeActuelle)['request'] == [] ){
-            $this->addFlash('notice', ['type'=>'alert', 'titre'=> '[ACTIVITE-003]', 'message'=>" Votre La liste des analyses sonarQube est vide. Veuillez rafraîchir la liste"]);
+        if ($activityEntity->selectActivity($actualYear)['request'] == [] ){
+            $this->addFlash('notice', ['type'=>'alert', 'titre'=> '[ACTIVITÉ-003]', 'message'=>" Votre La liste des analyses SonarQube est vide. Veuillez rafraîchir la liste"]);
             $request["-"] = [
-                'annee' => "-",
-                'nb_jour' => "-",
-                'nb_analyse' => "-",
-                'nb_reussi' => "-",
-                'nb_echec' => "-",
-                'max_temps' => "-",
-                'moyenne_analyse' => "-",
-                'taux_reussite' => "-"
+                'year' => "-",
+                'day' => "-",
+                'analyse' => "-",
+                'success' => "-",
+                'fail' => "-",
+                'max_time' => "-",
+                'analyse_average' => "-",
+                'success_rate' => "-"
             ];
-            return $this->render('activite/index.html.twig', [
+            return $this->render('activity/index.html.twig', [
                 'marque_entreprise_short' => $this->getParameter('marque.entreprise.short'),
                 'marque_entreprise_long' => $this->getParameter('marque.entreprise.long'),
                 'logo_entreprise' => $this->getParameter('logo.entreprise'),
@@ -123,30 +123,30 @@ class ActiviteController extends AbstractController
                 Response::HTTP_OK]);
         }
 
-        $dateBase = new \DateTime($activiteEntity->dernierDate()['request'][0]['date']);
+        $dateBase = new \DateTime($activityEntity->dernierDate()['request'][0]['date']);
         $dateSonar = new \DateTime($respond['tasks'][0]['executedAt']);
 
         if($dateSonar > $dateBase){
             // Ici on calcule l'interval de jour entre la base et sonar
             $interval = $dateBase->diff(new \DateTime($respond['tasks'][0]['executedAt']))->format('%d');
-            $this->addFlash('notice', ['type'=>'warning', 'titre'=> '[ACTIVITE-002]', 'message'=>"Vous pouvez mettre à jour  La liste des analyses SonarQube. Il y a " .$interval. " jours de retard"]);
+            $this->addFlash('notice', ['type'=>'warning', 'titre'=> '[ACTIVITÉ-002]', 'message'=>"Vous pouvez mettre à jour  La liste des analyses SonarQube. Il y a " .$interval. " jours de retard"]);
         }
         if($dateSonar == $dateBase){
-            $this->addFlash('notice', ['type'=>'default', 'titre'=> '[ACTIVITE-001]', 'message'=>" La liste des analyses SonarQube est à jour."]);
+            $this->addFlash('notice', ['type'=>'default', 'titre'=> '[ACTIVITÉ-001]', 'message'=>" La liste des analyses SonarQube est à jour."]);
         }
 
-        $historiqueActiviteEntity = $this->em->getRepository(ActiviteHistorique::class);
+        $historiqueActivityEntity = $this->em->getRepository(ActivityHistorique::class);
 
-        $result = $historiqueActiviteEntity->selectActivite();
+        $result = $historiqueActivityEntity->selectActivity();
         for ($i = 0; $i < count($result['request']); $i++){
-            $formatedDate = new \DateTimeImmutable($result['request'][$i]['max_temps']);
-            $result['request'][$i]['max_temps'] = $formatedDate->format('H:i:s');
+            $formatedDate = new \DateTimeImmutable($result['request'][$i]['max_time']);
+            $result['request'][$i]['max_time'] = $formatedDate->format('H:i:s');
         }
 
         $result['request'][0]["date_enregistrement"] = (new \DateTime($result['request'][0]["date_enregistrement"]))->format('d-m-Y H:i:s');
 
         $render=static::genericRender();
-        return $this->render('activite/index.html.twig', $render);
+        return $this->render('activity/index.html.twig', $render);
     }
 
 }
