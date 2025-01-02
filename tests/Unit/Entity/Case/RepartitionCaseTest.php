b2c58d628e066a0ebc1073bea5bc2b218b2f1d0d
Laurent HADJADJ <laurent_h@me.com>
2025-01-02 16:21:03 +0100

    Test: Ajout des TU pour l'entité Repartition

---
+++ tests/Unit/Entity/Case/RepartitionCaseTest.php
@@ -0,0 +1,112 @@
+<?php
+
+/*
+*  Ma-Moulinette
+*  --------------
+*  Copyright (c) 2021-2024.
+*  Laurent HADJADJ <laurent_h@me.com>.
+*  Licensed Creative Common  CC-BY-NC-SA 4.0.
+*  ---
+*  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+*  http://creativecommons.org/licenses/by-nc-sa/4.0/
+*/
+
+namespace App\Tests\Unit\Entity\Case;
+
+use App\Entity\Repartition;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * [Description RepartitionCaseTest]
+ */
+class RepartitionCaseTest extends TestCase
+{
+    private $repartition;
+
+    private static $mavenKey = 'fr.ma-petite-entreprise:ma-moulinette';
+    private static $name = 'nom ?';
+    private static $component = '/controller/auth';
+    private static $type = 'bug';
+    private static $severity = 'medium';
+    private static $setup = '202403261446';
+    private static $dateEnregistrement = '2024-04-12 16:23:11+01';
+
+    private function getEntity(): Repartition
+    {
+        return (new repartition())
+        ->setMavenKey(static::$mavenKey)
+        ->setName(static::$name)
+        ->setComponent(static::$component)
+        ->setType(static::$type)
+        ->setSeverity(static::$severity)
+        ->setSetup(static::$setup)
+        ->setDateEnregistrement(new \DateTimeImmutable(static::$dateEnregistrement));
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->repartition = $this->getEntity();
+    }
+
+    public function testSettingAndGettingMavenKey(): void
+    {
+        $newMavenKey = 'com.example:example-project';
+        $this->repartition->setMavenKey($newMavenKey);
+        $this->assertEquals($newMavenKey, $this->repartition->getMavenKey());
+    }
+
+    public function testSettingAndGettingName(): void
+    {
+        $newName = 'Updated Name';
+        $this->repartition->setName($newName);
+        $this->assertEquals($newName, $this->repartition->getName());
+    }
+
+    public function testSettingAndGettingComponent(): void
+    {
+        $newComponent = '/service/api';
+        $this->repartition->setComponent($newComponent);
+        $this->assertEquals($newComponent, $this->repartition->getComponent());
+    }
+
+    public function testSettingAndGettingType(): void
+    {
+        $newType = 'feature';
+        $this->repartition->setType($newType);
+        $this->assertEquals($newType, $this->repartition->getType());
+    }
+
+    public function testSettingAndGettingSeverity(): void
+    {
+        $newSeverity = 'critical';
+        $this->repartition->setSeverity($newSeverity);
+        $this->assertEquals($newSeverity, $this->repartition->getSeverity());
+    }
+
+    public function testSettingAndGettingSetup(): void
+    {
+        $newSetup = '202401011200';
+        $this->repartition->setSetup($newSetup);
+        $this->assertEquals($newSetup, $this->repartition->getSetup());
+    }
+
+    public function testSettingAndGettingDateEnregistrement(): void
+    {
+        $newDate = new \DateTimeImmutable('2025-01-01 12:00:00+01');
+        $this->repartition->setDateEnregistrement($newDate);
+        $this->assertEquals($newDate, $this->repartition->getDateEnregistrement());
+    }
+
+    public function testInitialValues(): void
+    {
+        $this->assertEquals(static::$mavenKey, $this->repartition->getMavenKey());
+        $this->assertEquals(static::$name, $this->repartition->getName());
+        $this->assertEquals(static::$component, $this->repartition->getComponent());
+        $this->assertEquals(static::$type, $this->repartition->getType());
+        $this->assertEquals(static::$severity, $this->repartition->getSeverity());
+        $this->assertEquals(static::$setup, $this->repartition->getSetup());
+        $this->assertEquals(new \DateTimeImmutable(static::$dateEnregistrement), $this->repartition->getDateEnregistrement());
+    }
+
+}
