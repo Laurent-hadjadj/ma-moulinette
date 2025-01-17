2c09a99f6a2edafc54023b8cf6757ec911c53a40
Laurent HADJADJ <laurent_h@me.com>
2025-01-17 13:48:14 +0100

    Fix: Correction date UTC

--- tests/Unit/Controller/Batch/BatchCollecteHotspotControllerTest.php
+++ tests/Unit/Controller/Batch/BatchCollecteHotspotControllerTest.php
@@ -37,7 +37,7 @@ class BatchCollecteHotspotControllerTest extends TestCase
     /** @var ContainerInterface&MockObject */
     private MockObject $container;
 
-    private static $date = ' 2024-08-09';
+    private static $date = '2024-08-09T00:00:00+00:00';
 
     protected function setUp(): void
     {
@@ -137,69 +137,84 @@ class BatchCollecteHotspotControllerTest extends TestCase
     {
         $this->informationProjetRepository
             ->method('selectInformationProjetProjectVersion')
-            ->willReturn(['code' => 200, 'info' => [['project_version' => '1.0', 'date' => static::$date]]]);
+            ->willReturn(['code' => 200, 'info' => [['project_version' => '1.0', 'date' => self::$date]]]);
 
         $this->client
             ->method('httpSonarQube')
-            ->willReturn(['paging' => ['total' => 0], 'hotspots' => []]);
+            ->willReturn([
+                'paging' => ['total' => 0],
+                'hotspots' => [],
+                'date' => (new \DateTimeImmutable(self::$date))->format(\DateTime::ATOM),
+            ]);
 
-        $this->hotspotsRepository
-            ->method('insertHotspots')
-            ->willReturn(['code' => 200]);
-
+        $this->hotspotsRepository->method('insertHotspots')->willReturn(['code' => 200]);
         $this->hotspotsRepository->method('deleteHotspotsMavenKey')->willReturn(['code' => 200]);
+
         $result = $this->controller->BatchCollecteHotspot('maven-key-123', 'manual', 'user123');
+
         $this->assertEquals(200, $result['code']);
         $this->assertCount(1, $result['message']);
-        // Compare directement les objets DateTimeImmutable
-        $expectedDateVersion = (new \DateTimeImmutable(static::$date))->setTimezone(new \DateTimeZone('UTC'));
-        $expectedDateEnregistrement = $result['message'][0]['date_enregistrement'];
 
-        $this->assertEquals($expectedDateVersion->format(\DateTime::ATOM), $result['message'][0]['date_version']->format(\DateTime::ATOM));
-        $this->assertEquals($expectedDateEnregistrement->format(\DateTime::ATOM), $result['message'][0]['date_enregistrement']->format(\DateTime::ATOM));
+        $expectedDateVersion = $this->createUtcDate(self::$date);
+        $expectedDateEnregistrement = $result['message'][0]['date_enregistrement']->setTimezone(new \DateTimeZone('UTC'));
+
+        $this->assertEquals(
+            $expectedDateVersion->format(\DateTime::ATOM),
+            $result['message'][0]['date_version']->setTimezone(new \DateTimeZone('UTC'))->format(\DateTime::ATOM)
+        );
+
+        $this->assertEquals(
+            $expectedDateEnregistrement->format(\DateTime::ATOM),
+            $result['message'][0]['date_enregistrement']->setTimezone(new \DateTimeZone('UTC'))->format(\DateTime::ATOM)
+        );
 
         $expectedMessage = [
-        'maven_key' => 'maven-key-123',
-        'version' => '1.0',
-        'date_version' => $expectedDateVersion->format(\DateTime::ATOM),
-        'hotspot_key' => 'NC',
-        'security_category' => 'NC',
-        'rule_key' => 'NC',
-        'probability' => 'NC',
-        'status' => 'NC',
-        'resolution' => '',
-        'niveau' => -1,
-        'mode_collecte' => 'manual',
-        'utilisateur_collecte' => 'user123',
-        'date_enregistrement' => $expectedDateEnregistrement->format(\DateTime::ATOM)
+            'maven_key' => 'maven-key-123',
+            'version' => '1.0',
+            'date_version' => $expectedDateVersion->format(\DateTime::ATOM),
+            'hotspot_key' => 'NC',
+            'security_category' => 'NC',
+            'rule_key' => 'NC',
+            'probability' => 'NC',
+            'status' => 'NC',
+            'resolution' => '',
+            'niveau' => -1,
+            'mode_collecte' => 'manual',
+            'utilisateur_collecte' => 'user123',
+            'date_enregistrement' => $expectedDateEnregistrement->format(\DateTime::ATOM),
         ];
 
         $actualMessage = $result['message'][0];
-        $actualMessage['date_version'] = $actualMessage['date_version']->format(\DateTime::ATOM);
-        $actualMessage['date_enregistrement'] = $actualMessage['date_enregistrement']->format(\DateTime::ATOM);
+        $actualMessage['date_version'] = $actualMessage['date_version']->setTimezone(new \DateTimeZone('UTC'))->format(\DateTime::ATOM);
+        $actualMessage['date_enregistrement'] = $actualMessage['date_enregistrement']->setTimezone(new \DateTimeZone('UTC'))->format(\DateTime::ATOM);
 
         $this->assertEquals($expectedMessage, $actualMessage);
         $this->assertCount(4, $result['data']);
     }
 
+    private function createUtcDate(string $date): \DateTimeImmutable
+    {
+        return new \DateTimeImmutable($date, new \DateTimeZone('UTC'));
+    }
+
     public function testBatchCollecteHotspotInsertionError(): void
-{
-    $this->informationProjetRepository
-        ->method('selectInformationProjetProjectVersion')
-        ->willReturn(['code' => 200, 'info' => [['project_version' => '1.0', 'date' => static::$date]]]);
+    {
+        $this->informationProjetRepository
+            ->method('selectInformationProjetProjectVersion')
+            ->willReturn(['code' => 200, 'info' => [['project_version' => '1.0', 'date' => static::$date]]]);
 
-    $this->client
-        ->method('httpSonarQube')
-        ->willReturn(['paging' => ['total' => 1], 'hotspots' => [['vulnerabilityProbability' => 'HIGH']]]);
+        $this->client
+            ->method('httpSonarQube')
+            ->willReturn(['paging' => ['total' => 1], 'hotspots' => [['vulnerabilityProbability' => 'HIGH']]]);
 
-    $this->hotspotsRepository
-        ->method('insertHotspots')
-        ->willReturn(['code' => 500, 'erreur' => 'Database Error']);
+        $this->hotspotsRepository
+            ->method('insertHotspots')
+            ->willReturn(['code' => 500, 'erreur' => 'Database Error']);
 
-    $this->hotspotsRepository->method('deleteHotspotsMavenKey')->willReturn(['code' => 200]);
-    $result = $this->controller->BatchCollecteHotspot('maven-key-123', 'manual', 'user123');
+        $this->hotspotsRepository->method('deleteHotspotsMavenKey')->willReturn(['code' => 200]);
+        $result = $this->controller->BatchCollecteHotspot('maven-key-123', 'manual', 'user123');
 
-    $this->assertEquals(500, $result['code']);
-    $this->assertArrayHasKey('erreur', $result);
-}
+        $this->assertEquals(500, $result['code']);
+        $this->assertArrayHasKey('erreur', $result);
+    }
 }
