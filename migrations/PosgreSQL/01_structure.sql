14dca558b1641d0498346dc2683a6cb9221ea81a
Laurent HADJADJ <laurent_h@me.com>
2025-01-20 12:19:10 +0100

    Refactor: Mise à jour de la table historique

--- migrations/PosgreSQL/01_structure.sql
+++ migrations/PosgreSQL/01_structure.sql
@@ -2,7 +2,7 @@
 ####################################################
 ##                                                ##
 ##         Creation des tables et des objets      ##
-##               V1.29.3 - 03/01/2025             ##
+##               V1.30.0 - 20/01/2025             ##
 ##                                                ##
 ####################################################*/
 
@@ -43,7 +43,7 @@
 -- 28/12/2024 : Laurent HADJADJ - Le type de l'attribut erreur de la table activity_batch_report est un json ;
 -- 31/12/2024 : Laurent HADJADJ - Correction de la table activity_batch_report ;
 -- 03/01/2025 : Laurent HADJADJ - Bonne année 2025. Renommage de l'attribut start en mode_collecte  pour la table batch_traitement ;
-
+-- 20/01/2025 : Laurent HADJADJ - Ajout des colones classes et functions à la table historique ;
 
 -- SCHEMA: ma_moulinette
 
@@ -430,6 +430,8 @@ CREATE TABLE IF NOT EXISTS ma_moulinette.historique
   todo integer NOT NULL,
   nombre_ligne integer NOT NULL,
   nombre_ligne_code integer NOT NULL,
+  classes integer NOT NULL,
+  functions integer NOT NULL,
   coverage double precision NOT NULL,
   duplicated_lines_density double precision NOT NULL,
   tests integer NOT NULL,
@@ -498,6 +500,8 @@ COMMENT ON COLUMN ma_moulinette.historique.no_sonar IS 'Compteur de l’utilisat
 COMMENT ON COLUMN ma_moulinette.historique.todo IS 'Compteur de l’utilisation de todo';
 COMMENT ON COLUMN ma_moulinette.historique.nombre_ligne IS 'Nombre total de lignes dans le projet';
 COMMENT ON COLUMN ma_moulinette.historique.nombre_ligne_code IS 'Nombre total de lignes de code dans le projet';
+COMMENT ON COLUMN ma_moulinette.historique.classes IS 'Nombre de classes dans le projet';
+COMMENT ON COLUMN ma_moulinette.historique.function IS 'Nombre de méthode/function dans le projet';
 COMMENT ON COLUMN ma_moulinette.historique.coverage IS 'Pourcentage de couverture de code par les tests';
 COMMENT ON COLUMN ma_moulinette.historique.duplicated_lines_density IS 'Pourcentage de duplication dans le code';
 COMMENT ON COLUMN ma_moulinette.historique.tests IS 'Nombre de tests unitaires exécutés';
