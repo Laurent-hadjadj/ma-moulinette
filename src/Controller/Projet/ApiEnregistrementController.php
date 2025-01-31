095c0d56af2ead3d233509e145520df51e48087c
Laurent HADJADJ <laurent_h@me.com>
2025-01-31 15:57:40 +0100

    Refactor: Amélioration de la couverture de tests

--- src/Controller/Projet/ApiEnregistrementController.php
+++ src/Controller/Projet/ApiEnregistrementController.php
@@ -32,6 +32,9 @@ class ApiEnregistrementController extends AbstractController
 {
     /** Définition des constantes */
     public static $europeParis = "Europe/Paris";
+    public static $reference = "<strong>[Enregistrement]</strong> ";
+    public static $erreur400 = "La requête est incorrecte (Erreur 400).";
+    public static $erreur403 = "Vous devez avoir le rôle COLLECTE pour réaliser cette action (Erreur 403).";
 
     /**
      * [Description for __construct]
@@ -69,54 +72,66 @@ class ApiEnregistrementController extends AbstractController
         /** On instancie l'entityRepository */
         $historiqueRepository = $this->em->getRepository(Historique::class);
 
+        /** On vérifie si l'utilisateur à un rôle Collecte ? */
+        if (!$this->isGranted('ROLE_COLLECTE')) {
+        return new JsonResponse(
+            ['code' => 403, 'type'=>'warning',
+            'message' => static::$reference . static::$erreur403], Response::HTTP_OK);
+        }
+
         /** On décode le body. */
         $data = json_decode($request->getContent());
-
-        /** todo : vérifier les droits et que les data != null */
+        /** On teste si la clé est valide */
+        if ($data === null) {
+        return new JsonResponse([
+            'data' => $data,'code' => 400, 'type' => 'alert',
+            'message' => static::$reference . static::$erreur400], Response::HTTP_OK);
+        }
 
         /** On créé un objet date Immutable, avec la date courante. */
         $dateEnregistrement = new \DateTimeImmutable('now', new \DateTimeZone(static::$europeParis));
-
         /** On contrôle le mode d'utilisation */
         $utilisateur_collecte = $this->security->getUser()->getCourriel();
+
         $json='{}';
-        //'logger_debug'=>$data->logger_debug, 'tests'=>$data->tests, 'violations'=>$data->violations
-        $map=['maven_key'=>$data->maven_key, 'analyse_key'=>$data->analyse_key,
-            'version'=>$data->version, 'date_version'=>$data->date_version,
-            'nom_projet'=>$data->nom_projet, 'version_release'=>$data->version_release, 'version_snapshot'=>$data->version_snapshot, 'version_autre'=>$data->version_autre,
-            'suppress_warning'=>$data->suppress_warning, 'no_sonar'=>$data->no_sonar,
-            'todo'=>$data->todo,
-            'logger_info'=>$data->logger_info, 'logger_warn'=>$data->logger_warn, 'logger_error'=>$data->logger_error, 'logger_debug'=>$data->logger_debug,
-            'nombre_ligne'=>$data->nombre_ligne, 'nombre_ligne_code'=>$data->nombre_ligne_code, 'coverage'=>$data->coverage, 'duplicated_lines_density'=>$data->duplicated_lines_density, 'sqale_debt_ratio'=>$data->sqale_debt_ratio, 'tests'=>$data->tests, 'violations'=>$data->violations, 'dette'=>$data->dette,
-            'nombre_bug'=>$data->nombre_bug, 'nombre_vulnerability'=>$data->nombre_vulnerability, 'nombre_code_smell'=>$data->nombre_code_smell,
-            'bug_blocker'=>$data->bug_blocker, 'bug_critical'=>$data->bug_critical,
-            'bug_major'=>$data->bug_major, 'bug_minor'=>$data->bug_minor, 'bug_info'=>$data->bug_info,
-            'vulnerability_blocker'=>$data->vulnerability_blocker, 'vulnerability_critical'=>$data->vulnerability_critical, 'vulnerability_major'=>$data->vulnerability_major,
-            'vulnerability_minor'=>$data->vulnerability_minor, 'vulnerability_info'=>$data->vulnerability_info,
-            'code_smell_blocker'=>$data->code_smell_blocker, 'code_smell_critical'=>$data->code_smell_critical, 'code_smell_major'=>$data->code_smell_major,
-            'code_smell_minor'=>$data->code_smell_minor, 'code_smell_info'=>$data->code_smell_info,
-            'frontend'=>$data->frontend, 'backend'=>$data->backend, 'autre'=>$data->autre, 'inconnue'=>$data->inconnue,
-            'nombre_anomalie_bloquant'=>$data->nombre_anomalie_bloquant, 'nombre_anomalie_critique'=>$data->nombre_anomalie_critique,
-            'nombre_anomalie_majeur'=>$data->nombre_anomalie_majeur,
-            'nombre_anomalie_mineur'=>$data->nombre_anomalie_mineur, 'nombre_anomalie_info'=>$data->nombre_anomalie_info,
-            'note_reliability'=>$data->note_reliability, 'note_security'=>$data->note_security,
-            'note_sqale'=>$data->note_sqale, 'note_hotspot'=>$data->note_hotspot, 'nombre_hotspot'=>$data->nombre_hotspot,
-            'hotspot_high'=>$data->hotspot_high, 'hotspot_medium'=>$data->hotspot_medium, 'hotspot_low'=>$data->hotspot_low,
-            'initial'=> $data->initial,
-            'mode_collecte'=>'COLLECTE', 'utilisateur_collecte'=>$utilisateur_collecte,
-            'date_enregistrement'=>$dateEnregistrement
+        $map=['maven_key' => $data->maven_key, 'analyse_key' => $data->analyse_key,
+            'version' => $data->version, 'date_version' => $data->date_version,
+            'nom_projet' => $data->nom_projet, 'version_release' => $data->version_release, 'version_snapshot' => $data->version_snapshot, 'version_autre' => $data->version_autre,
+            'suppress_warning' => $data->suppress_warning, 'no_sonar' => $data->no_sonar,
+            'todo' => $data->todo,
+            'logger_info' => $data->logger_info, 'logger_warn' => $data->logger_warn, 'logger_error' => $data->logger_error, 'logger_debug' => $data->logger_debug,
+            'nombre_ligne' => $data->nombre_ligne, 'nombre_ligne_code' => $data->nombre_ligne_code,
+            'files' => $data->files, 'classes' => $data->classes, 'functions' => $data->functions,
+            'coverage' => $data->coverage, 'duplicated_lines_density' => $data->duplicated_lines_density, 'sqale_debt_ratio' => $data->sqale_debt_ratio, 'tests' => $data->tests, 'violations' => $data->violations, 'dette' => $data->dette,
+            'nombre_bug' => $data->nombre_bug, 'nombre_vulnerability' => $data->nombre_vulnerability, 'nombre_code_smell' => $data->nombre_code_smell,
+            'bug_blocker' => $data->bug_blocker, 'bug_critical' => $data->bug_critical,
+            'bug_major' => $data->bug_major, 'bug_minor' => $data->bug_minor, 'bug_info' => $data->bug_info,
+            'vulnerability_blocker' => $data->vulnerability_blocker, 'vulnerability_critical' => $data->vulnerability_critical, 'vulnerability_major' => $data->vulnerability_major,
+            'vulnerability_minor' => $data->vulnerability_minor, 'vulnerability_info' => $data->vulnerability_info,
+            'code_smell_blocker' => $data->code_smell_blocker, 'code_smell_critical' => $data->code_smell_critical, 'code_smell_major' => $data->code_smell_major,
+            'code_smell_minor' => $data->code_smell_minor, 'code_smell_info' => $data->code_smell_info,
+            'frontend' => $data->frontend, 'backend' => $data->backend, 'autre' => $data->autre, 'inconnue' => $data->inconnue,
+            'nombre_anomalie_bloquant' => $data->nombre_anomalie_bloquant, 'nombre_anomalie_critique' => $data->nombre_anomalie_critique,
+            'nombre_anomalie_majeur' => $data->nombre_anomalie_majeur,
+            'nombre_anomalie_mineur' => $data->nombre_anomalie_mineur, 'nombre_anomalie_info' => $data->nombre_anomalie_info,
+            'note_reliability' => $data->note_reliability, 'note_security' => $data->note_security,
+            'note_sqale' => $data->note_sqale, 'note_hotspot' => $data->note_hotspot, 'nombre_hotspot' => $data->nombre_hotspot,
+            'hotspot_high' => $data->hotspot_high, 'hotspot_medium' => $data->hotspot_medium, 'hotspot_low' => $data->hotspot_low,
+            'mode_collecte' => 'COLLECTE', 'utilisateur_collecte' => $utilisateur_collecte,
+            'date_enregistrement' => $dateEnregistrement
         ];
+
        /** Enregistrement dans le table historique */
-        $historique=$historiqueRepository->insertHistoriqueAjoutProjet($map,$json);
-        if ($historique['code']!=200 && $historique['code']!=23505) {
-            return ['code' => $historique['code'], 'erreur'=>[$historique['erreur']],
-            'rq'=>'insertTodo'];
+        $historique = $historiqueRepository->insertHistoriqueAjoutProjet($map, $json);
+        if ($historique['code'] != 200 && $historique['code'] != 23505) {
+            return new JsonResponse(['code' => $historique['code'], 'erreur' => $historique['erreur']]);
         }
-        if ($historique['code']===23505){
-            return new JsonResponse(["code" => 202], Response::HTTP_OK);
+        if ($historique['code'] === 23505){
+            return new JsonResponse(['code' => $historique['code'], 'erreur' => $historique['erreur']], Response::HTTP_OK);
         }
 
     /** Tout va bien ! */
-    return new JsonResponse(["code" => 200], Response::HTTP_OK);
+    return new JsonResponse(['code' => 200], Response::HTTP_OK);
     }
+
 }
---
+++ tests/Unit/Controller/Projet/ApiEnregistrementControllerTest.php
@@ -0,0 +1,235 @@
+<?php
+
+namespace App\Tests\Unit\Controller\Projet;
+
+use App\Repository\HistoriqueRepository;
+use App\Repository\UtilisateurRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * [Description ApiEnregistrementControllerTest]
+ */
+class ApiEnregistrementControllerTest extends WebTestCase
+{
+    private static $contentType = 'application/json';
+    private static $erreur400 = 'La requête est incorrecte (Erreur 400).';
+    public static $reference = "<strong>[Enregistrement]</strong> ";
+    private static $josh = 'josh.liberman@ma-moulinette.fr';
+    private static $aurelie = 'aurelie.petit-coeur@ma-moulinette.fr';
+    private static $apiEnregistrement = '/api/enregistrement';
+
+    public function testEnregistrementAvecDataNull(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UtilisateurRepository::class);
+        $testUser = $userRepository->findOneByCourriel(static::$aurelie);
+        $client->loginUser($testUser);
+
+        //Envoyer une requête avec un body vide
+        $client->request('PUT', static::$apiEnregistrement, [], [], ['CONTENT_TYPE' => static::$contentType], null);
+        $response = $client->getResponse();
+
+        $this->assertNotEmpty($response->getContent(), 'La réponse de l\'API est vide.');
+        $jsonResponse = json_decode($response->getContent(), true);
+
+        $this->assertNotNull($jsonResponse, 'La réponse JSON est null.');
+        $this->assertEquals(200, $response->getStatusCode(), 'Le code HTTP attendu est 200.');
+        $this->assertNull($jsonResponse['data'], 'Le champ "data" devrait être null.');
+        $this->assertEquals(400, $jsonResponse['code'], 'Le code attendu est 400.');
+        $this->assertEquals('alert', $jsonResponse['type'], 'Le type attendu est "alert".');
+        $expectedMessage = static::$reference . static::$erreur400;
+        $this->assertStringContainsString($expectedMessage, $jsonResponse['message']);
+    }
+
+    //"message" => "<strong>[Enregistrement]</strong> La requête est incorrecte (Erreur 400)."
+
+    public function testEnregistrement(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UtilisateurRepository::class);
+        $testUser = $userRepository->findOneByCourriel(static::$aurelie);
+        $client->loginUser($testUser);
+
+        /** On supprime la maven_key avant de l'insérer */
+        $historiqueRepository = static::getContainer()->get(HistoriqueRepository::class);
+        $entityManager = static::getContainer()->get('doctrine.orm.entity_manager');
+
+        $historique = $historiqueRepository->findOneBy(['mavenKey' => 'example_key', 'analyseKey' => 'example_key']);
+        if ($historique) {
+            $entityManager->remove($historique);
+            $entityManager->flush();
+        }
+
+        $data = [
+            'maven_key' => 'example_key',
+            'analyse_key' => 'example_key',
+            'version' => '1.0.0',
+            'date_version' => '2023-10-01',
+            'nom_projet' => 'Example Project',
+            'version_release' => 2,
+            'version_snapshot' => 4,
+            'version_autre' => 0,
+            'suppress_warning' => 10,
+            'no_sonar' => 140,
+            'todo' => 13,
+            'logger_info' => 45,
+            'logger_warn' => 5,
+            'logger_error' => 12,
+            'logger_debug' => 23,
+            'nombre_ligne' => 1000,
+            'nombre_ligne_code' => 800,
+            'coverage' => 90.7,
+            'files' => 45,
+            'classes' => 120,
+            'functions' => 342,
+            'duplicated_lines_density' => 5,
+            'sqale_debt_ratio' => 0.5,
+            'tests' => 50,
+            'violations' => 10,
+            'dette' => 20,
+            'nombre_bug' => 5,
+            'nombre_vulnerability' => 2,
+            'nombre_code_smell' => 3,
+            'bug_blocker' => 1,
+            'bug_critical' => 1,
+            'bug_major' => 1,
+            'bug_minor' => 1,
+            'bug_info' => 1,
+            'vulnerability_blocker' => 1,
+            'vulnerability_critical' => 1,
+            'vulnerability_major' => 1,
+            'vulnerability_minor' => 1,
+            'vulnerability_info' => 1,
+            'code_smell_blocker' => 1,
+            'code_smell_critical' => 1,
+            'code_smell_major' => 1,
+            'code_smell_minor' => 1,
+            'code_smell_info' => 1,
+            'frontend' => 159,
+            'backend' => 30,
+            'autre' => 12,
+            'inconnue' => 0,
+            'nombre_anomalie_bloquant' => 1,
+            'nombre_anomalie_critique' => 1,
+            'nombre_anomalie_majeur' => 1,
+            'nombre_anomalie_mineur' => 1,
+            'nombre_anomalie_info' => 1,
+            'note_reliability' => 1,
+            'note_security' => 2,
+            'note_sqale' => 3,
+            'note_hotspot' => 4,
+            'nombre_hotspot' => 10,
+            'hotspot_high' => 5,
+            'hotspot_medium' => 3,
+            'hotspot_low' => 2,
+        ];
+
+        $client->request('PUT', static::$apiEnregistrement, [], [], ['CONTENT_TYPE' => static::$contentType], json_encode($data));
+
+        $response = $client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $jsonResponse = json_decode($response->getContent(), true);
+        $this->assertEquals(200, $jsonResponse['code']);
+    }
+
+    public function testEnregistrementHistoriqueAlreadySave(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UtilisateurRepository::class);
+        $testUser = $userRepository->findOneByCourriel(static::$aurelie);
+        $client->loginUser($testUser);
+
+        $data = [
+            'maven_key' => 'example_key', 'analyse_key' => 'example_key', 'version' => '1.0.0',
+            'date_version' => '2023-10-01', 'nom_projet' => 'Example Project', 'version_release' => 2,
+            'version_snapshot' => 4, 'version_autre' => 0, 'suppress_warning' => 10, 'no_sonar' => 140,
+            'todo' => 13, 'logger_info' => 45, 'logger_warn' => 5, 'logger_error' => 12,
+            'logger_debug' => 23, 'nombre_ligne' => 1000, 'nombre_ligne_code' => 800, 'coverage' => 90.7,
+            'files' => 45, 'classes' => 120, 'functions' => 342, 'duplicated_lines_density' => 5,
+            'sqale_debt_ratio' => 0.5, 'tests' => 50, 'violations' => 10, 'dette' => 20, 'nombre_bug' => 5,
+            'nombre_vulnerability' => 2, 'nombre_code_smell' => 3, 'bug_blocker' => 1, 'bug_critical' => 1,
+            'bug_major' => 1, 'bug_minor' => 1, 'bug_info' => 1, 'vulnerability_blocker' => 1,
+            'vulnerability_critical' => 1, 'vulnerability_major' => 1, 'vulnerability_minor' => 1,
+            'vulnerability_info' => 1, 'code_smell_blocker' => 1, 'code_smell_critical' => 1,
+            'code_smell_major' => 1, 'code_smell_minor' => 1, 'code_smell_info' => 1,
+            'frontend' => 159, 'backend' => 30, 'autre' => 12, 'inconnue' => 0,
+            'nombre_anomalie_bloquant' => 1, 'nombre_anomalie_critique' => 1, 'nombre_anomalie_majeur' => 1,
+            'nombre_anomalie_mineur' => 1, 'nombre_anomalie_info' => 1, 'note_reliability' => 1,
+            'note_security' => 2, 'note_sqale' => 3, 'note_hotspot' => 4, 'nombre_hotspot' => 10,
+            'hotspot_high' => 5, 'hotspot_medium' => 3, 'hotspot_low' => 2,
+        ];
+
+        $client->request('PUT', static::$apiEnregistrement, [], [], ['CONTENT_TYPE' => static::$contentType], json_encode($data));
+
+        $response = $client->getResponse();
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $jsonResponse = json_decode($response->getContent(), true);
+        $this->assertEquals(23505, $jsonResponse['code']);
+        $this->assertEquals('Les informations existent déjà.', $jsonResponse['erreur']);
+    }
+
+    public function testEnregistrementSansRoleCollecte(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UtilisateurRepository::class);
+        $testUser = $userRepository->findOneByCourriel(static::$josh);
+        $client->loginUser($testUser);
+
+        $client->request('PUT', static::$apiEnregistrement, [], [], ['CONTENT_TYPE' => static::$contentType], '{}');
+
+        $response = $client->getResponse();
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        $jsonResponse = json_decode($response->getContent(), true);
+        $this->assertEquals(403, $jsonResponse['code']);
+    }
+
+    public function testEnregistrementHistoriqueServerError(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UtilisateurRepository::class);
+        $testUser = $userRepository->findOneByCourriel(static::$aurelie);
+        $client->loginUser($testUser);
+
+        // Données mal formattées (ex: valeur non attendue)
+        $data = [
+            'maven_key' => null, 'analyse_key' => 'example_key', 'version' => '1.0.0',
+            'date_version' => '2023-10-01', 'nom_projet' => 'Example Project', 'version_release' => 2,
+            'version_snapshot' => 4, 'version_autre' => 0, 'suppress_warning' => 10, 'no_sonar' => 140,
+            'todo' => 13, 'logger_info' => 45, 'logger_warn' => 5, 'logger_error' => 12,
+            'logger_debug' => 23, 'nombre_ligne' => 1000, 'nombre_ligne_code' => 800, 'coverage' => 90.7,
+            'files' => 45, 'classes' => 120, 'functions' => 342, 'duplicated_lines_density' => 5,
+            'sqale_debt_ratio' => 0.5, 'tests' => 50, 'violations' => 10, 'dette' => 20, 'nombre_bug' => 5,
+            'nombre_vulnerability' => 2, 'nombre_code_smell' => 3, 'bug_blocker' => 1, 'bug_critical' => 1,
+            'bug_major' => 1, 'bug_minor' => 1, 'bug_info' => 1, 'vulnerability_blocker' => 1,
+            'vulnerability_critical' => 1, 'vulnerability_major' => 1, 'vulnerability_minor' => 1,
+            'vulnerability_info' => 1, 'code_smell_blocker' => 1, 'code_smell_critical' => 1,
+            'code_smell_major' => 1, 'code_smell_minor' => 1, 'code_smell_info' => 1,
+            'frontend' => 159, 'backend' => 30, 'autre' => 12, 'inconnue' => 0,
+            'nombre_anomalie_bloquant' => 1, 'nombre_anomalie_critique' => 1, 'nombre_anomalie_majeur' => 1,
+            'nombre_anomalie_mineur' => 1, 'nombre_anomalie_info' => 1, 'note_reliability' => 1,
+            'note_security' => 2, 'note_sqale' => 3, 'note_hotspot' => 4, 'nombre_hotspot' => 10,
+            'hotspot_high' => 5, 'hotspot_medium' => 3, 'hotspot_low' => 2,
+        ];
+
+        $client->request('PUT', static::$apiEnregistrement, [], [], ['CONTENT_TYPE' => static::$contentType], json_encode($data));
+
+        $response = $client->getResponse();
+        $responseContent = $response->getContent();
+        $this->assertNotEmpty($responseContent, 'La réponse de l\'API est vide.');
+
+        $jsonResponse = json_decode($response->getContent(), true);
+        $this->assertNotNull($jsonResponse, 'La réponse JSON est null. L\'API renvoie bien un JSON valide ?');
+        $this->assertArrayHasKey('code', $jsonResponse);
+        $this->assertArrayHasKey('erreur', $jsonResponse);
+        $this->assertNotEquals(200, $jsonResponse['code']);
+        $this->assertNotEquals(23505, $jsonResponse['code']);
+        $this->assertEquals(500, $jsonResponse['code']);
+        $this->assertStringContainsString('An exception occurred while executing a query: SQLSTATE[23502]: Not null violation: 7', $jsonResponse['erreur']);
+    }
+
+}
