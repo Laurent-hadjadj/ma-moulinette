fa8cbbf8a4331ee18910f560efb87e1b13fe6700
Laurent HADJADJ <laurent_h@me.com>
2025-02-04 12:37:15 +0100

    Refactor: Amélioration du gestionnaire d'erreur.

--- src/Repository/MesuresRepository.php
+++ src/Repository/MesuresRepository.php
@@ -44,11 +44,21 @@ class MesuresRepository extends ServiceEntityRepository
    */
   protected function handleDatabaseException(\Doctrine\DBAL\Exception $e): array
   {
+    $message = $e->getMessage();
+
     if (strpos($e->getMessage(), 'SQLSTATE[08006]') !== false) {
-      return ['code' => 500, 'erreur' => static::$noDataBase];
-    } else {
-      return ['code' => 500, 'erreur' => $e->getMessage()];
+        $message = static::$noDataBase;
     }
+
+    if ($e->getSqlState() == '23502') {
+        $message = $e->getMessage();
+    }
+
+    if ($e->getSqlState() == '23505'){
+        return ['code' => 23505, 'erreur' => 'Les informations existent déjà.'];
+    }
+
+    return ['code' => 500, 'erreur'=> $message];
   }
 
   /**
@@ -125,7 +135,7 @@ class MesuresRepository extends ServiceEntityRepository
         $this->getEntityManager()->getConnection()->commit();
     } catch (\Doctrine\DBAL\Exception $e) {
         $this->getEntityManager()->getConnection()->rollBack();
-        return ['code' => 500, 'erreur' => $e->getMessage()];
+        return $this->handleDatabaseException($e);
     }
 
     return ['code' => 200, 'erreur' => ''];
