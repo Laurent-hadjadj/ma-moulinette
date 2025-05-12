b1af5f2ca85a84dbab772c9214b645f8747f9740
Laurent HADJADJ <laurent_h@me.com>
2025-05-12 17:29:53 +0200

    Test: Amélioration de la couverture de tests pour la classe PortefeuilleRepository.

---
+++ tests/Unit/Repository/PortefeuilleRepositoryHandlerTest.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+*  Ma-Moulinette
+*  --------------
+*  Copyright (c) 2021-2025.
+*  Laurent HADJADJ <laurent_h@me.com>.
+*  Licensed Creative Common  CC-BY-NC-SA 4.0.
+*  ---
+*  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+*  http://creativecommons.org/licenses/by-nc-sa/4.0/
+*/
+
+namespace App\Tests\Unit\Repository;
+
+use App\Repository\PortefeuilleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Statement;
+use Doctrine\DBAL\Exception as DBALException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * [Description PortefeuilleRepositoryHandlerTest]
+ */
+class PortefeuilleRepositoryHandlerTest extends TestCase
+{
+    public function testSelectPortefeuille_WhenSQLException(): void
+    {
+        // 1) Exception DBAL factice
+        /** @var DBALException&\Throwable $fakeException */
+        $fakeException = $this->createMock(DBALException::class);
+
+        // 2) Mock partiel de Doctrine\DBAL\Statement
+        $stmtStub = $this->getMockBuilder(Statement::class)
+                        ->disableOriginalConstructor()
+                        ->onlyMethods(['bindValue', 'executeQuery'])
+                        ->getMock();
+
+        // bindValue() : pas d'effet
+        $stmtStub->method('bindValue')->withAnyParameters();
+        // executeQuery() : on jette notre exception
+        $stmtStub->method('executeQuery')->willThrowException($fakeException);
+
+        // 3) Mock de Connection dont prepare() renvoie notre Statement mocké
+        $connectionMock = $this->createMock(Connection::class);
+        $connectionMock->method('prepare')
+            ->with($this->isType('string'))
+            ->willReturn($stmtStub);
+
+        // 4) Stub d'EntityManager pour renvoyer le Connection mocké
+        $emStub = $this->createMock(EntityManagerInterface::class);
+        $emStub->method('getConnection')
+            ->willReturn($connectionMock);
+
+        // 5) Partial mock du Repository pour surcharger getEntityManager() & handleDatabaseException()
+        $registry = $this->createMock(ManagerRegistry::class);
+        $repo = $this->getMockBuilder(PortefeuilleRepository::class)
+                    ->setConstructorArgs([$registry])
+                    ->onlyMethods(['getEntityManager', 'handleDatabaseException'])
+                    ->getMock();
+
+        // 6) On s'attend à handleDatabaseException() appelé une fois avec notre exception
+        $expected = ['code' => 500, 'erreur' => 'test-error'];
+        $repo->expects($this->once())->method('handleDatabaseException')
+            ->with($fakeException)
+            ->willReturn($expected);
+
+        // 7) getEntityManager() renvoie notre EM stub
+        $repo->method('getEntityManager')->willReturn($emStub);
+
+        // 8) Appel de la méthode
+        $map = ['portefeuille' => 'exemple de projets'];
+        $result = $repo->selectPortefeuille($map);
+
+        // 9) Vérification
+        $this->assertSame($expected, $result);
+    }
+}
