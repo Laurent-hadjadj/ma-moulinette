2b1e54208a866bd6faa3f0887c5d4704255fbad3
Laurent HADJADJ <laurent_h@me.com>
2025-05-12 12:07:00 +0200

    Test: Mise à jour de la date de copyright

--- tests/Unit/Repository/ListeProjetRepositoryTest.php
+++ tests/Unit/Repository/ListeProjetRepositoryTest.php
@@ -3,7 +3,7 @@
 /*
  *  Ma-Moulinette
  *  --------------
- *  Copyright (c) 2021-2024.
+ *  Copyright (c) 2021-2025.
  *  Laurent HADJADJ <laurent_h@me.com>.
  *  Licensed Creative Common  CC-BY-NC-SA 4.0.
  *  ---
