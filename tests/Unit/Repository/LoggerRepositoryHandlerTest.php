aab527c8c4f7aa13b753f8e59540b4b641bd6fce
Laurent HADJADJ <laurent_h@me.com>
2025-05-12 16:36:43 +0200

    Test: Fix, mauvais copier coller

--- tests/Unit/Repository/LoggerRepositoryHandlerTest.php
+++ tests/Unit/Repository/LoggerRepositoryHandlerTest.php
@@ -30,58 +30,6 @@ class LoggerRepositoryHandlerTest extends TestCase
     private static $mavenKey = 'fr.ma-petite-entreprise:ma-moulinette';
     private static $gestionTest = 'gestion test';
 
-    public function testSelectLoggerIsValide_WhenSQLException(): void
-    {
-        // 1) Exception DBAL factice
-        /** @var DBALException&\Throwable $fakeException */
-        $fakeException = $this->createMock(DBALException::class);
-
-        // 2) Mock partiel de Doctrine\DBAL\Statement
-        $stmtStub = $this->getMockBuilder(Statement::class)
-                        ->disableOriginalConstructor()
-                        ->onlyMethods(['bindValue', 'executeQuery'])
-                        ->getMock();
-
-        // bindValue() : pas d'effet
-        $stmtStub->method('bindValue')->withAnyParameters();
-        // executeQuery() : on jette notre exception
-        $stmtStub->method('executeQuery')->willThrowException($fakeException);
-
-        // 3) Mock de Connection dont prepare() renvoie notre Statement mocké
-        $connectionMock = $this->createMock(Connection::class);
-        $connectionMock->method('prepare')
-            ->with($this->isType('string'))
-            ->willReturn($stmtStub);
-
-        // 4) Stub d'EntityManager pour renvoyer le Connection mocké
-        $emStub = $this->createMock(EntityManagerInterface::class);
-        $emStub->method('getConnection')
-            ->willReturn($connectionMock);
-
-        // 5) Partial mock du Repository pour surcharger getEntityManager() & handleDatabaseException()
-        $registry = $this->createMock(ManagerRegistry::class);
-        $repo = $this->getMockBuilder(LoggerRepository::class)
-                    ->setConstructorArgs([$registry])
-                    ->onlyMethods(['getEntityManager', 'handleDatabaseException'])
-                    ->getMock();
-
-        // 6) On s'attend à handleDatabaseException() appelé une fois avec notre exception
-        $expected = ['code' => 500, 'erreur' => 'test-error'];
-        $repo->expects($this->once())->method('handleDatabaseException')
-            ->with($fakeException)
-            ->willReturn($expected);
-
-        // 7) getEntityManager() renvoie notre EM stub
-        $repo->method('getEntityManager')->willReturn($emStub);
-
-        // 8) Appel de la méthode
-        $map = ['maven_key' => static::$mavenKey];
-        $result = $repo->selectLoggerIsValide($map);
-
-        // 9) Vérification
-        $this->assertSame($expected, $result);
-    }
-
     public function testSelectLogger_WhenSQLException(): void
     {
         // 1) Exception DBAL factice
