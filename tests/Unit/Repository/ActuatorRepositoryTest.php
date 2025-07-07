6fff22ee8fd6535ffea8720573c7253a8e80c0f5
Laurent HADJADJ <laurent_h@me.com>
2025-07-07 10:54:57 +0200

    Test: Ajout des unitaire pour actuatorRepository.

---
+++ tests/Unit/Repository/ActuatorRepositoryTest.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ *  Ma-Moulinette
+ *  --------------
+ *  Copyright (c) 2021-2025.
+ *  Laurent HADJADJ <laurent_h@me.com>.
+ *  Licensed Creative Common  CC-BY-NC-SA 4.0.
+ *  ---
+ *  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+ *  http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+namespace App\Tests\Unit\Repository;
+
+use App\Entity\Actuator;
+use App\DataFixtures\ActuatorFixtures;
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * [Description ActuatorRepositoryTest]
+ */
+class ActuatorRepositoryTest extends KernelTestCase
+{
+    private static $erreurCode200 = 'Erreur le code retour doit être 200';
+    private static $mavenKey = 'fr.ma-moulinette:app1';
+    private static $nomApplication = 'Application 01';
+    private static $url = 'http://ma-moulinette.fr/app01';
+    private static $actuatorUser = 'user1';
+    private static $actuatorPassword = 'password1';
+    private static $personne ='John Doe';
+
+    protected function setUp(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        $entityManager = $container->get('doctrine')->getManager();
+
+        // Réinitialiser la séquence
+        $connection = $entityManager->getConnection();
+        $platform = $connection->getDatabasePlatform('SET search_path TO ma_moulinette_test');
+
+        if ($platform instanceof \Doctrine\DBAL\Platforms\PostgreSqlPlatform) {
+            $sequence = 'ma_moulinette.actuator_id_seq';
+            $connection->executeQuery("SELECT setval('$sequence', 1, false);");
+        }
+
+        $purger = new ORMPurger($entityManager);
+        $executor = new ORMExecutor($entityManager, $purger);
+        $executor->execute([new ActuatorFixtures()]);
+    }
+
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        // On se déconnecte pour éviter des problèmes de mémoires
+        $container = static::getContainer();
+        $entityManager = $container->get('doctrine')->getManager();
+        $entityManager->close();
+        $entityManager = null;
+    }
+}
