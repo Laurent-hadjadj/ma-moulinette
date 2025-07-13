2993ba3419ebfda5869125e13f83cf8703ccdfdb
Laurent HADJADJ <laurent_h@me.com>
2025-07-13 12:13:36 +0200

    Code-Clean: Formatage du code.

--- src/Service/UrlBuilderService.php
+++ src/Service/UrlBuilderService.php
@@ -1,15 +1,15 @@
 <?php
 
 /*
- *  Ma-Moulinette
- *  --------------
- *  Copyright (c) 2021-2025.
- *  Laurent HADJADJ <laurent_h@me.com>.
- *  Licensed Creative Common  CC-BY-NC-SA 4.0.
- *  ---
- *  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
- *  http://creativecommons.org/licenses/by-nc-sa/4.0/
- */
+*  Ma-Moulinette
+*  --------------
+*  Copyright (c) 2021-2025.
+*  Laurent HADJADJ <laurent_h@me.com>.
+*  Licensed Creative Common  CC-BY-NC-SA 4.0.
+*  ---
+*  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+*  http://creativecommons.org/licenses/by-nc-sa/4.0/
+*/
 
 namespace App\Service;
 
@@ -21,25 +21,25 @@ use Psr\Log\LoggerInterface;
 class UrlBuilderService
 {
 
-  public function __construct(
+    public function __construct(
         private LoggerInterface $logger
     ) {
     }
 
-  /**
-   * [Description for build]
-   *
-   * @param string $baseUrl
-   * @param string $path
-   * @param array $queryParams
-   *
-   * @return string
-   *
-   * Created at: 13/07/2025 11:44:09 (Europe/Paris)
-   * @author     Laurent HADJADJ <laurent_h@me.com>
-   * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
-   */
-  public function build(string $baseUrl, string $path, array $queryParams = []): string
+    /**
+     * [Description for build]
+     *
+     * @param string $baseUrl
+     * @param string $path
+     * @param array $queryParams
+     *
+     * @return string
+     *
+     * Created at: 13/07/2025 11:44:09 (Europe/Paris)
+     * @author     Laurent HADJADJ <laurent_h@me.com>
+     * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function build(string $baseUrl, string $path, array $queryParams = []): string
     {
         // Construction propre de l'URL
         $url = rtrim($baseUrl, '/') . '/' . ltrim($path, '/');
@@ -73,4 +73,4 @@ class UrlBuilderService
 
         return $url;
     }
-  }
+}
