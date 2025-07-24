4ca6f1ad8ec080b9723419550240fd11e4fecf47
Laurent HADJADJ <laurent_h@me.com>
2025-07-24 10:06:09 +0200

    Refactor: Ajout du type debug.

--- assets/js/common/messageHelper.js
+++ assets/js/common/messageHelper.js
@@ -188,6 +188,7 @@ export const showMessage = function(type, message, technicalDetails = null) {
         info: 'ℹ️',
         primary: '📌',
         secondary: '📄',
+        critical: '🔴',
         debug: '🛠️',
         delete: '🗑️',
         notAuthorize: '🚫'
