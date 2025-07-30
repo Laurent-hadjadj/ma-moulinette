f2318ab9104ecd0bf16a47fd4b9a9252019dadd7
Laurent HADJADJ <laurent_h@me.com>
2025-07-30 08:54:37 +0200

    Fix: Renommage de la cle du token + suppression du = a la fin.

--- assets/js/mon-application/projet/index-projet.js
+++ assets/js/mon-application/projet/index-projet.js
@@ -1639,7 +1639,8 @@ $('.js-cosui').on('click', () => {
   /** on créé un token pour encoder les paramètres */
   const param = `${salt}|${apiMaven}`;
   const a = encode(btoa(param));
-  location.href=`${serveur()}/projet/cosui?maven_key=${a}`;
+  const b = a.slice(0, -1);
+  location.href=`${serveur()}/projet/cosui?token=${b}`;
   } else {
     log(' - ERROR - [COSUI] - Vous devez choisir un projet dans la liste !! !');
     }
