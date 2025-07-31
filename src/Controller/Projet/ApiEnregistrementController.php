c1b84b844ea8988e93401311e3dd22c677fd1b7d
Laurent HADJADJ <laurent_h@me.com>
2025-07-31 14:38:09 +0200

    Refactor: Réorganisation des imports, ajout d'un try/catch sur l’insertion des données.

--- src/Controller/Projet/ApiEnregistrementController.php
+++ src/Controller/Projet/ApiEnregistrementController.php
@@ -14,16 +14,15 @@
 namespace App\Controller\Projet;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\SecurityBundle\Security;
-
-/** Gestion de accès aux API */
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Psr\Log\LoggerInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 use App\Entity\Historique;
-use Doctrine\ORM\EntityManagerInterface;
 
 /**
  * [Description ApiEnregistrementController]
@@ -43,12 +42,10 @@ class ApiEnregistrementController extends AbstractController
      * @author    Laurent HADJADJ <laurent_h@me.com>
      * @copyright Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
      */
-    private $security;
-    private $em;
-
     public function __construct(
-        EntityManagerInterface $em,
-        Security $security
+        private EntityManagerInterface $em,
+        private Security $security,
+        private LoggerInterface $logger
     ) {
         $this->em = $em;
         $this->security = $security;
@@ -75,8 +72,11 @@ class ApiEnregistrementController extends AbstractController
         /** On vérifie si l'utilisateur à un rôle Collecte ? */
         if (!$this->isGranted('ROLE_COLLECTE')) {
         return new JsonResponse(
-            ['code' => 403, 'type'=>'warning',
-            'message' => static::$reference . static::$erreur403], Response::HTTP_OK);
+            [
+                'code' => 403,
+                'type'=>'warning',
+                'message' => static::$reference . static::$erreur403
+            ], Response::HTTP_OK);
         }
 
         /** On décode le body. */
@@ -84,8 +84,10 @@ class ApiEnregistrementController extends AbstractController
         /** On teste si la clé est valide */
         if ($data === null) {
         return new JsonResponse([
-            'data' => $data,'code' => 400, 'type' => 'alert',
-            'message' => static::$reference . static::$erreur400], Response::HTTP_OK);
+            'code' => 400,
+            'type' => 'alert',
+            'message' => static::$reference . static::$erreur400
+            ], Response::HTTP_OK);
         }
 
         /** On créé un objet date Immutable, avec la date courante. */
@@ -93,41 +95,51 @@ class ApiEnregistrementController extends AbstractController
         /** On contrôle le mode d'utilisation */
         $utilisateur_collecte = $this->security->getUser()->getCourriel();
 
-        $json = '{}';
-        $map = ['maven_key' => $data->maven_key, 'analyse_key' => $data->analyse_key,
-            'version' => $data->version, 'date_version' => $data->date_version,
-            'nom_projet' => $data->nom_projet, 'version_release' => $data->version_release, 'version_snapshot' => $data->version_snapshot, 'version_autre' => $data->version_autre,
-            'suppress_warning' => $data->suppress_warning, 'no_sonar' => $data->no_sonar,
-            'todo' => $data->todo,
-            'logger_info' => $data->logger_info, 'logger_warn' => $data->logger_warn, 'logger_error' => $data->logger_error, 'logger_debug' => $data->logger_debug,
-            'nombre_ligne' => $data->nombre_ligne, 'nombre_ligne_code' => $data->nombre_ligne_code,
-            'files' => $data->files, 'classes' => $data->classes, 'functions' => $data->functions,
-            'coverage' => $data->coverage, 'duplicated_lines_density' => $data->duplicated_lines_density, 'sqale_debt_ratio' => $data->sqale_debt_ratio, 'tests' => $data->tests, 'violations' => $data->violations, 'dette' => $data->dette,
-            'nombre_bug' => $data->nombre_bug, 'nombre_vulnerability' => $data->nombre_vulnerability, 'nombre_code_smell' => $data->nombre_code_smell,
-            'bug_blocker' => $data->bug_blocker, 'bug_critical' => $data->bug_critical,
-            'bug_major' => $data->bug_major, 'bug_minor' => $data->bug_minor, 'bug_info' => $data->bug_info,
-            'vulnerability_blocker' => $data->vulnerability_blocker, 'vulnerability_critical' => $data->vulnerability_critical, 'vulnerability_major' => $data->vulnerability_major,
-            'vulnerability_minor' => $data->vulnerability_minor, 'vulnerability_info' => $data->vulnerability_info,
-            'code_smell_blocker' => $data->code_smell_blocker, 'code_smell_critical' => $data->code_smell_critical, 'code_smell_major' => $data->code_smell_major,
-            'code_smell_minor' => $data->code_smell_minor, 'code_smell_info' => $data->code_smell_info,
-            'frontend' => $data->frontend, 'backend' => $data->backend, 'autre' => $data->autre, 'inconnu' => $data->inconnu,
-            'nombre_anomalie_bloquant' => $data->nombre_anomalie_bloquant, 'nombre_anomalie_critique' => $data->nombre_anomalie_critique,
-            'nombre_anomalie_majeur' => $data->nombre_anomalie_majeur,
-            'nombre_anomalie_mineur' => $data->nombre_anomalie_mineur, 'nombre_anomalie_info' => $data->nombre_anomalie_info,
-            'note_reliability' => $data->note_reliability, 'note_security' => $data->note_security,
-            'note_sqale' => $data->note_sqale, 'note_hotspot' => $data->note_hotspot, 'nombre_hotspot' => $data->nombre_hotspot,
-            'hotspot_high' => $data->hotspot_high, 'hotspot_medium' => $data->hotspot_medium, 'hotspot_low' => $data->hotspot_low,
-            'mode_collecte' => 'COLLECTE', 'utilisateur_collecte' => $utilisateur_collecte,
-            'date_enregistrement' => $dateEnregistrement
-        ];
+        try {
+            $json = '{}';
+            $map = ['maven_key' => $data->maven_key, 'analyse_key' => $data->analyse_key,
+                'version' => $data->version, 'date_version' => $data->date_version,
+                'nom_projet' => $data->nom_projet, 'version_release' => $data->version_release, 'version_snapshot' => $data->version_snapshot, 'version_autre' => $data->version_autre,
+                'suppress_warning' => $data->suppress_warning, 'no_sonar' => $data->no_sonar,
+                'todo' => $data->todo,
+                'logger_info' => $data->logger_info, 'logger_warn' => $data->logger_warn, 'logger_error' => $data->logger_error, 'logger_debug' => $data->logger_debug,
+                'nombre_ligne' => $data->nombre_ligne, 'nombre_ligne_code' => $data->nombre_ligne_code,
+                'files' => $data->files, 'classes' => $data->classes, 'functions' => $data->functions,
+                'coverage' => $data->coverage, 'duplicated_lines_density' => $data->duplicated_lines_density, 'sqale_debt_ratio' => $data->sqale_debt_ratio, 'tests' => $data->tests, 'violations' => $data->violations, 'dette' => $data->dette,
+                'nombre_bug' => $data->nombre_bug, 'nombre_vulnerability' => $data->nombre_vulnerability, 'nombre_code_smell' => $data->nombre_code_smell,
+                'bug_blocker' => $data->bug_blocker, 'bug_critical' => $data->bug_critical,
+                'bug_major' => $data->bug_major, 'bug_minor' => $data->bug_minor, 'bug_info' => $data->bug_info,
+                'vulnerability_blocker' => $data->vulnerability_blocker, 'vulnerability_critical' => $data->vulnerability_critical, 'vulnerability_major' => $data->vulnerability_major,
+                'vulnerability_minor' => $data->vulnerability_minor, 'vulnerability_info' => $data->vulnerability_info,
+                'code_smell_blocker' => $data->code_smell_blocker, 'code_smell_critical' => $data->code_smell_critical, 'code_smell_major' => $data->code_smell_major,
+                'code_smell_minor' => $data->code_smell_minor, 'code_smell_info' => $data->code_smell_info,
+                'frontend' => $data->frontend, 'backend' => $data->backend, 'autre' => $data->autre, 'inconnu' => $data->inconnu,
+                'nombre_anomalie_bloquant' => $data->nombre_anomalie_bloquant, 'nombre_anomalie_critique' => $data->nombre_anomalie_critique,
+                'nombre_anomalie_majeur' => $data->nombre_anomalie_majeur,
+                'nombre_anomalie_mineur' => $data->nombre_anomalie_mineur, 'nombre_anomalie_info' => $data->nombre_anomalie_info,
+                'note_reliability' => $data->note_reliability, 'note_security' => $data->note_security,
+                'note_sqale' => $data->note_sqale, 'note_hotspot' => $data->note_hotspot, 'nombre_hotspot' => $data->nombre_hotspot,
+                'hotspot_high' => $data->hotspot_high, 'hotspot_medium' => $data->hotspot_medium, 'hotspot_low' => $data->hotspot_low,
+                'mode_collecte' => 'COLLECTE', 'utilisateur_collecte' => $utilisateur_collecte,
+                'date_enregistrement' => $dateEnregistrement
+            ];
 
-       /** Enregistrement dans le table historique */
-        $historique = $historiqueRepository->insertHistoriqueAjoutProjet($map, $json);
-        if ($historique['code'] != 200 && $historique['code'] != 23505) {
-            return new JsonResponse(['code' => $historique['code'], 'erreur' => $historique['erreur']]);
-        }
-        if ($historique['code'] === 23505){
-            return new JsonResponse(['code' => $historique['code'], 'erreur' => $historique['erreur']], Response::HTTP_OK);
+            /** Enregistrement dans le table historique */
+            $historique = $historiqueRepository->insertHistoriqueAjoutProjet($map, $json);
+            if ($historique['code'] != 200 && $historique['code'] != 23505) {
+                return new JsonResponse(['code' => $historique['code'], 'erreur' => $historique['erreur']]);
+            }
+            if ($historique['code'] === 23505){
+                return new JsonResponse(['code' => $historique['code'], 'erreur' => $historique['erreur']], Response::HTTP_OK);
+            }
+        } catch (\Throwable $e) {
+            $this->logger->critical("🔴 [Enregistrement] Erreur lors de l'enregistrement des données.", ['exception' => $e]);
+            return new JsonResponse([
+                'code' => 500,
+                'type' => 'critical',
+                'message' => "[Enregistrement] Erreur lors de l'enregistrement des données.",
+                'trace' => $e->getMessage()
+            ],Response::HTTP_OK);
         }
 
     /** Tout va bien ! */
