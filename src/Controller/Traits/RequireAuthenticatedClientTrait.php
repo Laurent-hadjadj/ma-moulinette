0fc18e794bbdf8e119fbd353a5dedfcaaa53c06f
Laurent HADJADJ <laurent_h@me.com>
2025-10-18 21:53:00 +0200

    Refactor: Ajout du Trait pour le contrôle d'accès aux API.

---
+++ src/Controller/Traits/RequireAuthenticatedClientTrait.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ *  Ma-Moulinette
+ *  --------------
+ *  Copyright (c) 2024.
+ *  Laurent HADJADJ <laurent_h@me.com>.
+ *  Licensed Creative Common  CC-BY-NC-SA 4.0.
+ *  ---
+ *  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+ *  http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+namespace App\Controller\Traits;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Vérifie que l'appel API provient bien du client autorisé.
+ */
+trait RequireAuthenticatedClientTrait
+{
+    /**
+     * Vérifie l'en-tête X-App-Client.
+     *
+     * @param Request $request
+     * @param string  $appClient  La valeur attendue (ex: depuis les paramètres de config)
+     *
+     * @return JsonResponse|null
+     */
+    public function checkApiClient(Request $request, string $appClient): ?JsonResponse
+    {
+        $clientHeader = $request->headers->get('X-App-Client');
+
+        if ($clientHeader !== $appClient) {
+            return new JsonResponse([
+                'code'    => 403,
+                'message' => '[API-Credential] 👻 Vous n’avez pas les droits pour accéder à cette ressource.'
+            ], JsonResponse::HTTP_FORBIDDEN);
+        }
+
+        return null; // ✅ tout est ok
+    }
+}
