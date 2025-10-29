ff8424e66d3fe8479976f5f18170958e91fce450
Laurent HADJADJ <laurent_h@me.com>
2025-10-29 20:57:45 +0100

    Refactor: Utilisation du Trait pour la conversion de type, ajout du handler d'exception, ajout de la requête deleteBatchExecution()

--- src/Repository/BatchExecutionRepository.php
+++ src/Repository/BatchExecutionRepository.php
@@ -16,18 +16,87 @@ namespace App\Repository;
 use App\Entity\BatchExecution;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\Traits\DoctrineParamHelperTrait;
 
 /**
  * [Description BatchExecutionRepository]
  */
 class BatchExecutionRepository extends ServiceEntityRepository
 {
+  use DoctrineParamHelperTrait;
+
   public static $removeReturnLine = "/\s+/u";
   public static $noDataBase = 'La connexion à la base de données a échoué.';
 
   public function __construct(ManagerRegistry $registry)
   {
       parent::__construct($registry, BatchExecution::class);
+  }
+
+  /**
+   * [Description for handleDatabaseException]
+   *
+   * @param \Throwable $e
+   *
+   * @return array
+   *
+   * Created at: 07/07/2025 09:36:06 (Europe/Paris)
+   * @author     Laurent HADJADJ <laurent_h@me.com>
+   * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+   */
+  public function handleDatabaseException(\Throwable $e): array
+  {
+    $message = $e->getMessage();
+
+    // message = 'SQLSTATE[08006]'
+    if ($e instanceof \Doctrine\DBAL\Exception\ConnectionException) {
+        $message = static::$noDataBase;
+    }
+
+    // state = '23502'
+    if ($e instanceof \Doctrine\DBAL\Exception\NotNullConstraintViolationException) {
+        $message = $e->getMessage();
+    }
+
+    // state = '23505'
+    if ($e instanceof \Doctrine\DBAL\Exception\UniqueConstraintViolationException) {
+        return ['code' => 23505, 'erreur' => 'Les informations existent déjà.'];
+    }
+
+    return ['code' => 500, 'erreur' => $message];
+  }
+
+  /**
+   * [Description for deleteTraitement]
+   *
+   * @param mixed $traitement_id
+   *
+   * @return array
+   *
+   * Created at: 29/10/2025 21:22:32 (Europe/Paris)
+   * @author     Laurent HADJADJ <laurent_h@me.com>
+   * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+   */
+  public function deleteTraitement($traitement_id): array
+  {
+    $sql = "DELETE
+            FROM ma_moulinette.batch_execution
+            WHERE traitement_id = :traitement_id";
+
+    $conn = $this->getEntityManager()->getConnection();
+
+    try {
+        $conn->beginTransaction();
+        $stmt = $conn->prepare(preg_replace(static::$removeReturnLine, " ", $sql));
+          $this->bindUlidAsString($stmt, ':traitement_id', $traitement_id);
+          $stmt->executeStatement();
+        $conn->commit();
+    } catch (\Throwable $e) {
+        $conn->rollBack();
+        return $this->handleDatabaseException($e);
+    }
+
+    return ['code' => 200, 'erreur' => ''];
   }
 
 }
