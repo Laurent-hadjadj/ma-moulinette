3c36cb9fe6db6ef71f406094652329e30e7ed452
Laurent HADJADJ <laurent_h@me.com>
2025-11-09 13:33:58 +0100

    Code-Clean: Optimisation des imports.

--- src/Controller/Batch/BatchCollecteActuatorController.php
+++ src/Controller/Batch/BatchCollecteActuatorController.php
@@ -16,11 +16,8 @@ namespace App\Controller\Batch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Psr\Log\LoggerInterface;
 use Doctrine\ORM\EntityManagerInterface;
-
-use App\Service\ClientService;
-use App\Entity\Actuator;
-use App\Entity\ActuatorInfo;
-use App\Service\UrlBuilderService;
+use App\Entity\{Actuator, ActuatorInfo};
+use App\Service\{ClientService, UrlBuilderService};
 
 /**
  * [Description BatchCollecteActuatorController]
