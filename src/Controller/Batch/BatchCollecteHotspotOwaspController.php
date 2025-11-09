88762d8f3ba22b3e385cba5e48be2ec4096076f1
Laurent HADJADJ <laurent_h@me.com>
2025-11-09 13:44:18 +0100

    Code-Clean: Optimisation des imports.

--- src/Controller/Batch/BatchCollecteHotspotOwaspController.php
+++ src/Controller/Batch/BatchCollecteHotspotOwaspController.php
@@ -16,11 +16,8 @@ namespace App\Controller\Batch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Psr\Log\LoggerInterface;
 use Doctrine\ORM\EntityManagerInterface;
-
-use App\Entity\HotspotOwasp;
-use App\Entity\InformationProjet;
-use App\Service\ClientService;
-use App\Service\UrlBuilderService;
+use App\Entity\{HotspotOwasp, InformationProjet};
+use App\Service\{ClientService, UrlBuilderService};
 
 /**
  * [Description BatchCollecteHotspotOwaspController]
