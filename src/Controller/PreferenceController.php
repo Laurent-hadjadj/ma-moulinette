f21d237d7beb2f83d9568d72304685649953dd92
Laurent HADJADJ <laurent_h@me.com>
2025-11-09 14:54:22 +0100

    Code-Clean: Optimisation des imports.

--- src/Controller/PreferenceController.php
+++ src/Controller/PreferenceController.php
@@ -16,12 +16,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\{JsonResponse, Request, Response};
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
-
 use App\Service\ClientService;
 
 class PreferenceController extends AbstractController
