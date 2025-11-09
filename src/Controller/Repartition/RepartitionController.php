243437502aa655083bc127dbd08a03b6764cad09
Laurent HADJADJ <laurent_h@me.com>
2025-11-09 14:50:18 +0100

    Code-Clean: Optimisation des imports.

--- src/Controller/Repartition/RepartitionController.php
+++ src/Controller/Repartition/RepartitionController.php
@@ -17,11 +17,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\{Request, Response};
 use Psr\Log\LoggerInterface;
 use Doctrine\ORM\EntityManagerInterface;
-
 use App\Controller\Batch\BatchCollecteRepartitionController;
 use App\Entity\Repartition;
 use App\Service\ExtractName;
