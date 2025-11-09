d70d8ea6dcf31d751e17889da32716a7f2d25973
Laurent HADJADJ <laurent_h@me.com>
2025-11-09 13:44:40 +0100

    Code-Clean: Optimisation des imports.

--- src/Controller/Batch/BatchCollecteNoSonarController.php
+++ src/Controller/Batch/BatchCollecteNoSonarController.php
@@ -16,10 +16,8 @@ namespace App\Controller\Batch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Psr\Log\LoggerInterface;
 use Doctrine\ORM\EntityManagerInterface;
-
 use App\Entity\NoSonar;
-use App\Service\ClientService;
-use App\Service\UrlBuilderService;
+use App\Service\{ClientService, UrlBuilderService};
 
 /**
  * [Description BatchCollecteNoSonarController]
