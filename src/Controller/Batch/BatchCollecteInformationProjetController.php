65586a12be02e44a7ab613827b42dabe554f789f
Laurent HADJADJ <laurent_h@me.com>
2025-11-09 13:44:23 +0100

    Code-Clean: Optimisation des imports.

--- src/Controller/Batch/BatchCollecteInformationProjetController.php
+++ src/Controller/Batch/BatchCollecteInformationProjetController.php
@@ -16,11 +16,8 @@ namespace App\Controller\Batch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Psr\Log\LoggerInterface;
 use Doctrine\ORM\EntityManagerInterface;
-
 use App\Entity\InformationProjet;
-use App\Service\ClientService;
-use App\Service\IsValideMavenKey;
-use App\Service\UrlBuilderService;
+use App\Service\{IsValideMavenKey, ClientService, UrlBuilderService};
 
 /**
  * [Description BatchCollecteInformationProjetController]
