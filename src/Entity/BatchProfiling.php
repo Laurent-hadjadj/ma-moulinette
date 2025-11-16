2bcfaec7f1e48c2a1d2d0718f0524b6c48fb9dba
Laurent HADJADJ <laurent_h@me.com>
2025-11-16 08:11:03 +0100

    Code-Clean: Correction du typage des attributs de l'entité.

--- src/Entity/BatchProfiling.php
+++ src/Entity/BatchProfiling.php
@@ -17,7 +17,6 @@ use Doctrine\ORM\Mapping as ORM;
 use Doctrine\DBAL\Types\Types;
 use App\Repository\BatchProfilingRepository;
 
-
 /**
  * [Description BatchProfiling]
  */
@@ -27,36 +26,55 @@ class BatchProfiling
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column(type: Types::INTEGER, nullable: false,
-        options: ['comment' => 'Identifiant unique de la table batch_profiling'])]
+    #[ORM\Column(type: Types::INTEGER, nullable: false, options: [
+        'comment' => 'Identifiant unique de la table batch_profiling'
+    ])]
     private ?int $id = null;
 
-    #[ORM\Column(type: TYPES::STRING, length: 64)]
+    #[ORM\Column(type: Types::STRING, length: 64, options: [
+        'comment' => 'Nom du portefeuille concerné par le traitement'
+    ])]
     private string $portefeuille;
 
-    #[ORM\Column(type: TYPES::STRING, length: 255, nullable: true)]
+    #[ORM\Column(type: Types::STRING, length: 255, nullable: true, options: [
+        'comment' => 'Référence unique d’exécution du traitement (token ou UUID)'
+    ])]
     private ?string $executionReference = null;
 
-    #[ORM\Column(type: TYPES::INTEGER)]
+    #[ORM\Column(type: Types::INTEGER, options: [
+        'comment' => 'Nombre total de projets traités dans le batch'
+    ])]
     private int $nbProjets;
 
-    #[ORM\Column(type: TYPES::FLOAT)]
+    #[ORM\Column(type: Types::FLOAT, options: [
+        'comment' => 'Durée totale d’exécution en secondes'
+    ])]
     private float $tempsTotal;
 
-    #[ORM\Column(type: TYPES::FLOAT)]
+    #[ORM\Column(type: Types::FLOAT, options: [
+        'comment' => 'Durée moyenne d’exécution par projet (en secondes)'
+    ])]
     private float $tempsMoyen;
 
-    #[ORM\Column(type: TYPES::FLOAT)]
+    #[ORM\Column(type: Types::FLOAT, options: [
+        'comment' => 'Consommation mémoire maximale observée (en Mo)'
+    ])]
     private float $memoirePeak;
 
-    #[ORM\Column(type: TYPES::FLOAT)]
+    #[ORM\Column(type: Types::FLOAT, options: [
+        'comment' => 'Consommation mémoire moyenne observée (en Mo)'
+    ])]
     private float $memoireMoyenne;
 
-    #[ORM\Column(type: TYPES::STRING, length: 128)]
+    #[ORM\Column(type: Types::STRING, length: 128, options: [
+        'comment' => 'Identifiant ou adresse e-mail de l’utilisateur exécutant le batch'
+    ])]
     private string $utilisateur;
 
-    #[ORM\Column(type: Types::DATETIMETZ_IMMUTABLE, nullable: false,
-        options: ['comment' => "Date d'exécution de la collecte."])]
+    #[ORM\Column(type: Types::DATETIMETZ_IMMUTABLE, nullable: false, options: [
+        'default' => 'CURRENT_TIMESTAMP',
+        'comment' => "Horodatage de l’exécution du batch (timezone Europe/Paris)"
+    ])]
     private \DateTimeImmutable $dateExecution;
 
     public function __construct(
@@ -80,9 +98,11 @@ class BatchProfiling
         $this->dateExecution = new \DateTimeImmutable('now', new \DateTimeZone('Europe/Paris'));
     }
 
-    // getters uniquement pour consultation
+    // --- Getters ---
+
     public function getId(): ?int { return $this->id; }
     public function getPortefeuille(): string { return $this->portefeuille; }
+    public function getExecutionReference(): ?string { return $this->executionReference; }
     public function getNbProjets(): int { return $this->nbProjets; }
     public function getTempsTotal(): float { return $this->tempsTotal; }
     public function getTempsMoyen(): float { return $this->tempsMoyen; }
@@ -90,5 +110,32 @@ class BatchProfiling
     public function getMemoireMoyenne(): float { return $this->memoireMoyenne; }
     public function getUtilisateur(): string { return $this->utilisateur; }
     public function getDateExecution(): \DateTimeImmutable { return $this->dateExecution; }
-    public function getExecutionReference(): ?string { return $this->executionReference; }
+
+    // --- Méthodes utilitaires (facultatives mais utiles dans Twig ou debug) ---
+
+    public function getDateExecutionFormatted(string $format = 'd/m/Y H:i'): string
+    {
+        return $this->dateExecution->format($format);
+    }
+
+    /**
+     * [Description for __toString]
+     *
+     * @return string
+     * 
+     * Created at: 13/11/2025 21:17:41 (Europe/Paris)
+     * @author     Laurent HADJADJ <laurent_h@me.com> 
+     * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0. 
+     */
+    public function __toString(): string
+    {
+        return sprintf(
+            '[Profiling] %s (%s) - %d projets - %.2fs - %.2f Mo',
+            $this->portefeuille,
+            $this->utilisateur,
+            $this->nbProjets,
+            $this->tempsTotal,
+            $this->memoirePeak
+        );
+    }
 }
