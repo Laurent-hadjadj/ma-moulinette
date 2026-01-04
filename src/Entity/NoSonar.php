3d92cf098d9e5b4344da876e5e267948839cac28
Laurent HADJADJ <laurent_h@me.com>
2026-01-04 13:07:36 +0100

    Code-Clean: Mise en conformité du code.

--- src/Entity/NoSonar.php
+++ src/Entity/NoSonar.php
@@ -19,123 +19,142 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: NoSonarRepository::class)]
-#[ORM\Table(name: "no_sonar", schema: "ma_moulinette")]
+#[ORM\Table(
+    name: 'no_sonar',
+    schema: "ma_moulinette",
+    options: ['comment' => 'Table des exception NoSonar et suppressWarning.'])]
 class NoSonar
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column(type: Types::INTEGER, nullable: false,
-        options: ['comment' => 'Identifiant unique la table NoSonar'])]
-    private $id;
+    #[ORM\Column(
+        name: 'id',
+        type: Types::INTEGER,
+        options: ['comment' => 'Identifiant unique pour la table NoSonar'])]
+    private ?int $id = null;
 
-    #[ORM\Column(type: Types::STRING, length: 255, nullable: false,
+    #[ORM\Column(
+        name: 'maven_key',
+        type: Types::STRING,
+        length: 255, nullable: false,
         options: ['comment' => 'Clé Maven du projet'])]
     #[Assert\NotBlank]
-    #[Assert\Length(max: 255,
+    #[Assert\Length(
+        max: 255,
         maxMessage: "La clé Maven ne doit pas dépasser 255 caractères.")]
-    private $mavenKey;
+    private string $mavenKey;
 
-    #[ORM\Column(type: Types::STRING, length: 128, nullable: false,
+    #[ORM\Column(
+        name: 'rule',
+        type: Types::STRING,
+        length: 128,
+        nullable: false,
         options: ['comment' => 'Règle NoSonar appliquée'])]
     #[Assert\NotBlank]
-    #[Assert\Length(max: 128,
-        maxMessage: "La règle nosonar ne doit pas dépasser 128 caractères.")]
-    private $rule;
+    #[Assert\Length(
+        max: 128,
+        maxMessage: "La règle NoSonar ne doit pas dépasser 128 caractères.")]
+    private string $rule;
 
-    #[ORM\Column(type: Types::TEXT, nullable: false,
+    #[ORM\Column(
+        name: 'component',
+        type: Types::TEXT,
+        nullable: false,
         options: ['comment' => 'Composant auquel la règle est appliquée'])]
     #[Assert\NotBlank]
-    private $component;
+    private string $component;
 
-    #[ORM\Column(type: Types::INTEGER, nullable: false,
+    #[ORM\Column(
+        name: 'line',
+        type: Types::INTEGER,
+        nullable: false,
         options: ['comment' => 'Ligne où la règle NoSonar est appliquée'])]
     #[Assert\NotNull]
-    private $line;
+    #[Assert\PositiveOrZero]
+    private int $line;
 
-    #[ORM\Column(type: Types::STRING, length: 32, nullable: true,
-    options: ['comment' => 'Mode de collecte : [COLLECTE] | [TRAITEMENT MANUEL] | [TRAITEMENT AUTOMATIQUE]'])]
-    #[Assert\Length(max: 32,
+    #[ORM\Column(
+        name: 'mode_collecte',
+        type: Types::STRING,
+        length: 32,
+        nullable: true,
+        options: ['comment' => 'Mode de collecte : [COLLECTE] | [TRAITEMENT MANUEL] | [TRAITEMENT AUTOMATIQUE]'])]
+    #[Assert\Length(
+        max: 32,
         maxMessage: "Le mode de collecte ne peut pas dépasser 32 caractères.")]
-    private ?string $modeCollecte=null;
+    private ?string $modeCollecte = null;
 
-    #[ORM\Column(type: Types::STRING, length: 320, nullable: true,
-    options: ['comment' => "Compte de l'utilisateur qui a réalisé la collecte."])]
-    #[Assert\Length(max: 320,
+    #[ORM\Column(
+        name: 'utilisateur_collecte',
+        type: Types::STRING,
+        length: 320,
+        nullable: true,
+        options: ['comment' => "Compte de l'utilisateur qui a réalisé la collecte."])]
+    #[Assert\Length(
+        max: 320,
         maxMessage: "Le compte de l’utilisateur ne peut pas dépasser 320 caractères.")]
-    private ?string $utilisateurCollecte=null;
+    private ?string $utilisateurCollecte = null;
 
-    #[ORM\Column(type: Types::DATETIMETZ_IMMUTABLE, nullable: false,
+    #[ORM\Column(
+        name: 'date_enregistrement',
+        type: Types::DATETIMETZ_IMMUTABLE,
+        nullable: false,
         options: ['comment' => 'Date d’enregistrement de l’entrée NoSonar'])]
     #[Assert\NotNull]
-    private $dateEnregistrement;
+    private \DateTimeImmutable $dateEnregistrement;
 
+    public function __construct()
+    {
+        $this->dateEnregistrement = new \DateTimeImmutable('now', new \DateTimeZone('Europe/Paris'));
+    }
+
+    // Getters et setters
     public function getId(): ?int
     {
         return $this->id;
     }
-    public function setId(int $id): self
-    {
-        $this->id = $id;
-        return $this;
-    }
 
-    public function getMavenKey(): ?string
+    public function getMavenKey(): string
     {
         return $this->mavenKey;
     }
 
-    public function setMavenKey(string $mavenKey): static
+    public function setMavenKey(string $mavenKey): self
     {
         $this->mavenKey = $mavenKey;
-
         return $this;
     }
 
-    public function getRule(): ?string
+    public function getRule(): string
     {
         return $this->rule;
     }
 
-    public function setRule(string $rule): static
+    public function setRule(string $rule): self
     {
         $this->rule = $rule;
-
         return $this;
     }
 
-    public function getComponent(): ?string
+    public function getComponent(): string
     {
         return $this->component;
     }
 
-    public function setComponent(string $component): static
+    public function setComponent(string $component): self
     {
         $this->component = $component;
-
         return $this;
     }
 
-    public function getLine(): ?int
+    public function getLine(): int
     {
         return $this->line;
     }
 
-    public function setLine(int $line): static
+    public function setLine(int $line): self
     {
         $this->line = $line;
-
-        return $this;
-    }
-
-    public function getDateEnregistrement(): ?\DateTimeImmutable
-    {
-        return $this->dateEnregistrement;
-    }
-
-    public function setDateEnregistrement(\DateTimeImmutable $dateEnregistrement): static
-    {
-        $this->dateEnregistrement = $dateEnregistrement;
-
         return $this;
     }
 
@@ -144,10 +163,9 @@ class NoSonar
         return $this->modeCollecte;
     }
 
-    public function setModeCollecte(?string $modeCollecte): static
+    public function setModeCollecte(?string $modeCollecte): self
     {
         $this->modeCollecte = $modeCollecte;
-
         return $this;
     }
 
@@ -156,11 +174,20 @@ class NoSonar
         return $this->utilisateurCollecte;
     }
 
-    public function setUtilisateurCollecte(?string $utilisateurCollecte): static
+    public function setUtilisateurCollecte(?string $utilisateurCollecte): self
     {
         $this->utilisateurCollecte = $utilisateurCollecte;
-
         return $this;
     }
 
+    public function getDateEnregistrement(): \DateTimeImmutable
+    {
+        return $this->dateEnregistrement;
+    }
+
+    public function setDateEnregistrement(\DateTimeImmutable $dateEnregistrement): self
+    {
+        $this->dateEnregistrement = $dateEnregistrement;
+        return $this;
+    }
 }
