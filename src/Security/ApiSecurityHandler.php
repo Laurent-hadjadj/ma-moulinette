8d880311628a338457583272a639c2b865ee848c
Laurent HADJADJ <laurent_h@me.com>
2026-01-04 16:22:24 +0100

    Code-Clean: Amélioration du code.

--- src/Security/ApiSecurityHandler.php
+++ src/Security/ApiSecurityHandler.php
@@ -14,11 +14,8 @@
 namespace App\Security;
 
 use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\{Request, Response, JsonResponse};
+use Symfony\Component\Security\Core\Exception\{AuthenticationException, AccessDeniedException};
 use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
 use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
--- src/Security/CustomAuthenticator.php
+++ src/Security/CustomAuthenticator.php
@@ -13,23 +13,17 @@
 
 namespace App\Security;
 
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\HttpFoundation\{Response, Request, RedirectResponse, JsonResponse};
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\{AuthenticationException, CustomUserMessageAuthenticationException};
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
-use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\{CsrfTokenBadge, RememberMeBadge, UserBadge};
+use Symfony\Component\Security\Http\Authenticator\Passport\{Passport, SelfValidatingPassport};
 use Symfony\Component\Routing\RouterInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\UtilisateurRepository;
+use App\Service\{UserAgentTrackingFacade, UserAgentAnalysisService};
 
 /**
  * [Description CustomAuthenticator]
@@ -43,8 +37,9 @@ class CustomAuthenticator extends AbstractLoginFormAuthenticator
         private UtilisateurRepository $utilisateurRepository,
         private EntityManagerInterface $em,
         private UserPasswordHasherInterface $passwordHasher,
-        private RouterInterface $router
-    ) {
+        private RouterInterface $router,
+        private UserAgentTrackingFacade $tracking
+        ) {
     }
 
     /**
@@ -119,6 +114,7 @@ class CustomAuthenticator extends AbstractLoginFormAuthenticator
      */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
+        $this->tracking->track('LOGGED');
         $user = $token->getUser();
         if ($user instanceof \App\Entity\Utilisateur && $user->isResetPassword()) {
             return new RedirectResponse($this->router->generate('reset_mot_de_passe'));
