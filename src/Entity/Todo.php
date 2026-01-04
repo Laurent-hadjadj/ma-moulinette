2f7fecae5a75daa2d2a9867bf6c36828aec782f8
Laurent HADJADJ <laurent_h@me.com>
2026-01-04 15:54:39 +0100

    Code-Clean: Mise en conformité du code.

--- src/Entity/Todo.php
+++ src/Entity/Todo.php
@@ -19,53 +19,91 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: TodoRepository::class)]
-#[ORM\Table(name: "todo", schema: "ma_moulinette")]
+#[ORM\Table(
+    name: 'todo',
+    schema: 'ma_moulinette',
+    options: ['comment' => 'Table des todos.']
+    )]
 class Todo
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column(type: Types::INTEGER, nullable: false,
-        options: ['comment' => 'ID unique pour la table to.do'])]
-    private $id;
+    #[ORM\Column(
+        name: 'id',
+        type: Types::INTEGER,
+        options: ['comment' => 'ID unique pour la table todo'])]
+    private ?int $id = null;
 
-    #[ORM\Column(type: Types::STRING, length: 255, nullable: false,
+    #[ORM\Column(
+        name: 'maven_key',
+        type: Types::STRING,
+        length: 255,
+        nullable: false,
         options: ['comment' => 'Clé Maven du projet'])]
     #[Assert\NotBlank]
-    #[Assert\Length(max: 255,
-        maxMessage: "La clé Maven ne doit pas dépasser 255 caractères.")]
-    private $mavenKey;
+    #[Assert\Length(max: 255, maxMessage: "La clé Maven ne doit pas dépasser 255 caractères.")]
+    private string $mavenKey;
 
-    #[ORM\Column(type: Types::STRING, length: 128, nullable: false,
+    #[ORM\Column(
+        name: 'rule',
+        type: Types::STRING,
+        length: 128,
+        nullable: false,
         options: ['comment' => 'Règle appliquée à la tâche'])]
     #[Assert\NotBlank]
-    private $rule;
+    private string $rule;
 
-    #[ORM\Column(type: Types::TEXT, nullable: false,
+    #[ORM\Column(
+        name: 'component',
+        type: Types::TEXT,
+        nullable: false,
         options: ['comment' => 'Détails du composant concerné par la tâche'])]
     #[Assert\NotBlank]
-    private $component;
+    private string $component;
 
-    #[ORM\Column(type: Types::INTEGER, nullable: false,
+    #[ORM\Column(
+        name: 'line',
+        type: Types::INTEGER,
+        nullable: false,
         options: ['comment' => 'Numéro de ligne du code associée à la tâche'])]
     #[Assert\NotNull]
-    private $line;
+    #[Assert\PositiveOrZero]
+    private int $line;
 
-    #[ORM\Column(type: Types::STRING, length: 32, nullable: true,
-    options: ['comment' => 'Mode de collecte : MANUEL | AUTOMATIQUE'])]
-    #[Assert\Length(max: 32,
+    #[ORM\Column(
+        name: 'mode_collecte',
+        type: Types::STRING,
+        length: 32,
+        nullable: true,
+        options: ['comment' => 'Mode de collecte : MANUEL | AUTOMATIQUE'])]
+    #[Assert\Length(
+        max: 32,
         maxMessage: "Le mode de collecte ne peut pas dépasser 32 caractères.")]
-    private ?string $modeCollecte=null;
+    private ?string $modeCollecte = null;
 
-    #[ORM\Column(type: Types::STRING, length: 320, nullable: true,
-    options: ['comment' => "Le compte de l'utilisateur qui a réalisé la collecte."])]
-    #[Assert\Length(max: 320,
+    #[ORM\Column(
+        name: 'utilisateur_collecte',
+        type: Types::STRING,
+        length: 320,
+        nullable: true,
+        options: ['comment' => "Le compte de l'utilisateur qui a réalisé la collecte."])]
+    #[Assert\Length(
+        max: 320,
         maxMessage: "Le compte de l'utilisateur ne peut pas dépasser 320 caractères.")]
-    private ?string $utilisateurCollecte=null;
+    private ?string $utilisateurCollecte = null;
 
-    #[ORM\Column(type: Types::DATETIMETZ_IMMUTABLE, nullable: false,
+    #[ORM\Column(
+        name: 'date_enregistrement',
+        type: Types::DATETIMETZ_IMMUTABLE,
+        nullable: false,
         options: ['comment' => 'Date d’enregistrement de la tâche dans le système'])]
     #[Assert\NotNull]
-    private $dateEnregistrement;
+    private \DateTimeImmutable $dateEnregistrement;
+
+    public function __construct()
+    {
+        $this->dateEnregistrement = new \DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
@@ -82,7 +120,7 @@ class Todo
         return $this->mavenKey;
     }
 
-    public function setMavenKey(string $mavenKey): static
+    public function setMavenKey(string $mavenKey): self
     {
         $this->mavenKey = $mavenKey;
 
@@ -94,7 +132,7 @@ class Todo
         return $this->rule;
     }
 
-    public function setRule(string $rule): static
+    public function setRule(string $rule): self
     {
         $this->rule = $rule;
 
@@ -106,7 +144,7 @@ class Todo
         return $this->component;
     }
 
-    public function setComponent(string $component): static
+    public function setComponent(string $component): self
     {
         $this->component = $component;
 
@@ -118,7 +156,7 @@ class Todo
         return $this->line;
     }
 
-    public function setLine(int $line): static
+    public function setLine(int $line): self
     {
         $this->line = $line;
 
@@ -130,7 +168,7 @@ class Todo
         return $this->dateEnregistrement;
     }
 
-    public function setDateEnregistrement(\DateTimeImmutable $dateEnregistrement): static
+    public function setDateEnregistrement(\DateTimeImmutable $dateEnregistrement): self
     {
         $this->dateEnregistrement = $dateEnregistrement;
 
@@ -142,7 +180,7 @@ class Todo
         return $this->modeCollecte;
     }
 
-    public function setModeCollecte(?string $modeCollecte): static
+    public function setModeCollecte(?string $modeCollecte): self
     {
         $this->modeCollecte = $modeCollecte;
 
@@ -154,7 +192,7 @@ class Todo
         return $this->utilisateurCollecte;
     }
 
-    public function setUtilisateurCollecte(?string $utilisateurCollecte): static
+    public function setUtilisateurCollecte(?string $utilisateurCollecte): self
     {
         $this->utilisateurCollecte = $utilisateurCollecte;
 
