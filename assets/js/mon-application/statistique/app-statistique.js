89ed30602658df5ea58b5d5c3bd2f45a2429c66e
Laurent HADJADJ <laurent_h@me.com>
2026-03-08 20:38:01 +0100

    Refactor: Ajout du script JS pour la page statistique

---
+++ assets/js/mon-application/statistique/app-statistique.js
@@ -0,0 +1,30 @@
+/**
+ *  Ma-Moulinette
+ *  --------------
+ *  Copyright © 2015-2026.
+ *  Laurent HADJADJ <laurent_h@me.com>.
+ *  Licensed Creative Common  CC-BY-NC-SA 4.0.
+ *  ---
+ *  Vous pouvez obtenir une copie de la licence à l'adresse suivante :
+ *  http://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+/** Import des dépendances */
+import 'foundation-sites/dist/css/foundation.min.css';
+import 'motion-ui/dist/motion-ui.css';
+import '../../../styles/common/common.css';
+import '../../../styles/common/police.css';
+import '../../../styles/mon-application/statistique.css';
+
+/** Intégration de jquery */
+import $ from 'jquery';
+window.$ = $;
+
+import 'what-input';
+import 'foundation-sites';
+import 'motion-ui';
+
+import '../../common/foundation.js';
+
+/** Affiche le détails de l'utilisateur */
+import '../../auth/details.js';
