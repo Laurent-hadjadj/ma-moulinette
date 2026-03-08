e06d47c49bf9f1e65e4e3b8dffa0518a90509069
Laurent HADJADJ <laurent_h@me.com>
2026-03-08 20:23:56 +0100

    Sql: Mise à jour du changelog.

--- migrations/PosgreSQL/60_comments/comments.sql
+++ migrations/PosgreSQL/60_comments/comments.sql
@@ -2,13 +2,15 @@
 ####################################################
 ##                                                ##
 ##           Create TABLES                        ##
-##           V2.1.0 - 05/02/2026                  ##
+##           V2.2.0 - 08/03/2026                  ##
 ##                                                ##
 ####################################################*/
 
 --- 2025-11-30 : Migration postGreSql 18
 --- 2026-02-05 : Ajout du commentaire pour visitor_id de la table user_agent_event.
 --- 2026-02-05 : Ajout de l'index pour visitor_id et user_id de la table user_agent_analysis
+--- 2026-03-08 : Ajout de la colonne last_activity_at à la table utilisateur
+
 
 -- ⚠️ Le script doit être lancé avec l'utilisateur propriétaire du schema
 \c ma_moulinette db_user
