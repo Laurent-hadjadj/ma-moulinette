1c85bc9ff0e84b53097585e7f1cb265bb34127ce
Laurent HADJADJ <laurent_h@me.com>
2026-03-29 18:48:42 +0200

    Refactor: Mise à jour de la méthode d'insertion des données.

--- src/Repository/MesuresRepository.php
+++ src/Repository/MesuresRepository.php
@@ -22,155 +22,258 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class MesuresRepository extends ServiceEntityRepository
 {
-  public static $removeReturnLine = "/\s+/u";
-  public static $mavenKey = ':maven_key';
-  public static $noDataBase = 'La connexion à la base de données a échoué.';
+    public static $removeReturnLine = "/\s+/u";
+    public static $mavenKey = ':maven_key';
+    public static $noDataBase = 'La connexion à la base de données a échoué.';
 
-  public function __construct(ManagerRegistry $registry)
-  {
-      parent::__construct($registry, Mesures::class);
-  }
-
-  /**
-   * [Description for handleDatabaseException]
-   *
-   * @param \Throwable $e
-   *
-   * @return array
-   *
-   * Created at: 18/12/2024 15:29:21 (Europe/Paris)
-   * @author     Laurent HADJADJ <laurent_h@me.com>
-   * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
-   */
-  public function handleDatabaseException(\Throwable $e): array
-  {
-      $message = $e->getMessage();
-
-      // message = 'SQLSTATE[08006]'
-      if ($e instanceof \Doctrine\DBAL\Exception\ConnectionException) {
-          $message = static::$noDataBase;
-      }
-
-      // state = '23502'
-      if ($e instanceof \Doctrine\DBAL\Exception\NotNullConstraintViolationException) {
-          $message = $e->getMessage();
-      }
-
-      // state = '23505'
-      if ($e instanceof \Doctrine\DBAL\Exception\UniqueConstraintViolationException) {
-          return ['code' => 23505, 'erreur' => 'Les informations existent déjà.'];
-      }
-
-      return ['code' => 500, 'erreur' => $message];
-  }
-
-  /**
-   * [Description for selectMesuresVersionLast]
-   * Retourne les mesures de la dernière version d'un projet
-   *
-   * @param array $map
-   *
-   * @return array
-   *
-   * Created at: 17/03/2024 22:51:02 (Europe/Paris)
-   * @author     Laurent HADJADJ <laurent_h@me.com>
-   * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
-   */
-  public function selectMesuresVersionLast($map): array
-  {
-    $sql = "SELECT project_name as name, ncloc, language_distribution,
-                lines, files, classes, functions, coverage, sqale_debt_ratio,
-                duplicated_lines_density, tests, issues
-            FROM ma_moulinette.mesures
-            WHERE maven_key=:maven_key
-            ORDER BY date_enregistrement DESC LIMIT 1";
-    try {
-          $stmt = $this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnLine, " ", $sql));
-            $stmt->bindValue(static::$mavenKey, $map['maven_key']);
-          $mesures = $stmt->executeQuery()->fetchAllAssociative();
-      } catch (\Throwable $e) {
-          return $this->handleDatabaseException($e);
-      }
-      return ['code' => 200, 'mesures' => $mesures, 'erreur' => ''];
-  }
-
-  /**
-   * [Description for insertMesures]
-   *
-   * @param array $map
-   *
-   * @return array
-   *
-   * Created at: 21/05/2024 22:57:29 (Europe/Paris)
-   * @author     Laurent HADJADJ <laurent_h@me.com>
-   * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
-   */
-  public function insertMesures($map): array
-  {
-    $sql = "INSERT INTO ma_moulinette.mesures
-                (maven_key, project_name, lines, ncloc, language_distribution, sqale_debt_ratio, files, classes, functions, coverage, duplicated_lines_density, tests, issues, mode_collecte, utilisateur_collecte, date_enregistrement)
-            VALUES
-                (:maven_key, :project_name, :lines, :ncloc, :language_distribution::json, :sqale_debt_ratio, :files, :classes, :functions, :coverage, :duplicated_lines_density, :tests, :issues, :mode_collecte, :utilisateur_collecte, :date_enregistrement)";
-    try {
-        $this->getEntityManager()->getConnection()->beginTransaction();
-          $stmt = $this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnLine, " ", $sql));
-            $stmt->bindValue(static::$mavenKey, $map['maven_key']);
-            $stmt->bindValue(':project_name', $map['project_name']);
-            $stmt->bindValue(':lines', $map['lines']);
-            $stmt->bindValue(':ncloc', $map['ncloc']);
-            // Encode le tableau en JSON et l'encapsule dans un tableau PostgreSQL
-            $language_distribution_json = json_encode([$map['language_distribution']]);
-            $stmt->bindValue(':language_distribution', $language_distribution_json);
-            $stmt->bindValue(':sqale_debt_ratio', $map['sqale_debt_ratio']);
-            $stmt->bindValue(':files', $map['files']);
-            $stmt->bindValue(':classes', $map['classes']);
-            $stmt->bindValue(':functions', $map['functions']);
-            $stmt->bindValue(':coverage', $map['coverage']);
-            $stmt->bindValue(':duplicated_lines_density', $map['duplicated_lines_density']);
-            $stmt->bindValue(':tests', $map['tests']);
-            $stmt->bindValue(':issues', $map['issues']);
-            $stmt->bindValue(':mode_collecte', $map['mode_collecte']);
-            $stmt->bindValue(':utilisateur_collecte', $map['utilisateur_collecte']);
-
-            // Formate la date avant de l'enregistrer
-            $stmt->bindValue(':date_enregistrement', $map['date_enregistrement']->format('Y-m-d H:i:sO'));
-            $stmt->executeStatement();
-        $this->getEntityManager()->getConnection()->commit();
-    } catch (\Throwable $e) {
-        $this->getEntityManager()->getConnection()->rollBack();
-        return $this->handleDatabaseException($e);
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Mesures::class);
     }
 
-    return ['code' => 200, 'erreur' => ''];
-  }
+    /**
+     * [Description for handleDatabaseException]
+     *
+     * @param \Throwable $e
+     *
+     * @return array
+     *
+     * Created at: 18/12/2024 15:29:21 (Europe/Paris)
+     * @author     Laurent HADJADJ <laurent_h@me.com>
+     * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function handleDatabaseException(\Throwable $e): array
+    {
+        $message = $e->getMessage();
 
-  /**
-   * [Description for deleteMesuresMavenKey]
-   * Supprime les mesures de la version courante (i.e. correspondant à la maven_key)
-   *
-   * @param array $map
-   *
-   * @return array
-   *
-   * Created at: 26/05/2024 10:51:36 (Europe/Paris)
-   * @author     Laurent HADJADJ <laurent_h@me.com>
-   * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
-   */
-  public function deleteMesuresMavenKey($map): array
-  {
-    $sql = "DELETE
-            FROM ma_moulinette.mesures
-            WHERE maven_key=:maven_key";
-    try {
-          $this->getEntityManager()->getConnection()->beginTransaction();
+        // message = 'SQLSTATE[08006]'
+        if ($e instanceof \Doctrine\DBAL\Exception\ConnectionException) {
+            $message = static::$noDataBase;
+        }
+
+        // state = '23502'
+        if ($e instanceof \Doctrine\DBAL\Exception\NotNullConstraintViolationException) {
+            $message = $e->getMessage();
+        }
+
+        // state = '23505'
+        if ($e instanceof \Doctrine\DBAL\Exception\UniqueConstraintViolationException) {
+            return ['code' => 23505, 'erreur' => 'Les informations existent déjà.'];
+        }
+
+        return ['code' => 500, 'erreur' => $message];
+    }
+
+    /**
+     * [Description for selectMesuresVersionLast]
+     * Retourne les mesures de la dernière version d'un projet
+     *
+     * @param array $map
+     *
+     * @return array
+     *
+     * Created at: 17/03/2024 22:51:02 (Europe/Paris)
+     * @author     Laurent HADJADJ <laurent_h@me.com>
+     * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function selectMesuresVersionLast($map): array
+    {
+        $sql = "SELECT *
+                FROM ma_moulinette.mesures
+                WHERE maven_key = :maven_key
+                ORDER BY date_enregistrement DESC LIMIT 1";
+
+        $con = $this->getEntityManager()->getConnection();
+
+        try {
+            $stmt = $con->prepare(preg_replace(static::$removeReturnLine, " ", $sql));
+                    $stmt->bindValue(static::$mavenKey, $map['maven_key']);
+            //on returne un tableau associatif (une ligne) ou false si aucune ligne trouvée
+            $mesures = $stmt->executeQuery()->fetchAssociative();
+        } catch (\Throwable $e) {
+            return $this->handleDatabaseException($e);
+        }
+        return ['code' => 200, 'mesures' => $mesures, 'erreur' => ''];
+    }
+
+    /**
+     * [Description for insertMesures]
+     *
+     * @param array $map
+     *
+     * @return array
+     *
+     * Created at: 21/05/2024 22:57:29 (Europe/Paris)
+     * @author     Laurent HADJADJ <laurent_h@me.com>
+     * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function insertMesures($map): array
+    {
+        $columns = [
+            'maven_key',
+            'project_name',
+
+            'alert_status',
+
+            'lines',
+            'ncloc',
+            'ncloc_language_distribution',
+            'files',
+            'classes',
+            'functions',
+            'statements',
+
+            'comment_lines',
+            'comment_lines_density',
+            'comment_lines_rating',
+
+            'coverage',
+            'branch_coverage',
+            'line_coverage',
+            'lines_to_cover',
+            'conditions_to_cover',
+            'uncovered_conditions',
+            'coverage_rating',
+
+            'tests',
+            'test_execution_time',
+            'test_errors',
+            'test_failures',
+            'skipped_tests',
+            'test_success_density',
+
+            'duplicated_files',
+            'duplicated_blocks',
+            'duplicated_lines',
+            'duplicated_lines_density',
+            'duplicated_lines_rating',
+
+            'complexity',
+            'complexity_rating',
+            'cognitive_complexity',
+            'cognitive_complexity_rating',
+            'complexity_ratio',
+            'cognitive_complexity_ratio',
+
+            'open_issues',
+            'reopened_issues',
+            'confirmed_issues',
+            'false_positive_issues',
+            'accepted_issues',
+            'high_impact_accepted_issues',
+
+            'violations',
+            'blocker_violations',
+            'critical_violations',
+            'major_violations',
+            'minor_violations',
+            'info_violations',
+
+            'software_quality_blocker_issues',
+            'software_quality_high_issues',
+            'software_quality_medium_issues',
+            'software_quality_low_issues',
+            'software_quality_info_issues',
+
+            'code_smells',
+            'maintainability_issues',
+            'sqale_index',
+            'sqale_debt_ratio',
+            'sqale_rating',
+            'effort_to_reach_maintainability_rating_a',
+
+            'software_quality_maintainability_issues',
+            'software_quality_maintainability_rating',
+            'software_quality_maintainability_debt_ratio',
+            'software_quality_maintainability_remediation_effort',
+            'effort_to_reach_software_quality_maintainability_rating_a',
+
+            'bugs',
+            'reliability_issues',
+            'reliability_rating',
+            'reliability_remediation_effort',
+
+            'software_quality_reliability_issues',
+            'software_quality_reliability_rating',
+            'software_quality_reliability_remediation_effort',
+
+            'vulnerabilities',
+            'security_issues',
+            'security_rating',
+            'security_remediation_effort',
+
+            'software_quality_security_issues',
+            'software_quality_security_rating',
+            'software_quality_security_remediation_effort',
+
+            'security_hotspots',
+            'security_review_rating',
+            'security_hotspots_reviewed',
+
+            'mode_collecte',
+            'utilisateur_collecte',
+            'date_enregistrement'
+        ];
+
+        // Construction SQL automatique
+        $sql = sprintf(
+            "INSERT INTO ma_moulinette.mesures (%s) VALUES (%s)",
+            implode(', ', $columns),
+            ':' . implode(', :', $columns)
+        );
+
+        try {
+            $this->getEntityManager()->getConnection()->beginTransaction();
             $stmt = $this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnLine, " ", $sql));
-              $stmt->bindValue(static::$mavenKey, $map['maven_key']);
-              $stmt->executeStatement();
-          $this->getEntityManager()->getConnection()->commit();
-    } catch (\Throwable $e) {
-        $this->getEntityManager()->getConnection()->rollback();
-        return $this->handleDatabaseException($e);
+                // Bind automatique sécurisé
+                foreach ($columns as $column) {
+                    if ($column === 'date_enregistrement') {
+                        $stmt->bindValue(
+                            ':' . $column,
+                            $map[$column]->format('Y-m-d H:i:s')
+                        );
+                    } else {
+                        $stmt->bindValue(':' . $column, $map[$column] ?? null);
+                    }
+                }
+
+                $stmt->executeStatement();
+            $this->getEntityManager()->getConnection()->commit();
+        } catch (\Throwable $e) {
+            $this->getEntityManager()->getConnection()->rollBack();
+            return $this->handleDatabaseException($e);
+        }
+
+        return ['code' => 200, 'erreur' => ''];
     }
-    return ['code' => 200, 'erreur' => ''];
-  }
+
+    /**
+     * [Description for deleteMesuresMavenKey]
+     * Supprime les mesures de la version courante (i.e. correspondant à la maven_key)
+     *
+     * @param array $map
+     *
+     * @return array
+     *
+     * Created at: 26/05/2024 10:51:36 (Europe/Paris)
+     * @author     Laurent HADJADJ <laurent_h@me.com>
+     * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+     */
+    public function deleteMesuresMavenKey($map): array
+    {
+        $sql = "DELETE
+                FROM ma_moulinette.mesures
+                WHERE maven_key = :maven_key";
+        try {
+            $this->getEntityManager()->getConnection()->beginTransaction();
+                $stmt = $this->getEntityManager()->getConnection()->prepare(preg_replace(static::$removeReturnLine, " ", $sql));
+                $stmt->bindValue(static::$mavenKey, $map['maven_key']);
+                $stmt->executeStatement();
+            $this->getEntityManager()->getConnection()->commit();
+        } catch (\Throwable $e) {
+            $this->getEntityManager()->getConnection()->rollback();
+            return $this->handleDatabaseException($e);
+        }
+        return ['code' => 200, 'erreur' => ''];
+    }
 }
