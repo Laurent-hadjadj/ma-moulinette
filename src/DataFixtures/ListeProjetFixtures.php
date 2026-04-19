12fd0e2bd5c19eb9960c207afd697b5156d97a96
Laurent HADJADJ <laurent_h@me.com>
2026-04-19 19:05:08 +0200

    Fixe: Mise à jour des fixtures.

--- src/DataFixtures/ListeProjetFixtures.php
+++ src/DataFixtures/ListeProjetFixtures.php
@@ -17,10 +17,26 @@ class ListeProjetFixtures extends Fixture
   private static $visibility = 'private';
   private static $dateEnregistrement = '2024-04-12 16:23:11+01';
 
+  /**
+   * [Description for load]
+   *
+   * @param ObjectManager $manager
+   *
+   * @return void
+   *
+   * Created at: 19/04/2026 18:34:33 (Europe/Paris)
+   * @author     Laurent HADJADJ <laurent_h@me.com>
+   * @copyright  Licensed Ma-Moulinette - Creative Common CC-BY-NC-SA 4.0.
+   */
   public function load(ObjectManager $manager): void
     {
       /** création du jeu de données pour la table LISTE_PROJET */
-        $listeProjet=(new ListeProjet())
+        $listeProjet=(new ListeProjet(
+          static::$mavenKey,
+          static::$name,
+          static::$visibility,
+          static::$tags,
+          new \DateTimeImmutable(static::$dateEnregistrement)))
             ->setMavenKey(static::$mavenKey)
             ->setName(static::$name)
             ->setTags(static::$tags)
