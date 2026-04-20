b79ccbc87da5ef279730620de104947b2fe7c690
Laurent HADJADJ <laurent_h@me.com>
2026-04-20 21:01:35 +0200

    Fix: Ajoute du setId()

--- src/Entity/Logger.php
+++ src/Entity/Logger.php
@@ -136,6 +136,12 @@ class Logger
         return $this->id;
     }
 
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
     public function getMavenKey(): string
     {
         return $this->mavenKey;
