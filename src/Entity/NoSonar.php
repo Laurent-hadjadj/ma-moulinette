b25bdb9641ec04e3630d6835d02cb29bb8d648be
Laurent HADJADJ <laurent_h@me.com>
2026-04-20 21:02:25 +0200

    Fix: Ajout du setId()

--- src/Entity/NoSonar.php
+++ src/Entity/NoSonar.php
@@ -114,6 +114,12 @@ class NoSonar
         return $this->id;
     }
 
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
     public function getMavenKey(): string
     {
         return $this->mavenKey;
