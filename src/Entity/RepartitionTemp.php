252e91c7b9307498aa1c553049b71805da8aa638
Laurent HADJADJ <laurent_h@me.com>
2026-04-20 21:02:38 +0200

    Fix: Ajout du setId()

--- src/Entity/RepartitionTemp.php
+++ src/Entity/RepartitionTemp.php
@@ -108,6 +108,12 @@ class RepartitionTemp
         return $this->id;
     }
 
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
     public function getMavenKey(): string
     {
         return $this->mavenKey;
