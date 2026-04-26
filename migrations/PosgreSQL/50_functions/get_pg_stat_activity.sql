a780f0136bdf65de76bad7dfa0a746716f7120df
Laurent HADJADJ <laurent_h@me.com>
2026-04-26 18:35:43 +0200

    SQL: Modification de l'utilisateur propriétaire de la fonction et ajout des droits d'exécution pour db_user

--- migrations/PosgreSQL/50_functions/get_pg_stat_activity.sql
+++ migrations/PosgreSQL/50_functions/get_pg_stat_activity.sql
@@ -2,14 +2,16 @@
 ####################################################
 ##                                                ##
 ##           Create TABLES                        ##
-##           V2.0.0 - 30/11/2025                  ##
+##           V2.1.0 - 26/04/2026                  ##
 ##                                                ##
 ####################################################*/
 
 --- 2025-11-30 : Migration postGreSql 18
+--- 2026-04-26 : Modification de l'utilisateur propriétaire de la fonction et ajout des droits d'exécution pour postgres
 
--- ⚠️ Le script doit être lancé avec l'utilisateur propriétaire du schema
-\c ma_moulinette db_user
+-- ⚠️ Ce script doit être lancé avec un superuser (ex: postgres)
+-- car on définit l'ownership de la fonction à postgres (SECURITY DEFINER).
+\c ma_moulinette postgres
 
 CREATE OR REPLACE FUNCTION get_pg_stat_activity()
 RETURNS SETOF pg_catalog.pg_stat_activity
