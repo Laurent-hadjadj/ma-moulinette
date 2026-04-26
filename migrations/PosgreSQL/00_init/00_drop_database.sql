06bae3c67a2b9640a62fe08090d9bac0c5b0712e
Laurent HADJADJ <laurent_h@me.com>
2026-04-26 18:11:06 +0200

    SQL: Ajout de la suppresion de la base de tests

--- migrations/PosgreSQL/00_init/00_drop_database.sql
+++ migrations/PosgreSQL/00_init/00_drop_database.sql
@@ -2,14 +2,20 @@
 ####################################################
 ##                                                ##
 ##         Drop de la base de données             ##
-##         V2.0.0 - 30/11/2025                    ##
+##         V2.1.0 - 26/04/2026                    ##
 ##                                                ##
 ####################################################*/
 
 --- 2025-11-30 : Migration postGreSql 18
+--- 2026_04-26 : Suppressionde la base de tests si elle exite.
+
 -- ⚠️ Le script doit être lancé avec l'utilisateur propriétaire de la base
 -- a lancer depuis la console
 \c postgres;
 
+-- La base principale
 DROP DATABASE IF EXISTS ma_moulinette WITH (FORCE);
+-- La base de test Symfony (APP_ENV=test)
+DROP DATABASE IF EXISTS ma_moulinette_test WITH (FORCE);
+-- Le role peut maintenant etre supprime (plus aucun objet dependant)
 DROP ROLE IF EXISTS db_user;
