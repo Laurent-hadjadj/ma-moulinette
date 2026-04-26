c2ee6b835b8a2aa0ae3e0acfc9162e21e7dc1512
Laurent HADJADJ <laurent_h@me.com>
2026-04-26 18:11:45 +0200

    Test:  Suppression, des tests e2e cypress

--- cypress/e2e/functional/auth/snippet_login.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Tests de la page de connexion', () => {
-  it('devrait permettre une connexion avec des identifiants corrects', function() {
-    cy.fixture('users').then((users) => {
-      cy.login(users.validUser.mel, users.validUser.password);
-    });
-    cy.url().should('include', '/dashboard');
-  });
-});
--- cypress/e2e/functional/home/home_href.cy.js
+++
@@ -1,9 +0,0 @@
-describe('Vérification des liens', () => {
-  it('doit avoir des href valides', () => {
-    cy.visit('/accueil');
-    cy.get('a').each(($el) => {
-      const href = $el.prop('href');
-      cy.request(href).its('status').should('eq', 200);
-    });
-  });
-});
