07ba841bc42bcf8b87db29116482cee8bde3ec17
Laurent HADJADJ <laurent_h@me.com>
2026-06-28 07:52:00 +0200

    Code-Clean: Utilisation de la variable db_user_password pour passer le mode de passe + nettoyage.

--- migrations/PosgreSQL/00_init/01_create_roles.sql
+++ migrations/PosgreSQL/00_init/01_create_roles.sql
@@ -2,16 +2,20 @@
 ####################################################
 ##                                                ##
 ##           Create ROLE                          ##
-##           V2.0.0 - 30/11/2025                  ##
+##           V3.0.0 - 03/05/2026                  ##
 ##                                                ##
 ####################################################*/
 
 --- 2025-11-30 : Migration postGreSql 18
+--- 2026-05-03 : Mot de passe injecté depuis la variable d'environnement
+---              DB_USER_PASSWORD via la variable psql `db_user_password`
+---              (cf. docker/scripts/postgresql/init_sql_files.sh).
+---              Plus de literal `db_password` en dur.
 
 -- ⚠️ Le script doit être lancé avec l'utilisateur propriétaire de la base
 -- a lancer depuis la console.
 \c postgres;
 
-CREATE ROLE db_user LOGIN PASSWORD 'db_password';
+CREATE ROLE db_user LOGIN PASSWORD :'db_user_password';
 ALTER ROLE db_user CREATEDB;
 COMMENT ON ROLE db_user IS 'Utilisateur propriétaire de la base Ma-Moulinette.';
