1135389b85fdb093fb4505e4102b7d42e83df06d
Laurent HADJADJ <laurent_h@me.com>
2026-06-28 09:56:49 +0200

    Code-Clean: L'utilisateur postgres est obligatoire pour passer les grants sur les tables et les autres objets.

--- migrations/PosgreSQL/80_grants/grants.sql
+++ migrations/PosgreSQL/80_grants/grants.sql
@@ -2,14 +2,14 @@
 ####################################################
 ##                                                ##
 ##           Create TABLES                        ##
-##           V2.0.0 - 30/11/2025                  ##
+##           V2.0.1 - 26/06/2026                  ##
 ##                                                ##
 ####################################################*/
 
 --- 2025-11-30 : Migration postGreSql 18
 
 -- ⚠️ Le script doit être lancé avec l'utilisateur propriétaire du schema
-\c ma_moulinette db_user
+\c ma_moulinette postgres;
 
 ------------------------------------------------------------
 -- 1. Droits sur le schéma
@@ -20,6 +20,7 @@ REVOKE ALL ON SCHEMA ma_moulinette FROM PUBLIC;
 -- Donne l'accès au rôle
 GRANT USAGE ON SCHEMA ma_moulinette TO db_user;
 GRANT CREATE ON SCHEMA ma_moulinette TO db_user;
+
 
 ------------------------------------------------------------
 -- 2. Droits sur toutes les tables existantes
@@ -39,6 +40,7 @@ GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA ma_moulinette TO db_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA ma_moulinette
     GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO db_user;
 
+
 ------------------------------------------------------------
 -- 4. Droits sur les fonctions (si un jour tu en ajoutes)
 ------------------------------------------------------------
@@ -47,8 +49,9 @@ GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA ma_moulinette TO db_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA ma_moulinette
     GRANT EXECUTE ON FUNCTIONS TO db_user;
 
+
 ------------------------------------------------------------
--- 5. Blocage de public
+-- 5. En bonus
 ------------------------------------------------------------
 -- Empêcher PUBLIC d'accéder par erreur
 REVOKE ALL PRIVILEGES ON ALL TABLES    IN SCHEMA ma_moulinette FROM PUBLIC;
