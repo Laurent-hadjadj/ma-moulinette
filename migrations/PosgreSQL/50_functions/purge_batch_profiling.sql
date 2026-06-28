cad973950415e6caf98350abe27e933dc2920bb8
Laurent HADJADJ <laurent_h@me.com>
2026-06-28 09:11:20 +0200

    Code-Clean: Correction du titre d'en-tête (était "Create TABLES") + suppression de db_user.

--- migrations/PosgreSQL/50_functions/purge_batch_profiling.sql
+++ migrations/PosgreSQL/50_functions/purge_batch_profiling.sql
@@ -1,15 +1,16 @@
 /*
 ####################################################
 ##                                                ##
-##           Create TABLES                        ##
-##           V2.0.0 - 30/11/2025                  ##
+##           Create FUNCTIONS                     ##
+##           V2.0.1 - 03/05/2026                  ##
 ##                                                ##
 ####################################################*/
 
 --- 2025-11-30 : Migration postGreSql 18
+--- 2026-05-03 : Correction du titre d'en-tête (était "Create TABLES").
 
 -- ⚠️ Le script doit être lancé avec l'utilisateur propriétaire du schema
-\c ma_moulinette db_user
+\c ma_moulinette;
 
 CREATE OR REPLACE FUNCTION ma_moulinette.purge_batch_profiling(v_limit_days INTEGER DEFAULT 90)
 RETURNS INTEGER
