1cb3e490f53dfe65d2b1bd1003fc0795571b07da
Laurent HADJADJ <laurent_h@me.com>
2026-07-12 22:03:38 +0200

    Refactor: Ajout du tracking pour les pages d'erreur avec leur code erreur.

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -3,7 +3,7 @@
 /*
  *  Ma-Moulinette
  *  --------------
- *  Copyright (c) 2021-2024.
+ *  Copyright (c) 2021-2026.
  *  Laurent HADJADJ <laurent_h@me.com>.
  *  Licensed Creative Common  CC-BY-NC-SA 4.0.
  *  ---
@@ -16,20 +16,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Service\UserAgentTrackingFacade;
 use Symfony\Component\ErrorHandler\Exception\FlattenException;
 use Twig\Environment;
+use App\Service\UserAgent\UserAgentTrackingFacade;
 
 /**
  * [Description ErrorController]
  */
 class ErrorController extends AbstractController
 {
-    #[Route('/error', name: 'erreur', methods: ['GET'])]
-    public function show(FlattenException $exception, Environment $env, UserAgentTrackingFacade $tracking): Response
-    {
-        $tracking->track("ERROR_{$exception->getStatusCode()}");
+    public function __construct(
+        private UserAgentTrackingFacade $tracking
+    ) {}
 
+    #[Route('/error', name: 'erreur', methods: ['GET'])]
+    public function show(FlattenException $exception, Environment $env): Response
+    {
+        $this->tracking->track(sprintf('ERROR_%d', $exception->getStatusCode()));
         /** On affiche la page correspondant au code HTTP */
         $view = "bundles/TwigBundle/Exception/error{$exception->getStatusCode()}.html.twig";
         if (!$env->getLoader()->exists($view)) {
