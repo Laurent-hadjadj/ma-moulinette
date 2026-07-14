bba297fc696c04c1e8d91aeb721178a988d2fadd
Laurent HADJADJ <laurent_h@me.com>
2026-07-14 18:55:30 +0200

    Fix: Correction du namespace.

--- src/Command/Prod/CompareSonarMetricsCommand.php
+++ src/Command/Prod/CompareSonarMetricsCommand.php
@@ -11,7 +11,7 @@
  *  http://creativecommons.org/licenses/by-nc-sa/4.0/
  */
 
-namespace App\Command;
+namespace App\Command\Prod;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
