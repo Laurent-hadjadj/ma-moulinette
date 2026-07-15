43bc88c249922da2a5a5f009666f342a4a4ac31b
Laurent HADJADJ <laurent_h@me.com>
2026-07-15 11:21:57 +0200

    Code-Clean: Correction typage.

--- src/Exception/FetchDataException.php
+++ src/Exception/FetchDataException.php
@@ -14,10 +14,10 @@ class FetchDataException extends Exception
     private string $debug;
     /** @var array<int|string, mixed> */
     private array $render;
-/**
- * @param array<int|string, mixed> $render
- */
 
+    /**
+     * @param array<int|string, mixed> $render
+     */
     public function __construct(string $message, string $debug, array $render, int $code = 0, ?Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
@@ -30,6 +30,7 @@ class FetchDataException extends Exception
         return $this->debug;
     }
 
+    /** @return array<int|string, mixed> */
     public function getRender(): array
     {
         return $this->render;
