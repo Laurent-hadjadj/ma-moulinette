5b7a54178e6638f64d70d67b7f4eb7476ab8aba8
Laurent HADJADJ <laurent_h@me.com>
2026-07-22 18:36:14 +0200

    SQL: Suppression de Extensions (pg_stat_statements)

--- migrations/POSTGRESQL/99_master_install.sql
+++ migrations/POSTGRESQL/99_master_install.sql
@@ -12,17 +12,16 @@
 -- ##   1. Drop DB + drop role                  (00_init/00)
 -- ##   2. Create role                          (00_init/01)
 -- ##   3. Create database                      (00_init/02)
--- ##   4. Extensions (pg_stat_statements)      (00_init/03)
--- ##   5. Search path                          (00_init/04)
--- ##   6. Schema ma_moulinette                 (10_schema/01)
--- ##   7. Tables 20_tables                     (37 fichiers .sql)
--- ##   8. Views 30_views                       (5 fichiers .sql)
--- ##   9. Indexes 40_indexes
--- ##  10. Functions 50_functions               (2 fichiers .sql)
--- ##  11. Comments 60_comments
--- ##  12. Constraints 70_constraints
--- ##  13. Grants 80_grants
--- ##  14. Fixtures 90_fixtures
+-- ##   4. Search path                          (00_init/03)
+-- ##   5. Schema ma_moulinette                 (10_schema/01)
+-- ##   6. Tables 20_tables                     (38 fichiers .sql)
+-- ##   7. Views 30_views                       (5 fichiers .sql)
+-- ##   8. Indexes 40_indexes
+-- ##   9. Functions 50_functions               (2 fichiers .sql)
+-- ##  10. Comments 60_comments
+-- ##  11. Constraints 70_constraints
+-- ##  12. Grants 80_grants
+-- ##  13. Fixtures 90_fixtures
 -- ############################################################################
 
 \c postgres;
@@ -35,16 +34,13 @@
 \echo ===== 3. Create database =====
 \ir 00_init/02_create_database.sql
 
-\echo ===== 4. Extensions =====
-\ir 00_init/03_extensions.sql
+\echo ===== 4. Search path =====
+\ir 00_init/03_search_path.sql
 
-\echo ===== 5. Search path =====
-\ir 00_init/04_search_path.sql
-
-\echo ===== 6. Schema =====
+\echo ===== 5. Schema =====
 \ir 10_schema/01_create_schema.sql
 
-\echo ===== 7. Tables =====
+\echo ===== 6. Tables =====
 \ir 20_tables/activity.sql
 \ir 20_tables/activity_batch_report.sql
 \ir 20_tables/activity_historique.sql
@@ -92,33 +88,33 @@
 \ir 20_tables/user_role_log.sql
 \ir 20_tables/utilisateur.sql
 
-\echo ===== 8. Views =====
+\echo ===== 7. Views =====
 \ir 30_views/01_vw_batch_profiling_weekly.sql
 \ir 30_views/02_vw_batch_profiling_monthly.sql
 \ir 30_views/03_vw_batch_profiling_global.sql
 \ir 30_views/04_vw_batch_profiling_stats.sql
 \ir 30_views/05_vw_batch_profiling_summary.sql
 
-\echo ===== 9. Indexes =====
+\echo ===== 8. Indexes =====
 \ir 40_indexes/indexes.sql
 
-\echo ===== 10. Functions =====
+\echo ===== 9. Functions =====
 \ir 50_functions/get_pg_stat_activity.sql
 \ir 50_functions/purge_batch_profiling.sql
 
-\echo ===== 11. Comments =====
+\echo ===== 10. Comments =====
 \ir 60_comments/comments.sql
 
-\echo ===== 12. Constraints =====
+\echo ===== 11. Constraints =====
 \ir 70_constraints/constraints.sql
 
-\echo ===== 13. Grants =====
+\echo ===== 12. Grants =====
 \ir 80_grants/grants.sql
 
-\echo ===== 14. Fixtures =====
+\echo ===== 13. Fixtures =====
 \ir 90_fixtures/fixtures.sql
 
-\echo ===== 15. Fixtures E2E (Phase K) =====
+\echo ===== 14. Fixtures E2E (Phase K) =====
 \ir 90_fixtures/fixtures-e2e.sql
 
 \echo ===== INSTALL TERMINE =====
