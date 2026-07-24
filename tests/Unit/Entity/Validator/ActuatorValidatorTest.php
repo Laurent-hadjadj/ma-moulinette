63338b4ba7bc54ef9e06fc9467ab0a579dedc533
Laurent HADJADJ <laurent_h@me.com>
2026-07-24 19:48:29 +0200

    Test: Code-Clean, formatage du code

--- tests/Unit/Entity/Validator/ActuatorValidatorTest.php
+++ tests/Unit/Entity/Validator/ActuatorValidatorTest.php
@@ -23,7 +23,7 @@ use Symfony\Component\Validator\ConstraintViolation;
  */
 class ActuatorValidatorTest extends KernelTestCase
 {
-  private static string $mavenKey = 'fr.ma-petite-entreprise:ma-moulinette';
+  private static string $mavenKey = 'fr.ma-moulinette:ma-moulinette';
   private static string $nomApplication = 'Application 04';
   private static string $url = 'http://ma-moulinette.fr/app04';
   private static string $actuatorUser = 'user4';
@@ -33,25 +33,25 @@ class ActuatorValidatorTest extends KernelTestCase
   private static string $actuatorInfoDescription = "Actuator INFO pour l'application 04";
   private static string $infoValue = '[SOCLE][ANGULAR]';
 
-private function getEntity(): Actuator
-{
-  $actuator = new Actuator();
-  $actuator->setMavenKey(self::$mavenKey)
-              ->setNomApplication(self::$nomApplication)
-              ->setActuatorUser(self::$actuatorUser)
-              ->setActuatorPassword(self::$actuatorPassword)
-              ->setUrl(self::$url)
-              ->setPersonne(self::$personne)
-              ->setDateModification(new \DateTimeImmutable(self::$dateModification));
+  private function getEntity(): Actuator
+  {
+    $actuator = new Actuator();
+    $actuator->setMavenKey(self::$mavenKey)
+      ->setNomApplication(self::$nomApplication)
+      ->setActuatorUser(self::$actuatorUser)
+      ->setActuatorPassword(self::$actuatorPassword)
+      ->setUrl(self::$url)
+      ->setPersonne(self::$personne)
+      ->setDateModification(new \DateTimeImmutable(self::$dateModification));
 
-  $actuatorInfo = new ActuatorInfo();
-  $actuatorInfo->setActuator($actuator);
-  $actuatorInfo->setActuatorInfoDescription(self::$actuatorInfoDescription);
-  $actuatorInfo->setActuatorInfoValue(self::$infoValue);
-  $actuator->addActuatorInfo($actuatorInfo);
+    $actuatorInfo = new ActuatorInfo();
+    $actuatorInfo->setActuator($actuator);
+    $actuatorInfo->setActuatorInfoDescription(self::$actuatorInfoDescription);
+    $actuatorInfo->setActuatorInfoCle(self::$infoValue);
+    $actuator->addActuatorInfo($actuatorInfo);
 
-  return $actuator;
-}
+    return $actuator;
+  }
 
   public function assertHasErrors(Actuator $entity, int $number = 0): void
   {
@@ -60,7 +60,7 @@ private function getEntity(): Actuator
     $errors = $container->get('validator')->validate($entity);
     $messages = [];
     /** @var ConstraintViolation $error */
-    foreach($errors as $error) {
+    foreach ($errors as $error) {
       $messages[] = $error->getPropertyPath() . ' => ' . $error->getMessage();
     }
     $this->assertCount($number, $errors, implode(', ', $messages));
@@ -76,16 +76,17 @@ private function getEntity(): Actuator
     $this->assertHasErrors($this->getEntity()->setMavenKey(''), 1);
     $this->assertHasErrors($this->getEntity()->setNomApplication(''), 1);
     $this->assertHasErrors($this->getEntity()->setUrl(''), 1);
-    $this->assertHasErrors($this->getEntity()->setActuatorUser  (''), 0);$this->assertHasErrors($this->getEntity()->setActuatorPassword  (''), 0);
+    $this->assertHasErrors($this->getEntity()->setActuatorUser(''), 0);
+    $this->assertHasErrors($this->getEntity()->setActuatorPassword(''), 0);
     $this->assertHasErrors($this->getEntity()->setPersonne(''), 1);
   }
 
   public function testCountAttribut(): void
   {
-      /* MODIF 2026-05-07 [tests-validators] : 11 → 10 (alignement Entity↔DDL session 2026-05-04). */
-      $entity = $this->getEntity();
-      $reflectionClass = new \ReflectionClass($entity);
-      $nbAttributs = count($reflectionClass->getProperties());
-      $this->assertEquals($nbAttributs, 10);
+    /* MODIF 2026-05-07 : 11 → 10 (alignement Entity↔DDL session 2026-05-04). */
+    $entity = $this->getEntity();
+    $reflectionClass = new \ReflectionClass($entity);
+    $nbAttributs = count($reflectionClass->getProperties());
+    $this->assertEquals($nbAttributs, 10);
   }
 }
